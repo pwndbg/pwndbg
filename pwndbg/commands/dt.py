a6d3f65f3e70553d9bb96ef7b8afeee0498468d8
Zach Riggle <riggle@google.com>
2015-03-11 17:22:02 -0700

    Dont parse type name

--- pwndbg/commands/dt.py
+++ pwndbg/commands/dt.py
@@ -4,7 +4,9 @@ import pwndbg.commands
 import pwndbg.color
 import pwndbg.dt
 
-@pwndbg.commands.ParsedCommand
+@pwndbg.commands.Command
 @pwndbg.commands.OnlyWhenRunning
 def dt(typename, address=None):
+    if address is not None:
+        address = pwndbg.commands.fix(address)
     print(pwndbg.dt.dt(typename, addr=address))
