8747be438f04941b776665ba2d6fc59e2ecf3ae7
Zach Riggle <riggle@google.com>
2016-04-21 14:12:41 -0700

    Enhance procinfo with AIDs, add capabilities

--- pwndbg/commands/procinfo.py
+++ pwndbg/commands/procinfo.py
@@ -27,17 +27,69 @@ uid = [287138, 287138, 287138, 287138]
 gid = [5000, 5000, 5000, 5000]
 """
 
+capabilities = {
+ 0: "CAP_CHOWN",
+ 1: "CAP_DAC_OVERRIDE",
+ 2: "CAP_DAC_READ_SEARCH",
+ 3: "CAP_FOWNER",
+ 4: "CAP_FSETID",
+ 5: "CAP_KILL",
+ 6: "CAP_SETGID",
+ 7: "CAP_SETUID",
+ 8: "CAP_SETPCAP",
+ 9: "CAP_LINUX_IMMUTABLE",
+ 10: "CAP_NET_BIND_SERVICE",
+ 11: "CAP_NET_BROADCAST",
+ 12: "CAP_NET_ADMIN",
+ 13: "CAP_NET_RAW",
+ 14: "CAP_IPC_LOCK",
+ 15: "CAP_IPC_OWNER",
+ 16: "CAP_SYS_MODULE",
+ 17: "CAP_SYS_RAWIO",
+ 18: "CAP_SYS_CHROOT",
+ 19: "CAP_SYS_PTRACE",
+ 20: "CAP_SYS_PACCT",
+ 21: "CAP_SYS_ADMIN",
+ 22: "CAP_SYS_BOOT",
+ 23: "CAP_SYS_NICE",
+ 24: "CAP_SYS_RESOURCE",
+ 25: "CAP_SYS_TIME",
+ 26: "CAP_SYS_TTY_CONFIG",
+ 27: "CAP_MKNOD",
+ 28: "CAP_LEASE",
+ 29: "CAP_AUDIT_WRITE",
+ 30: "CAP_AUDIT_CONTROL",
+ 31: "CAP_SETFCAP",
+ 32: "CAP_MAC_OVERRIDE",
+ 33: "CAP_MAC_ADMIN",
+ 34: "CAP_SYSLOG",
+ 35: "CAP_WAKE_ALARM",
+ 36: "CAP_BLOCK_SUSPEND",
+}
+
 class Process():
-    def __init__(self, pid=None):
+    def __init__(self, pid=None, tid=None):
         if pid is None:
             pid = pwndbg.proc.pid
+        if tid is None:
+            tid = pwndbg.proc.tid
         self.pid = pid
+        self.tid = tid
+
+        # Precalculate
         self.status
 
     @property
     @pwndbg.memoize.reset_on_stop
+    def selinux(self):
+        path = '/proc/%i/task/%i/attr/current' % (self.pid, self.tid)
+        raw = pwndbg.file.get(path)
+        return raw.decode().rstrip('\x00')
+
+    @property
+    @pwndbg.memoize.reset_on_stop
     def status(self):
-        raw = pwndbg.file.get('/proc/%i/status' % self.pid)
+        raw = pwndbg.file.get('/proc/%i/task/%i/status' % (self.pid, self.tid))
 
         status = {}
         for line in raw.splitlines():
@@ -77,6 +129,14 @@ class Process():
             # uid and gid and groups
             elif all(map(str.isdigit, v.split())):
                 v = list(map(int, v.split()))
+
+            # capability sets
+            if k in ['capeff', 'capinh', 'capprm', 'capbnd']:
+                orig = v
+                v = []
+                for i in range(max(capabilities)+1):
+                    if (orig >> i) & 1 == 1:
+                        v.append(capabilities[i])
 
             status[k] = v
             setattr(self, k, v)
@@ -145,20 +205,28 @@ def procinfo():
     if not proc.status:
         return
 
-    pid  = proc.pid
-    ppid = proc.ppid
-    uids = proc.uid
-    gids = proc.gid
-
     files = dict(proc.open_files)
 
     for c in proc.connections:
         files[c.fd] = str(c) 
 
-    print("%-10s %s" % ("pid", pid))
-    print("%-10s %s" % ("ppid", ppid))
-    print("%-10s %s" % ("uid", uids))
-    print("%-10s %s" % ("gid", gids))
+    print("%-10s %s" % ("pid",     proc.pid))
+    print("%-10s %s" % ("tid",     proc.tid))
+
+    if proc.selinux != 'unconfined':
+        print("%-10s %s" % ("selinux", proc.selinux))
+
+    print("%-10s %s" % ("ppid",    proc.ppid))
+
+    if not pwndbg.android.is_android():
+        print("%-10s %s" % ("uid",     proc.uid))
+        print("%-10s %s" % ("gid",     proc.gid))
+        print("%-10s %s" % ("groups",  proc.groups))
+    else:
+        print("%-10s %s" % ("uid",     list(map(pwndbg.android.aid_name, proc.uid))))
+        print("%-10s %s" % ("gid",     list(map(pwndbg.android.aid_name, proc.gid))))
+        print("%-10s %s" % ("groups",  list(map(pwndbg.android.aid_name, proc.groups))))
+
     for fd, path in files.items():
         if not set(path) < set(string.printable):
             path = repr(path)
