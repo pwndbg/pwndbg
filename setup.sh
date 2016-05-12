b148edfaed7f9ef34c418b1df8627082fe188cdd
Zach Riggle <riggle@google.com>
2016-05-12 12:35:35 -0700

    Use correct Python; don't use apt on OSX

--- setup.sh
+++ setup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -ex
+
+if uname | grep -i Linux &>/dev/null; then
 sudo apt-get update
 sudo apt-get install python-dev python3-dev python-pip python3-pip libglib2.0-dev
+fi
 
 # Update all submodules
 git submodule update --init --recursive
 
-# Install Python dependencies
-sudo pip install -Ur requirements.txt
-
 # Find the path to the Python interpreter used by GDB.
 PYTHON=$(gdb -batch -q --nx -ex 'pi import sys; print(sys.executable)')
+
+# Install Python dependencies
+sudo $PYTHON -m pip install -Ur requirements.txt
 
 # Find the path to the Python2 interpreter needed by the Unicorn install process.
 export UNICORN_QEMU_FLAGS="--python=$(which python2)"
