abd0a6854c90c3647d17dfb3ea980fa49aa5372f
Zach Riggle <riggle@google.com>
2016-05-11 22:25:23 -0700

    Add fsbase and gsbase commands

--- pwndbg/commands/segments.py
+++ pwndbg/commands/segments.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import gdb
 import pwndbg.regs
+import pwndbg.commands
 
 class segment(gdb.Function):
     """Get the flat address of memory based off of the named segment register.
@@ -14,3 +15,20 @@ class segment(gdb.Function):
 
 segment('fsbase')
 segment('gsbase')
+
+@pwndbg.commands.OnlyWhenRunning
+@pwndbg.commands.ParsedCommand
+def fsbase():
+    """
+    Prints out the FS base address.  See also $fsbase.
+    """
+    print(hex(pwndbg.regs.fsbase))
+
+
+@pwndbg.commands.OnlyWhenRunning
+@pwndbg.commands.ParsedCommand
+def gsbase():
+    """
+    Prints out the GS base address.  See also $gsbase.
+    """
+    print(hex(pwndbg.regs.gsbase))
