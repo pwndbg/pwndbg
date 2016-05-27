f5705206b7f47026291982d2c37bc0cf3bfb42cb
Zach Riggle <zachriggle@users.noreply.github.com>
2016-05-27 14:35:46 -0500

    Fix Python version detection in the install script

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ fi
 git submodule update --init --recursive
 
 # Find the path to the Python interpreter used by GDB.
-PYTHON=$(gdb -batch -q --nx -ex 'pi import sys; print(sys.executable)')
+PYTHON=$(gdb -batch -q --nx -ex 'pi import platform; print("python%s.%s" % platform.python_version_tuple()[:2])')
 
 # Install Python dependencies
 sudo $PYTHON -m pip install -Ur requirements.txt
