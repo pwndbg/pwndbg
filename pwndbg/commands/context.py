c036b33d0147f08a7db3ebfb7a667296a6a4143c
Zach Riggle <riggle@google.com>
2016-06-01 17:07:15 -0700

    Handle QEMU where _siginfo is unavailable

--- pwndbg/commands/context.py
+++ pwndbg/commands/context.py
@@ -225,7 +225,11 @@ def save_signal(signal):
     elif isinstance(signal, gdb.SignalEvent):
         msg = 'Program received signal %s' % signal.stop_signal
         if signal.stop_signal == 'SIGSEGV':
-            msg += ' (fault address %#x)' % int(gdb.parse_and_eval("$_siginfo._sifields._sigfault.si_addr"))
+            try:
+                si_addr = gdb.parse_and_eval("$_siginfo._sifields._sigfault.si_addr")
+                msg += ' (fault address %#x)' % int(si_addr or 0)
+            except gdb.error:
+                pass
         msg = pwndbg.color.red(msg)
         msg = pwndbg.color.bold(msg)
         result.append(msg)
