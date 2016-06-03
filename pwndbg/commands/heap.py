6cf81a0bb74ceda07747be17315d41638bfe5f20
Gulshan Singh <gsingh2011@gmail.com>
2016-06-03 01:22:29 +0000

    Added docs and constructed values from types

--- pwndbg/commands/heap.py
+++ pwndbg/commands/heap.py
@@ -11,13 +11,15 @@ PREV_INUSE = 1
 IS_MMAPED = 2
 NON_MAIN_ARENA = 4
 
+def value_from_type(type_name, addr):
+    gdb_type = pwndbg.typeinfo.load(type_name)
+    return gdb.Value(addr).cast(gdb_type.pointer()).dereference()
+
 def get_main_arena(addr=None):
     if addr == None:
         main_arena = gdb.lookup_symbol('main_arena')[0].value()
     else:
-        if isinstance(addr, int):
-            addr = hex(addr)
-        main_arena = gdb.parse_and_eval('(struct malloc_state)*' + addr)
+        main_arena = value_from_type('struct malloc_state', addr)
 
     if main_arena == None:
         print(red('Symbol \'main_arena\' not found. Try installing libc ' \
@@ -41,6 +43,9 @@ def get_heap_bounds():
 @pwndbg.commands.ParsedCommand
 @pwndbg.commands.OnlyWhenRunning
 def heap(addr=None):
+    """
+    Prints out all chunks in the main_arena, or the arena specified by `addr`.
+    """
     main_arena = get_main_arena(addr)
     if main_arena == None:
         return
@@ -71,6 +76,9 @@ def heap(addr=None):
 @pwndbg.commands.ParsedCommand
 @pwndbg.commands.OnlyWhenRunning
 def arena(addr=None):
+    """
+    Prints out the main arena or the arena at the specified by address.
+    """
     main_arena = get_main_arena(addr)
     if main_arena == None:
         return
@@ -80,6 +88,10 @@ def arena(addr=None):
 @pwndbg.commands.ParsedCommand
 @pwndbg.commands.OnlyWhenRunning
 def bins(addr=None):
+    """
+    Prints out the contents of the fastbins of the main arena or the arena
+    at the specified address.
+    """
     main_arena = get_main_arena(addr)
     if main_arena == None:
         return
@@ -87,22 +99,26 @@ def bins(addr=None):
     fastbins = main_arena['fastbinsY']
     bins = main_arena['bins']
 
-    size_t_size = gdb.lookup_type('size_t').sizeof
+    size_t_size = pwndbg.typeinfo.load('size_t').sizeof
     num_fastbins = int(fastbins.type.sizeof / fastbins.type.target().sizeof)
     num_bins = int(bins.type.sizeof / bins.type.target().sizeof)
     fd_field_offset = 2 * size_t_size
 
-    print(underline(yellow('Fastbins')))
+    print(underline(yellow('fastbins')))
     for i in range(num_fastbins):
         size = 2 * size_t_size * (i + 1)
         chain = pwndbg.chain.format(int(fastbins[i]), offset=fd_field_offset)
-        print(bold(size) + ': ' + chain)
+        print((bold(size) + ': ').ljust(13) + chain)
 
     # TODO: Print other bins
 
 @pwndbg.commands.ParsedCommand
 @pwndbg.commands.OnlyWhenRunning
 def top_chunk(addr=None):
+    """
+    Prints out the address of the top chunk of the main arena, or of the arena
+    at the specified address.
+    """
     main_arena = get_main_arena(addr)
     if main_arena == None:
         heap_start, heap_end = get_heap_bounds()
@@ -115,7 +131,7 @@ def top_chunk(addr=None):
         last_addr = None
         addr = heap_start
         while addr < heap_end:
-            chunk = gdb.parse_and_eval('(struct malloc_chunk)*' + hex(addr))
+            chunk = value_from_type('struct malloc_chunk', addr)
             size = int(chunk['size'])
 
             # Clear the bottom 3 bits
@@ -130,10 +146,13 @@ def top_chunk(addr=None):
 @pwndbg.commands.ParsedCommand
 @pwndbg.commands.OnlyWhenRunning
 def malloc_chunk(addr):
+    """
+    Prints out the malloc_chunk at the specified address.
+    """
     if not isinstance(addr, int):
         addr = int(addr)
 
-    chunk = gdb.parse_and_eval('(struct malloc_chunk)*' + hex(addr))
+    chunk = value_from_type('struct malloc_chunk', addr)
     size = int(chunk['size'])
     prev_inuse = (size & PREV_INUSE) == 1
     is_mmaped = (size & IS_MMAPED) == 1
