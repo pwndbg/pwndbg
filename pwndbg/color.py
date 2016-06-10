58ccfef946d11063bda9f2b5bf645f96d425012b
Zach Riggle <zachriggle@users.noreply.github.com>
2016-06-10 12:57:28 -0600

    Add configurable color points (#63)

--- pwndbg/color.py
+++ pwndbg/color.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
+import functools
 import gdb
+import pwndbg.config
 import pwndbg.enhance
+import pwndbg.memoize
 import pwndbg.vmmap
 
 NORMAL         = "\x1b[0m"
@@ -15,10 +18,11 @@ GREY = GRAY    = "\x1b[90m"
 BOLD           = "\x1b[1m"
 UNDERLINE      = "\x1b[4m"
 
-STACK = YELLOW
-HEAP  = BLUE
-CODE  = RED
-DATA  = PURPLE
+pwndbg.config.Parameter('color-stack', 'yellow', 'color for stack memory')
+pwndbg.config.Parameter('color-heap', 'blue', 'color for heap memory')
+pwndbg.config.Parameter('color-code', 'red', 'color for executable memory')
+pwndbg.config.Parameter('color-data', 'purple', 'color for all other writable memory')
+pwndbg.config.Parameter('color-rwx', 'underline', 'color added to all RWX memory')
 
 def normal(x): return NORMAL + str(x)
 def bold(x): return BOLD + str(x) + NORMAL
@@ -27,7 +31,35 @@ def blue(x): return BLUE + str(x) + NORMAL
 def gray(x): return GRAY + str(x) + NORMAL
 def green(x): return GREEN + str(x) + NORMAL
 def yellow(x): return YELLOW + str(x) + NORMAL
+def purple(x): return PURPLE + str(x) + NORMAL
 def underline(x): return UNDERLINE + str(x) + NORMAL
+
+@pwndbg.memoize.reset_on_stop
+def generateColorFunctionInner(old, new):
+    def wrapper(text):
+        return new(old(text))
+    return wrapper
+
+def generateColorFunction(config):
+    function = lambda x: x
+    for color in str(config).split(','):
+        function = generateColorFunctionInner(function, globals()[color.lower()])
+    return function
+
+def stack(x):
+    return generateColorFunction(pwndbg.config.color_stack)(x)
+
+def heap(x):
+    return generateColorFunction(pwndbg.config.color_heap)(x)
+
+def code(x):
+    return generateColorFunction(pwndbg.config.color_code)(x)
+
+def data(x):
+    return generateColorFunction(pwndbg.config.color_data)(x)
+
+def rwx(x):
+    return generateColorFunction(pwndbg.config.color_rwx)(x)
 
 def get(address, text = None):
     """
@@ -42,32 +74,29 @@ def get(address, text = None):
 
     page = pwndbg.vmmap.find(int(address))
 
-    if page is None:                 color = NORMAL
-    elif '[stack' in page.objfile:   color = STACK
-    elif '[heap'  in page.objfile:   color = HEAP
-    elif page.execute:               color = CODE
-    elif page.rw:                    color = DATA
-    else:                            color = NORMAL
+    if page is None:                 color = normal
+    elif '[stack' in page.objfile:   color = stack
+    elif '[heap'  in page.objfile:   color = heap
+    elif page.execute:               color = code
+    elif page.rw:                    color = data
+    else:                            color = normal
 
     if page and page.rwx:
-        color = color + UNDERLINE
+        color = lambda x: rwx(color(x))
 
     if text is None and isinstance(address, (long, int)) and address > 255:
         text = hex(int(address))
     if text is None:
         text = str(int(address))
 
-    if color == NORMAL:
-        return text
-
-    return "%s%s%s" % (color, text, NORMAL)
+    return color(text)
 
 def legend():
     return 'LEGEND: ' + ' | '.join((
-        STACK + 'STACK' + NORMAL,
-        HEAP + 'HEAP' + NORMAL,
-        CODE + 'CODE' + NORMAL,
-        DATA + 'DATA' + NORMAL,
-        UNDERLINE + 'RWX' + NORMAL,
+        stack('STACK'),
+        heap('HEAP'),
+        code('CODE'),
+        data('DATA'),
+        rwx('RWX'),
         'RODATA'
     ))
