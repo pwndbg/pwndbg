ce6723c4315c9d09efb9dc302a5ee9f6bfb32f95
Dominik Czarnota <dominik.b.czarnota@gmail.com>
2016-06-27 19:30:08 +0200

    Fix testLoadsWithoutCrashing message assertion

--- tests/testLoadsWithoutCrashing.py
+++ tests/testLoadsWithoutCrashing.py
@@ -6,4 +6,4 @@ from . import common
 
 def test_loads_wivout_crashing_bruv():
     output = common.run_gdb_with_script()
-    assert 'Type pwndbg for a list' in output, output
+    assert 'Type pwndbg [filter] for a list.' in output, output
