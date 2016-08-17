00150efce7ad89db3f66aeab2307faec86d17cff
Zach Riggle <riggle@google.com>
2016-08-17 11:04:10 -0700

    Use floor division operator
    Fixes #100

--- pwndbg/commands/misc.py
+++ pwndbg/commands/misc.py
@@ -68,4 +68,4 @@ def distance(a, b):
 
     distance = (b-a)
 
-    print("%#x->%#x is %#x bytes (%#x words)" % (a, b, distance, distance / _arch.ptrsize))
+    print("%#x->%#x is %#x bytes (%#x words)" % (a, b, distance, distance // _arch.ptrsize))
