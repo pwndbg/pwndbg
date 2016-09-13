40869c352f415240f2f3b7d6c482c700f9f9ba06
Zach Riggle <riggle@google.com>
2016-09-12 18:57:37 -0700

    Fix tag script for OSX

--- tag_release.sh
+++ tag_release.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+DATE=$(which gdate || which date)
+
 git fetch --all
 
-TAG=$(date -d "$(git log --tags -1 --format='%aD')" '+%Y.%m.%d')
+TAG=$($DATE -d "$(git log --tags -1 --format='%aD')" '+%Y.%m.%d')
 
 if git tag $TAG origin/master; then
     git push origin $TAG
