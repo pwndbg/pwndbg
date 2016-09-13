65e5037cf091e7287d2e232239bf35a4a30bdcb8
Zach Riggle <riggle@google.com>
2016-09-12 19:02:13 -0700

    Use annotated tags

--- tag_release.sh
+++ tag_release.sh
@@ -6,7 +6,7 @@ git fetch --all
 
 TAG=$($DATE -d "$(git log --tags -1 --format='%aD')" '+%Y.%m.%d')
 
-if git tag $TAG origin/master; then
+if git tag -a $TAG -m "Add release $TAG" origin/master; then
     git push origin $TAG
 fi
 
