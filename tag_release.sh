561e0d3734a5034f87196c4409f8df2a3b8a41e8
Zach Riggle <riggle@google.com>
2016-09-22 14:45:13 -0700

    Fix script

--- tag_release.sh
+++ tag_release.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -ex
+
 DATE=$(which gdate || which date)
 
 git fetch --all
 
-TAG=$($DATE -d "$(git log --tags -1 --format='%aD')" '+%Y.%m.%d')
+TAG=$($DATE '+%Y.%m.%d')
 
 if git tag -a $TAG -m "Add release $TAG" origin/master; then
     git push origin $TAG
