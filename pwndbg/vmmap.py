pwndbg/pwndbg

commit 363ffcaea78627488b6a03ba55829461b3295a8a

Mark PC as executable upon resuming execution
Fixes #90

pwndbg/vmmap.py
@@ -10,6 +10,7 @@ system has /proc/$$/maps, which backs 'info proc mapping'.
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os
 import sys
 
 import gdb
@@ -381,3 +382,9 @@ def check_aslr():
         vmmap.aslr = True
 
     return vmmap.aslr
+
+@pwndbg.events.cont
+def mark_pc_as_executable():
+    mapping = find(pwndbg.regs.pc)
+    if not mapping.execute:
+        mapping.flags |= os.X_OK

pwndbg/memory.py
@@ -185,9 +185,9 @@ class Page(object):
     @property
     def permstr(self):
         flags = self.flags
-        return ''.join(['r' if flags & 4 else '-',
-                        'w' if flags & 2 else '-',
-                        'x' if flags & 1 else '-',
+        return ''.join(['r' if flags & os.R_OK else '-',
+                        'w' if flags & os.W_OK else '-',
+                        'x' if flags & os.X_OK else '-',
                         'p'])
     def __str__(self):
         width = 2 + 2*pwndbg.typeinfo.ptrsize

commit b36b3ff9311c68e8cbc4b53598b30c066964dbd2

Fix issue introduced by 363ffcae

pwndbg/vmmap.py
@@ -391,5 +391,5 @@ def check_aslr():
 @pwndbg.events.cont
 def mark_pc_as_executable():
     mapping = find(pwndbg.regs.pc)
-    if not mapping.execute:
+    if mapping and not mapping.execute:
         mapping.flags |= os.X_OK