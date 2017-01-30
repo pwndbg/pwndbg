pwndbg/pwndbg

commit 2bfe549b22e3dbaa2da168e547250f7a861ee02d

Add libc6-dbg to installed packages

setup.sh
@@ -3,7 +3,11 @@ set -ex
 
 if uname | grep -i Linux &>/dev/null; then
     sudo apt-get update || true
-    sudo apt-get -y install python-dev python3-dev python-pip python3-pip libglib2.0-dev
+    sudo apt-get -y install python-dev python3-dev python-pip python3-pip libglib2.0-dev libc6-dbg
+
+    if uname -m | grep x86_64 > /dev/null; then
+        sudo apt-get install libc6-dbg:i386
+    done
 fi
 
 # Update all submodules

commit 95f7a1d94ce15f3b6765cf6732bd9590603ee0b5

Fix CI regression introduced in 2bfe549
- Typo
- Travis does not have libc6:i386

setup.sh
@@ -6,8 +6,8 @@ if uname | grep -i Linux &>/dev/null; then
     sudo apt-get -y install python-dev python3-dev python-pip python3-pip libglib2.0-dev libc6-dbg
 
     if uname -m | grep x86_64 > /dev/null; then
-        sudo apt-get install libc6-dbg:i386
-    done
+        sudo apt-get install libc6-dbg:i386 || true
+    fi
 fi
 
 # Update all submodules