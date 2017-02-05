5346cf9a0a052472bef94c5f1ad552fc46e44b4e
Zach Riggle <riggle@google.com>
2017-02-05 14:05:52 -0500

    Add an error message when PC is invalid
    Closes #160

--- pwndbg/commands/nearpc.py
+++ pwndbg/commands/nearpc.py
@@ -9,6 +9,7 @@ import gdb
 from capstone import *
 
 import pwndbg.arguments
+import pwndbg.color
 import pwndbg.color.context as C
 import pwndbg.color.disasm as D
 import pwndbg.color.nearpc as N
@@ -54,6 +55,10 @@ def nearpc(pc=None, lines=None, to_string=False, emulate=False):
 
     pc    = int(pc)
     lines = int(lines)
+
+    # Check whether we can even read this address
+    if not pwndbg.memory.peek(address):
+        return [pwndbg.color.red('Invalid address %#x' % address)]
 
     # # Load source data if it's available
     # pc_to_linenos = collections.defaultdict(lambda: [])
