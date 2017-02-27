a9eb332a93de74b93d855ac889b90b22a3fd51ea
Zach Riggle <riggle@google.com>
2017-02-27 18:17:26 -0500

    Add basic tests using XMLRPC server

--- tests/common.py
+++ tests/common.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import atexit
+import functools
 import subprocess
+import sys
 import tempfile
+import time
 import unittest
 
+from . import xmlhacks
 
 def pywrite(data):
     return write(data, suffix='.py')
@@ -15,7 +20,7 @@ def write(data, suffix=''):
     t.write(data.encode('utf-8'))
     return t
 
-def run_gdb_with_script(pybefore='', pyafter=''):
+def gdb_with_script(pybefore='', pyafter=''):
     command = ['gdb','--silent','--nx','--nh']
 
     if pybefore:
@@ -27,5 +32,17 @@ def run_gdb_with_script(pybefore='', pyafter=''):
         command += ['--command', pywrite(pyafter).name]
 
     command += ['--eval-command', 'quit']
+    return command
 
+def run_gdb_with_script(*a, **kw):
+    command = gdb_with_script(*a, **kw)
     return subprocess.check_output(command, stderr=subprocess.STDOUT)
+
+def run_with_server():
+    command = gdb_with_script(pyafter='import pwndbg;\npwndbg.server.serve()')
+    return subprocess.Popen(command, stdout=subprocess.PIPE)
+
+gdb_with_server = run_with_server()
+server = xmlhacks.xmlrpclib.ServerProxy('http://127.0.0.1:8889', verbose=1)
+
+atexit.register(lambda: gdb_with_server.kill())
---
+++ tests/xmlhacks.py
@@ -0,0 +1,1 @@
+../pwndbg/xmlhacks.py
