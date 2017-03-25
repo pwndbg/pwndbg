198606fc2add7c8adadfca0aac900f32b8258ec7
ysf <ich@youssef.de>
2017-03-25 02:04:17 +0100

    setup.sh installs gdb and git if not installed

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -ex
 
 if uname | grep -i Linux &>/dev/null; then
     sudo apt-get update || true
-    sudo apt-get -y install python-dev python3-dev python-pip python3-pip libglib2.0-dev libc6-dbg
+    sudo apt-get -y install git gdb python-dev python3-dev python-pip python3-pip libglib2.0-dev libc6-dbg
 
     if uname -m | grep x86_64 > /dev/null; then
         sudo apt-get install libc6-dbg:i386 || true
