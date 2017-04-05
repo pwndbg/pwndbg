2d9b6eac21c284d3baa2a92497ae4ecf7b78cc63
Zach Riggle <riggle@google.com>
2017-04-05 18:59:33 -0500

    Allow installation to the user site-packages for Darwin, since SIP prevents installation to the system site-packages

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -ex
 
+PYTHON=''
+INSTALLFLAGS=''
+
+if [ "$1" == "--user" ] || (uname | grep -i Darwin &>/dev/null); then
+    INSTALLFLAGS="--user"
+else
+    PYTHON="sudo "
+fi
+
 if uname | grep -i Linux &>/dev/null; then
     sudo apt-get update || true
     sudo apt-get -y install gdb python-dev python3-dev python-pip python3-pip libglib2.0-dev libc6-dbg
@@ -20,8 +29,23 @@ git submodule update --init --recursive
 
 # Find the Python version used by GDB.
 PYVER=$(gdb -batch -q --nx -ex 'pi import platform; print(".".join(platform.python_version_tuple()[:2]))')
-PYTHON=$(gdb -batch -q --nx -ex 'pi import sys; print(sys.executable)')
-PYTHON="${PYTHON}${PYVER}"
+PYTHON+=$(gdb -batch -q --nx -ex 'pi import sys; print(sys.executable)')
+PYTHON+="${PYVER}"
+
+# Find the Python site-packages that we need to use so that
+# GDB can find the files once we've installed them.
+if (uname | grep -i Linux &>/dev/null) && [ -z "$INSTALLFLAGS" ]; then
+    SITE_PACKAGES=$(gdb -batch -q --nx -ex 'pi import site; print(site.getsitepackages()[0])')
+    INSTALLFLAGS="--target ${SITE_PACKAGES}"
+fi
+
+# Make sure that pip is available
+if ! ${PYTHON} -m pip -V; then
+    ${PYTHON} -m ensurepip --upgrade
+fi
+
+# Upgrade pip itself
+${PYTHON} -m pip install ${INSTALLFLAGS} --upgrade pip
 
 # Find the Python site-packages that we need to use so that
 # GDB can find the files once we've installed them.
@@ -36,7 +60,7 @@ fi
 sudo ${PYTHON} -m pip install --upgrade pip
 
 # Install Python dependencies
-sudo ${PYTHON} -m pip install --target ${SITE_PACKAGES} -Ur requirements.txt
+${PYTHON} -m pip install ${INSTALLFLAGS} -Ur requirements.txt
 
 # Load Pwndbg into GDB on every launch.
 if ! grep pwndbg ~/.gdbinit &>/dev/null; then
