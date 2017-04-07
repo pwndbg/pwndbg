202e28f279330dd5a73841beb57eb34205995e1f
cloud <cloudburst@users.noreply.github.com>
2017-04-07 11:48:25 -0700

    fix bash syntax error

--- setup.sh
+++ setup.sh
@@ -39,8 +39,8 @@ git submodule update --init --recursive
 
 # Find the Python version used by GDB.
 PYVER=$(gdb -batch -q --nx -ex 'pi import platform; print(".".join(platform.python_version_tuple()[:2]))')
-PYTHON+=$(gdb -batch -q --nx -ex 'pi import sys; print(sys.executable)')
-PYTHON+="${PYVER}"
+PYTHON=$(gdb -batch -q --nx -ex 'pi import sys; print(sys.executable)')
+PYTHON="${PYTHON}${PYVER}"
 
 # Find the Python site-packages that we need to use so that
 # GDB can find the files once we've installed them.
