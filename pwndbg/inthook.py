pwndbg/pwndbg

commit b565ff8fc0b180ac76a891f25bb31b8e0d003fcf

Fix big with enums OR operation in Python 3 (#222)
The bug can be reproduced using such code:
```
pwndbg> py import re; re.DOTALL | re.MULTILINE
Traceback (most recent call last):
  File "<string>", line 1, in <module>
  File "/usr/lib/python3.6/enum.py", line 794, in __or__
    result = self.__class__(self._value_ | self.__class__(other)._value_)
  File "/usr/lib/python3.6/enum.py", line 291, in __call__
    return cls.__new__(cls, value)
  File "/usr/lib/python3.6/enum.py", line 533, in __new__
    return cls._missing_(value)
  File "/usr/lib/python3.6/enum.py", line 760, in _missing_
    new_member = cls._create_pseudo_member_(value)
  File "/usr/lib/python3.6/enum.py", line 786, in _create_pseudo_member_
    pseudo_member._name_ = None
AttributeError: 'int' object has no attribute '_name_'
Error while executing Python code.
```

The fix seems to work with Python 2 as well:
http://asciinema.org/a/czopcwobjwt2dk9vzif0zk49u

pwndbg/inthook.py
@@ -18,17 +18,19 @@ from future.utils import with_metaclass
 
 import pwndbg.typeinfo
 
-if sys.version_info < (3,0):
+if sys.version_info < (3, 0):
     import __builtin__ as builtins
 else:
     import builtins
 
 _int = builtins.int
 
+
 # We need this class to get isinstance(7, xint) to return True
 class IsAnInt(type):
     def __instancecheck__(self, other):
         return isinstance(other, _int)
+
 
 class xint(with_metaclass(IsAnInt, builtins.int)):
     def __new__(cls, value, *a, **kw):
@@ -37,11 +39,16 @@ class xint(with_metaclass(IsAnInt, builtins.int)):
                 value = value.cast(pwndbg.typeinfo.ulong)
             else:
                 value = value.cast(pwndbg.typeinfo.long)
-        if isinstance(value, gdb.Symbol):
+
+        elif isinstance(value, gdb.Symbol):
             symbol = value
-            value  = symbol.value()
+            value = symbol.value()
             if symbol.is_function:
                 value = value.cast(pwndbg.typeinfo.ulong)
+
+        else:
+            return _int.__new__(cls, value, *a, **kw)
+
         return _int(_int(value, *a, **kw))
 
 # Do not hook 'int' if we are just generating documentation
@@ -49,6 +56,6 @@ if os.environ.get('SPHINX', None) is None:
     builtins.int = xint
     globals()['int'] = xint
 
-    if sys.version_info >= (3,0):
+    if sys.version_info >= (3, 0):
         builtins.long = xint
         globals()['long'] = xint

commit 7fd9aa384d0782347aaf134fa78814262d1974f5

Fix IDA synchronization marshalling xint issue
Because of recent changes, the IDA synchronization fails on marshalling a xint instance:
```
$ gdb -q ./babyuse
Pwndbg successfully connected to Ida Pro xmlrpc: http://127.0.0.1:8888
Loaded 108 commands.  Type pwndbg [filter] for a list.
Reading symbols from ./babyuse...(no debugging symbols found)...done.
pwndbg> entry
Temporary breakpoint 1 at 0x56555ca0
Exception during func=pwndbg.ida.Auto_Color_PC (<gdb.BreakpointEvent object at 0x7f1648274468>,)
Traceback (most recent call last):
  File "/usr/lib/python3.6/xmlrpc/client.py", line 510, in __dump
    f = self.dispatch[type(value)]
KeyError: <class 'pwndbg.inthook.xint'>

During handling of the above exception, another exception occurred:

Traceback (most recent call last):
  File "/home/dc/installed/pwndbg/pwndbg/events.py", line 122, in caller
    func()
  File "/home/dc/installed/pwndbg/pwndbg/ida.py", line 74, in __call__
    return self.fn(*args, **kwargs)
  File "/home/dc/installed/pwndbg/pwndbg/ida.py", line 253, in Auto_Color_PC
    SetColor(colored_pc, 0x7f7fff)
  File "/home/dc/installed/pwndbg/pwndbg/ida.py", line 74, in __call__
    return self.fn(*args, **kwargs)
  File "/home/dc/installed/pwndbg/pwndbg/ida.py", line 87, in wrapper
    return function(l2r(address), *args, **kwargs)
  File "/home/dc/installed/pwndbg/pwndbg/ida.py", line 109, in l2r
    result = (addr - int(exe.address) + base()) & pwndbg.arch.ptrmask
  File "/home/dc/installed/pwndbg/pwndbg/memoize.py", line 47, in __call__
    value = self.func(*args, **kwargs)
  File "/home/dc/installed/pwndbg/pwndbg/ida.py", line 131, in base
    base = _ida.get_fileregion_offset(segaddr)
  File "/usr/lib/python3.6/xmlrpc/client.py", line 1112, in __call__
    return self.__send(self.__name, args)
  File "/usr/lib/python3.6/xmlrpc/client.py", line 1446, in __request
    allow_none=self.__allow_none).encode(self.__encoding, 'xmlcharrefreplace')
  File "/usr/lib/python3.6/xmlrpc/client.py", line 971, in dumps
    data = m.dumps(params)
  File "/usr/lib/python3.6/xmlrpc/client.py", line 502, in dumps
    dump(v, write)
  File "/usr/lib/python3.6/xmlrpc/client.py", line 520, in __dump
    raise TypeError("cannot marshal %s objects" % type(value))
TypeError: cannot marshal <class 'pwndbg.inthook.xint'> objects
Python Exception <class 'TypeError'> cannot marshal <class 'pwndbg.inthook.xint'> objects:

Temporary breakpoint 1, 0x56555ca0 in ?? ()
Exception during func=pwndbg.ida.Auto_Color_PC ()
Traceback (most recent call last):
  File "/usr/lib/python3.6/xmlrpc/client.py", line 510, in __dump
    f = self.dispatch[type(value)]
KeyError: <class 'pwndbg.inthook.xint'>

During handling of the above exception, another exception occurred:

Traceback (most recent call last):
  File "/home/dc/installed/pwndbg/pwndbg/events.py", line 122, in caller
    func()
  File "/home/dc/installed/pwndbg/pwndbg/ida.py", line 74, in __call__
    return self.fn(*args, **kwargs)
  File "/home/dc/installed/pwndbg/pwndbg/ida.py", line 253, in Auto_Color_PC
    SetColor(colored_pc, 0x7f7fff)
  File "/home/dc/installed/pwndbg/pwndbg/ida.py", line 74, in __call__
    return self.fn(*args, **kwargs)
  File "/home/dc/installed/pwndbg/pwndbg/ida.py", line 87, in wrapper
    return function(l2r(address), *args, **kwargs)
  File "/home/dc/installed/pwndbg/pwndbg/ida.py", line 109, in l2r
    result = (addr - int(exe.address) + base()) & pwndbg.arch.ptrmask
  File "/home/dc/installed/pwndbg/pwndbg/memoize.py", line 47, in __call__
    value = self.func(*args, **kwargs)
  File "/home/dc/installed/pwndbg/pwndbg/ida.py", line 131, in base
    base = _ida.get_fileregion_offset(segaddr)
  File "/usr/lib/python3.6/xmlrpc/client.py", line 1112, in __call__
    return self.__send(self.__name, args)
  File "/usr/lib/python3.6/xmlrpc/client.py", line 1446, in __request
    allow_none=self.__allow_none).encode(self.__encoding, 'xmlcharrefreplace')
  File "/usr/lib/python3.6/xmlrpc/client.py", line 971, in dumps
    data = m.dumps(params)
  File "/usr/lib/python3.6/xmlrpc/client.py", line 502, in dumps
    dump(v, write)
  File "/usr/lib/python3.6/xmlrpc/client.py", line 520, in __dump
    raise TypeError("cannot marshal %s objects" % type(value))
TypeError: cannot marshal <class 'pwndbg.inthook.xint'> objects
Python Exception <class 'TypeError'> cannot marshal <class 'pwndbg.inthook.xint'> objects:
```

This patch fixes bug introduced in https://github.com/pwndbg/pwndbg/pull/222

pwndbg/inthook.py
@@ -14,6 +14,7 @@ import os
 import sys
 
 import gdb
+import six
 from future.utils import with_metaclass
 
 import pwndbg.typeinfo
@@ -46,7 +47,7 @@ class xint(with_metaclass(IsAnInt, builtins.int)):
             if symbol.is_function:
                 value = value.cast(pwndbg.typeinfo.ulong)
 
-        else:
+        elif not isinstance(value, six.string_types):
             return _int.__new__(cls, value, *a, **kw)
 
         return _int(_int(value, *a, **kw))