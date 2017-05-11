386735c8a8860566d62003a489cbd5ce702f9840
disconnect3d <dominik.b.czarnota@gmail.com>
2017-05-11 02:49:56 +0200

    Mini refactor: commands/auxv.py

--- pwndbg/commands/auxv.py
+++ pwndbg/commands/auxv.py
@@ -5,7 +5,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import gdb
 import six
 
 import pwndbg.auxv
@@ -19,6 +18,6 @@ def auxv():
     """
     Print information from the Auxiliary ELF Vector.
     """
-    for k,v in sorted(pwndbg.auxv.get().items()):
+    for k, v in sorted(pwndbg.auxv.get().items()):
         if v is not None:
             print(k.ljust(24), v if not isinstance(v, six.integer_types) else pwndbg.chain.format(v))
