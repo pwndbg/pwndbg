25ab700233d954244f6b88877fc58b21e23c0586
degrigis <degrigis@gmail.com>
2017-05-12 22:10:17 +0200

    removed unused import
    Signed-off-by: degrigis <degrigis@gmail.com>

--- pwndbg/commands/checksec.py
+++ pwndbg/commands/checksec.py
@@ -5,8 +5,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import subprocess
-
 import pwndbg.commands
 import pwndbg.which
 import pwndbg.wrappers
