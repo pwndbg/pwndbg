01cf5fd3745e4d172da20bebaaed41a4c8f76d4b
Zach Riggle <riggle@google.com>
2017-05-15 12:58:35 -0500

    Documentation, hook pdb.set_trace()

--- pwndbg/exception.py
+++ pwndbg/exception.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import functools
 import pdb
 import sys
 import traceback
@@ -22,6 +23,13 @@ verbose = pwndbg.config.Parameter('exception-verbose', False, 'whether to print 
 debug = pwndbg.config.Parameter('exception-debugger', False, 'whether to debug exceptions raised in Pwndbg commands')
 
 def handle():
+    """Displays an exception to the user, optionally displaying a full traceback
+    and spawning an interactive post-moretem debugger.
+
+    Notes:
+        - ``set exception-verbose on`` enables stack traces.
+        - ``set exception-debugger on`` enables the post-mortem debugger.
+    """
     # Display the error
     if debug or verbose:
         print(traceback.format_exc())
@@ -33,3 +41,16 @@ def handle():
     if debug:
         with pwndbg.stdio.stdio:
             pdb.post_mortem()
+
+
+
+pdb_set_trace = pdb.set_trace
+
+@functools.wraps(pdb_set_trace)
+def set_trace():
+    """Enable sane debugging in Pwndbg by switching to the "real" stdio.
+    """
+    with pwndbg.stdio.stdio:
+        pdb_set_trace()
+
+pdb.set_trace = set_trace()
