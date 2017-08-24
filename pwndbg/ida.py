124e997555629cb76eb9db7a215e0bb1f1f894a3
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2017-08-24 11:55:49 +0200

    Fix hard error when something else (not IDA) listens on IDA's port (#330)
    * Fix hard error when something else (not IDA) listens on IDA's port
    
    The default IDA port is 8888 and it can happen that some other program (such as
    a jupyter notebook) is listening on that address. This made pwndbg unusable,
    because it would crash trying to connect to IDA.
    
    * add timeout to ida connect

--- pwndbg/ida.py
+++ pwndbg/ida.py
@@ -13,6 +13,7 @@ from __future__ import unicode_literals
 import errno
 import functools
 import socket
+import sys
 import traceback
 
 import gdb
@@ -45,21 +46,39 @@ xmlrpclib.Marshaller.dispatch[type(0)] = lambda _, v, w: w("<value><i8>%d</i8></
 
 _ida = None
 
+# to avoid printing the same exception multiple times, we store the last exception here
+_ida_last_exception = None
+
 
 @pwndbg.config.Trigger([ida_rpc_host, ida_rpc_port])
 def init_ida_rpc_client():
-    global _ida
+    global _ida, _ida_last_exception
     addr = 'http://{host}:{port}'.format(host=ida_rpc_host, port=ida_rpc_port)
 
     _ida = xmlrpclib.ServerProxy(addr)
+    socket.setdefaulttimeout(3)
 
+    exception = None # (type, value, traceback)
     try:
         _ida.here()
         print(pwndbg.color.green("Pwndbg successfully connected to Ida Pro xmlrpc: %s" % addr))
     except socket.error as e:
         if e.errno != errno.ECONNREFUSED:
-            traceback.print_exc()
+            exception = sys.exc_info()
         _ida = None
+    except socket.timeout:
+        exception = sys.exc_info()
+        _ida = None
+    except xmlrpclib.ProtocolError:
+        exception = sys.exc_info()
+        _ida = None
+
+    if exception:
+        if not isinstance(_ida_last_exception, exception[0]) or _ida_last_exception.args != exception[1].args:
+            print(pwndbg.color.yellow("[!] Ida Pro xmlrpc error"))
+            traceback.print_exception(*exception)
+
+    _ida_last_exception = exception and exception[1]
 
 
 class withIDA(object):
