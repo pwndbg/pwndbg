f60ee6d9dfb7cd40ef7634ff8a604d074ed70f26
Zach Riggle <riggle@google.com>
2017-09-11 14:35:06 -0500

    Fix Python<=2.7.6 "TypeError: Struct() argument 1 must be string, not unicode"
    Additional information is available here: http://python-future.org/stdlib_incompatibilities.html#struct-pack

--- pwndbg/heap/libheap.py
+++ pwndbg/heap/libheap.py
@@ -26,7 +26,6 @@ SOFTWARE.
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from __future__ import unicode_literals
 
 import struct
 import sys
