7e667cae51443678ae64e87cdf065c7ef5b3fcb3
disconnect3d <dominik.b.czarnota@gmail.com>
2017-12-17 08:17:34 +0100

    Fix isort, e2e tests, add pytest requirement

--- e2e_tests/test_loads.py
+++ e2e_tests/test_loads.py
@@ -9,7 +9,4 @@ from . import common
 def test_loads_without_crashing():
     output = common.run_gdb_with_script()
 
-    assert output.endswith(
-        'pwndbg: loaded 113 commands. Type pwndbg [filter] for a list.\n'
-        'pwndbg: created $rebase, $ida gdb functions (can be used with print/break)\n'
-    ), output
+    assert output.endswith('Loaded 113 commands. Type pwndbg [filter] for a list.\n'), output
--- pwndbg/tests/test_misc.py
+++ pwndbg/tests/test_misc.py
@@ -1,3 +1,8 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 import pytest
 
 from pwndbg.services.misc import pwndbg_list_and_filter_commands
