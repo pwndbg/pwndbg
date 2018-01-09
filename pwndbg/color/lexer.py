019b65efff112b79f813268df87d8fe59c0bedc6
MengHuan Yu <cebrusfs@gmail.com>
2018-01-09 17:41:24 +0800

    Syntax-highlight: Update lexer for supporting ARM
    Support symbol, constant, comments

--- pwndbg/color/lexer.py
+++ pwndbg/color/lexer.py
@@ -7,7 +7,6 @@ import re
 
 import six
 from pygments.lexer import RegexLexer
-from pygments.lexer import bygroups
 from pygments.lexer import include
 from pygments.token import Comment
 from pygments.token import Name
@@ -18,25 +17,18 @@ from pygments.token import Punctuation
 from pygments.token import String
 from pygments.token import Text
 
+import pwndbg.compat
+
 __all__ = ['PwntoolsLexer']
 
-# Text        Token.Text            for any type of text data
-# Whitespace  Token.Text.Whitespace for specially highlighted whitespace
-# Error       Token.Error           represents lexer errors
-# Other       Token.Other           special token for data not matched by a parser (e.g. HTML markup in PHP code)
-# Keyword     Token.Keyword         any kind of keywords
-# Name        Token.Name            variable/function names
-# Literal     Token.Literal         Any literals
-# String      Token.Literal.String  string literals
-# Number      Token.Literal.Number  number literals
-# Operator    Token.Operator        operators (+, not...)
-# Punctuation Token.Punctuation     punctuation ([, (...)
-# Comment     Token.Comment         any kind of comments
-# Generic     Token.Generic         generic tokens (have a look at the explanation below)
 class PwntoolsLexer(RegexLexer):
     """
-    Fork foom pwntools
+    Fork from pwntools
     https://github.com/Gallopsled/pwntools/blob/7860eecf025135380b137dd9df85dd02a2fd1667/pwnlib/lexer.py
+
+    Edit:
+        * Remove Objdump rules
+        * Merge pygments-arm (https://github.com/heia-fr/pygments-arm)
     """
     name = 'PwntoolsLexer'
     filenames = ['*.s', '*.S', '*.asm']
@@ -47,86 +39,94 @@ class PwntoolsLexer(RegexLexer):
     identifier = r'(?:[a-zA-Z$_]' + char + '*|\.' + char + '+|or)'
     number = r'(?:0[xX][a-zA-Z0-9]+|\d+)'
     memory = r'(?:[\]\[])'
-    bad = r'(?:\(bad\))'
+
+    eol = r'[\r\n]+'
 
     tokens = {
         'root': [
             include('whitespace'),
+
+            # Label
             (identifier + ':', Name.Label),
+            (number + ':', Name.Label),
+
+            # AT&T directive
             (r'\.' + identifier, Name.Attribute, 'directive-args'),
             (r'lock|rep(n?z)?|data\d+', Name.Attribute),
-            (identifier, Name.Function, 'instruction-args'),
-            (r'[\r\n]+', Text),
-            (bad, Text)
-        ],
 
+            # Instructions
+            (identifier, Name.Function, 'instruction-args'),
+
+            (r'[\r\n]+', Text),
+        ],
         'directive-args': [
             (identifier, Name.Constant),
             (string, String),
             ('@' + identifier, Name.Attribute),
             (number, Number.Integer),
-            (r'[\r\n]+', Text, '#pop'),
 
+            (eol, Text, '#pop'),
             (r'#.*?$', Comment, '#pop'),
 
             include('punctuation'),
             include('whitespace')
         ],
         'instruction-args': [
-            # For objdump-disassembled code, shouldn't occur in
-            # actual assembler input
-            ('([a-z0-9]+)( )(<)('+identifier+')(>)',
-                bygroups(Number.Hex, Text, Punctuation, Name.Constant,
-                         Punctuation)),
-            ('([a-z0-9]+)( )(<)('+identifier+')([-+])('+number+')(>)',
-                bygroups(Number.Hex, Text, Punctuation, Name.Constant,
-                         Punctuation, Number.Integer, Punctuation)),
-
             # Fun things
             (r'([\]\[]|BYTE|DWORD|PTR|\+|\-|}|{|\^|>>|<<|&)', Text),
 
             # Address constants
             (identifier, Name.Constant),
+            ('=' + identifier, Name.Constant), # ARM symbol
             (number, Number.Integer),
+
             # Registers
             ('%' + identifier, Name.Variable),
             ('$' + identifier, Name.Variable),
+
             # Numeric constants
-            ('$'+number, Number.Integer),
-            ('#'+number, Number.Integer),
+            ('$' + number, Number.Integer),
+            ('#' + number, Number.Integer),
+
+            # ARM predefined constants
+            ('#' + identifier, Name.Constant),
+
             (r"$'(.|\\')'", String.Char),
-            (r'[\r\n]+', Text, '#pop'),
+
+            (eol, Text, '#pop'),
+
             include('punctuation'),
             include('whitespace')
         ],
         'whitespace': [
             (r'\n', Text),
             (r'\s+', Text),
+
+            # Block comments
+            # /* */ (AT&T)
             (r'/\*.*?\*/', Comment),
-            (r';.*$', Comment)
+
+            # Line comments
+            # //    (AArch64)
+            # #     (AT&T)
+            # ;     (NASM/intel, LLVM)
+            # @     (ARM)
+            (r'(//|[#;@]).*$', Comment.Single)
         ],
         'punctuation': [
             (r'[-*,.():]+', Punctuation)
         ]
     }
 
-    def analyse_text(text):
-        if re.match(r'^\.(text|data|section)', text, re.M):
-            return True
-        elif re.match(r'^\.\w+', text, re.M):
-            return 0.1
-
-if six.PY2:
-    # XXX: convert all unicode() to str() if in Python2.7 since unicode_literals is enabled
-    #   The pygments<=2.2.0 (lastest stable when commit) in Python2.7 use 'str' type in rules matching
-    #   We must convert all unicode back to str()
+# Note: convert all unicode() to str() if in Python2.7 since unicode_literals is enabled
+# The pygments<=2.2.0 (latest stable when commit) in Python2.7 use 'str' type in rules matching
+# We must convert all unicode back to str()
+if pwndbg.compat.python2:
     def _to_str(obj):
-        typ = type(obj)
-        if typ is tuple:
-            return tuple(map(_to_str, obj))
-        elif typ is list:
-            return map(_to_str, obj)
-        elif typ is unicode:
+        type_ = type(obj)
+        if type_ in (tuple, list):
+            return type_(map(_to_str, obj))
+        elif type_ is unicode:
             return str(obj)
         return obj
 
