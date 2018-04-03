2e479d2ad5e606f252d12eab3eb67411dd8510ee
disconnect3d <dominik.b.czarnota@gmail.com>
2018-04-03 18:12:30 +0200

    Fix test_loads_without_crashing

--- e2e_tests/test_loads.py
+++ e2e_tests/test_loads.py
@@ -5,8 +5,15 @@ from __future__ import unicode_literals
 
 from . import common
 
+import re
+
 
 def test_loads_without_crashing():
     output = common.run_gdb_with_script()
 
-    assert output.endswith('Loaded 113 commands. Type pwndbg [filter] for a list.\n'), output
+    output = re.sub(r'loaded [0-9]+ commands', r'loaded ### commands', output)
+
+    assert (
+        'pwndbg: loaded ### commands. Type pwndbg [filter] for a list.\n'
+        'pwndbg: created $rebase, $ida gdb functions (can be used with print/break)\n'
+    ) in output, "The output was: %r" % output
