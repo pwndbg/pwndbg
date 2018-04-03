c096275146083335d5c1911f5b25139d306f6ce7
disconnect3d <dominik.b.czarnota@gmail.com>
2018-04-03 21:15:55 +0200

    Change tests launcher name from unittests to pytests

--- tests.sh
+++ tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PWNDBG_TESTS_DISABLE_COLORS=yes gdb --silent --nx --nh --command gdbinit.py --command unittests_launcher.py
+PWNDBG_TESTS_DISABLE_COLORS=yes gdb --silent --nx --nh --command gdbinit.py --command pytests_launcher.py
