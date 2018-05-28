9d9c4b95f93f41821774c750c510d6a43b179974
ecx86 <ecx86@users.noreply.github.com>
2018-05-28 15:32:56 -0400

    Improve piebase command

--- pwndbg/commands/pie.py
+++ pwndbg/commands/pie.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Command to print the virtual memory map a la /proc/self/maps.
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
 import argparse
-
 import gdb
-import six
-from elftools.elf.constants import SH_FLAGS
-from elftools.elf.elffile import ELFFile
+import os
 
 import pwndbg.commands
-import pwndbg.elf
 import pwndbg.vmmap
 import pwndbg.auxv
 
@@ -34,9 +27,18 @@ def translate_addr(offset, module):
     print("No executeable segments found")
     return
 
+def get_exe_name():
+    module = ''
+    auxv = pwndbg.auxv.get()
+    if 'AT_EXECFN' in auxv:
+        module = auxv['AT_EXECFN']
+    if not module:
+        module = os.readlink("/proc/%d/exe" % (gdb.selected_inferior().pid,))
+    return module
+
 parser = argparse.ArgumentParser()
 parser.description = 'Calculate VA of RVA from PIE base.'
-parser.add_argument('offset', nargs='?', default='0',
+parser.add_argument('offset', nargs='?', default=0,
                     help='Offset from PIE base.')
 parser.add_argument('module', type=str, nargs='?', default='',
                     help='Module to choose as base. Defaults to the target executable.')
@@ -46,14 +48,14 @@ parser.add_argument('module', type=str, nargs='?', default='',
 def piebase(offset=None, module=None):
     offset = int(offset)
     if not module:
-        module = pwndbg.auxv.get()['AT_EXECFN']
+        module = get_exe_name()
     addr = translate_addr(offset, module)
     print(hex(addr))
 
 
 parser = argparse.ArgumentParser()
 parser.description = 'Break at RVA from PIE base.'
-parser.add_argument('offset', nargs='?', default='0',
+parser.add_argument('offset', nargs='?', default=0,
                     help='Offset to add.')
 parser.add_argument('module', type=str, nargs='?', default='',
                     help='Module to choose as base. Defaults to the target executable.')
@@ -63,7 +65,7 @@ parser.add_argument('module', type=str, nargs='?', default='',
 def breakrva(offset=None, module=None):
     offset = int(offset)
     if not module:
-        module = pwndbg.auxv.get()['AT_EXECFN']
+        module = get_exe_name()
     addr = translate_addr(offset, module)
     spec = "*%#x" % (addr)
     gdb.Breakpoint(spec)
@@ -72,4 +74,5 @@ def breakrva(offset=None, module=None):
 @pwndbg.commands.QuietSloppyParsedCommand
 @pwndbg.commands.OnlyWhenRunning
 def brva(map):
+    """Alias for breakrva."""
     return breakrva(map)
