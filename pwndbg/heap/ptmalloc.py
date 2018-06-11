abee30aa390da35ea2f8c857914cbdd085fe71ef
Istvan Kurucsai <pistukem@gmail.com>
2018-06-11 16:03:17 +0200

    Adds comment for HEAP_MAX_SIZE

--- pwndbg/heap/ptmalloc.py
+++ pwndbg/heap/ptmalloc.py
@@ -15,6 +15,8 @@ from pwndbg.color import message
 from pwndbg.constants import ptmalloc
 from pwndbg.heap import heap_chain_limit
 
+# See https://sourceware.org/git/?p=glibc.git;a=blob;f=malloc/arena.c;h=37183cfb6ab5d0735cc82759626670aff3832cd0;hb=086ee48eaeaba871a2300daf85469671cc14c7e9#l30
+# and https://sourceware.org/git/?p=glibc.git;a=blob;f=malloc/malloc.c;h=f8e7250f70f6f26b0acb5901bcc4f6e39a8a52b2;hb=086ee48eaeaba871a2300daf85469671cc14c7e9#l869
 HEAP_MAX_SIZE = 1024 * 1024 if pwndbg.arch.ptrsize == 4 else 2 * 4 * 1024 * 1024 * 8
 
 Arena = namedtuple('Arena', ['addr', 'heaps'])
