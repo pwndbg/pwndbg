5015b16c3c1d491f0bff60db72f8ce4f7f1b8f22
anthraxx <levente@leventepolyak.net>
2018-06-29 01:26:06 +0200

    isort: fix import order to make travis pass

--- pwndbg/heap/ptmalloc.py
+++ pwndbg/heap/ptmalloc.py
@@ -5,7 +5,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from collections import OrderedDict, namedtuple
+from collections import OrderedDict
+from collections import namedtuple
 
 import gdb
 
