ee7086bc5d5e610aef7511f8a184b245f0ba7d2a
disconnect3d <dominik.b.czarnota@gmail.com>
2018-07-08 20:03:59 +0200

    Add test for emulate_disasm_loop

--- tests/test_emulate.py
+++ tests/test_emulate.py
@@ -8,9 +8,10 @@ from pwndbg.commands.nearpc import emulate, nearpc, pdisass
 from pwndbg.commands.windbg import u
 
 EMULATE_DISASM_BINARY = tests.binaries.get('emulate_disasm.out')
+EMULATE_DISASM_LOOP_BINARY = tests.binaries.get('emulate_disasm_loop.out')
 
 
-def test_disasm_commands(start_binary):
+def test_emulate_disasm(start_binary):
     """
     Tests emulate command and its caching behavior
     """
@@ -44,6 +45,46 @@ def test_disasm_commands(start_binary):
         '   0x400090             add    byte ptr [rax], al',
         '   0x400092             add    byte ptr [rax], al',
         '   0x400094             add    byte ptr [rax], al'
+    ]
+
+    assert nearpc(to_string=True) == disasm_without_emu
+    assert emulate(to_string=True, emulate=False) == disasm_without_emu
+    assert pdisass(to_string=True) == disasm_without_emu
+    assert u(to_string=True) == disasm_without_emu
+
+
+def test_emulate_disasm_loop(start_binary):
+    start_binary(EMULATE_DISASM_LOOP_BINARY)
+
+    assert emulate(to_string=True) == [
+        ' ► 0x400080 <_start>       movabs rsi, string                   <0x400094>',
+        '   0x40008a <_start+10>    mov    rdi, rsp',
+        '   0x40008d <_start+13>    mov    ecx, 3',
+        '   0x400092 <_start+18>    rep movsb byte ptr [rdi], byte ptr [rsi]',
+        '    ↓',
+        '   0x400092 <_start+18>    rep movsb byte ptr [rdi], byte ptr [rsi]',
+        '    ↓',
+        '   0x400092 <_start+18>    rep movsb byte ptr [rdi], byte ptr [rsi]',
+        '    ↓',
+        '   0x400092 <_start+18>    rep movsb byte ptr [rdi], byte ptr [rsi]',
+        '   0x400094 <string>       xor    dword ptr [rdx], esi',
+        '   0x400096 <string+2>     xor    esi, dword ptr [rsi]',
+        '   0x40009d                add    byte ptr [rax], al',
+        '   0x40009f                add    byte ptr [rax], al'
+    ]
+
+    disasm_without_emu = [
+        ' ► 0x400080 <_start>       movabs rsi, string                   <0x400094>',
+        '   0x40008a <_start+10>    mov    rdi, rsp',
+        '   0x40008d <_start+13>    mov    ecx, 3',
+        '   0x400092 <_start+18>    rep movsb byte ptr [rdi], byte ptr [rsi]',
+        '   0x400094 <string>       xor    dword ptr [rdx], esi',
+        '   0x400096 <string+2>     xor    esi, dword ptr [rsi]',
+        '   0x40009d                add    byte ptr [rax], al',
+        '   0x40009f                add    byte ptr [rax], al',
+        '   0x4000a1                add    byte ptr [rax], al',
+        '   0x4000a3                add    byte ptr [rax], al',
+        '   0x4000a5                add    byte ptr [rax], al'
     ]
 
     assert nearpc(to_string=True) == disasm_without_emu
