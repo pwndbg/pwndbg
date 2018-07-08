87aa1675995afa800fc2dd18e747910937308294
Disconnect3d <dominik.b.czarnota@gmail.com>
2018-07-08 19:03:03 +0200

    Run each test in a separate GDB session (#498)
    * it would be cool to have tests that run within GDB so that we don't have to parse GDB output and deal with weird problems
    * we can't run all tests in one GDB session as `file x; entry; <some pwndbg command>; file y; entry; <some wndbg command>;` may have different results - it seems either us or GDB fails to cleanup everything properly

--- tests.sh
+++ tests.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-PWNDBG_DISABLE_COLORS=1 gdb --silent --nx --nh --command gdbinit.py --command pytests_launcher.py
+
+# NOTE: We run tests under GDB sessions and because of some cleanup/tests dependencies problems
+# we decided to run each test in a separate GDB session
+TESTS_LIST=$(gdb --silent --nx --nh --command gdbinit.py --command pytests_collect.py | grep -o "tests/.*::.*")
+
+for test_case in ${TESTS_LIST}; do
+    PWNDBG_LAUNCH_TEST="${test_case}" PWNDBG_DISABLE_COLORS=1 gdb --silent --nx --nh --command gdbinit.py --command pytests_launcher.py
+done
