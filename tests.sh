c10af6747b0dbeaa0ff6545de5e53ec28ccb8c1a
disconnect3d <dominik.b.czarnota@gmail.com>
2018-07-08 20:51:19 +0200

    Add --eval-command quit to tests invocation
    This should prevent travis from staying in gdb/stalled build when something fails in weird way (like a file is missing)
    ```
    [+] Building 'emulate_disasm.o'
    make: nasm: Command not found
    make: *** [emulate_disasm.o] Error 127
    gdbinit.py: No such file or directory.
    pytests_collect.py: No such file or directory.
    No output has been received in the last 10m0s, this potentially indicates a stalled build or something wrong with the build itself.
    Check the details on how to adjust your build configuration on: https://docs.travis-ci.com/user/common-build-problems/#Build-times-out-because-no-output-was-received
    ```

--- tests.sh
+++ tests.sh
@@ -4,13 +4,13 @@ cd tests/binaries && make && cd ../..
 
 # NOTE: We run tests under GDB sessions and because of some cleanup/tests dependencies problems
 # we decided to run each test in a separate GDB session
-TESTS_LIST=$(gdb --silent --nx --nh --command gdbinit.py --command pytests_collect.py | grep -o "tests/.*::.*")
+TESTS_LIST=$(gdb --silent --nx --nh --command gdbinit.py --command pytests_collect.py --eval-command quit | grep -o "tests/.*::.*")
 
 tests_passed_or_skipped=0
 tests_failed=0
 
 for test_case in ${TESTS_LIST}; do
-    PWNDBG_LAUNCH_TEST="${test_case}" PWNDBG_DISABLE_COLORS=1 gdb --silent --nx --nh --command gdbinit.py --command pytests_launcher.py
+    PWNDBG_LAUNCH_TEST="${test_case}" PWNDBG_DISABLE_COLORS=1 gdb --silent --nx --nh --command gdbinit.py --command pytests_launcher.py --eval-command quit
 
     exit_status=$?
 
