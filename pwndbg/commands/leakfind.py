ebf2cd00170d9793f4ca63669f31059a40628406
Stuart Nevans Locke <stuartnevlocke@gmail.com>
2019-04-09 10:54:59 -0700

    Remove periods in docs. Also fix example commands.

--- pwndbg/commands/leakfind.py
+++ pwndbg/commands/leakfind.py
@@ -57,15 +57,15 @@ parser = argparse.ArgumentParser()
 parser.description = """
 Attempt to find a leak chain given a starting address. Scans memory near the given address, looks for pointers, 
 and continues that process to attempt to find leaks.\n
-Example: leakfind $rsp filename 0x48 6. This would look for any chains of leaks that point to a section in filename which begin near $rsp, are never 0x48 bytes further from a known pointer, 
-and are a maximum length of 6.\n
+Example: leakfind $rsp --page_name=filename --offset=0x48 --max_depth=6. This would look for any chains of leaks that point to a section in filename which begin near $rsp, are never 0x48 bytes further from a known pointer, 
+and are a maximum length of 6\n
 """
-parser.add_argument("address",help="Starting address to find a leak chain from.")
+parser.add_argument("address",help="Starting address to find a leak chain from")
 parser.add_argument("-p","--page_name",type=str,nargs="?",default=None,help="Substring required to be part of the name of any found pages")
-parser.add_argument("-o","--max_offset",default=0x48,nargs="?",help="Max offset to add to addresses when looking for leak.")
-parser.add_argument("-d","--max_depth",default=0x4,nargs="?",help="Maximum depth to follow pointers to.")
-parser.add_argument("-s","--stride",nargs="?",default=0x1,help="Stride to add to add between pointers considered. For example, if this is 4 it would only consider pointers at an offset divisible by 4 from the starting pointer.")
-parser.add_argument('--negative_offset',nargs="?",default=0x0,help="Max negative offset to search before an address when looking for a leak.")
+parser.add_argument("-o","--max_offset",default=0x48,nargs="?",help="Max offset to add to addresses when looking for leak")
+parser.add_argument("-d","--max_depth",default=0x4,nargs="?",help="Maximum depth to follow pointers to")
+parser.add_argument("-s","--stride",nargs="?",default=0x1,help="Stride to add to add between pointers considered. For example, if this is 4 it would only consider pointers at an offset divisible by 4 from the starting pointer")
+parser.add_argument('--negative_offset',nargs="?",default=0x0,help="Max negative offset to search before an address when looking for a leak")
 @pwndbg.commands.ArgparsedCommand(parser)
 @pwndbg.commands.OnlyWhenRunning
 def leakfind(address=-1,page_name=None,max_offset=0x40,max_depth=0x4,stride=0x1,negative_offset=0x0):
