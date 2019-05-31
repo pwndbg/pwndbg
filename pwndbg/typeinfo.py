ce96491487a1405544902e3a9a1eb7b999b58b72
StalkR <stalkr@stalkr.net>
2019-05-31 07:09:08 +0000

    typeinfo: more types for golang and tests (#652)

--- pwndbg/typeinfo.py
+++ pwndbg/typeinfo.py
@@ -49,20 +49,20 @@ def lookup_types(*types):
 def update():
 
     module.char   = gdb.lookup_type('char')
-    module.ulong  = lookup_types('unsigned long', 'uint', 'u32')
-    module.long   = lookup_types('long', 'int', 'i32')
-    module.uchar  = lookup_types('unsigned char', 'ubyte', 'u8')
+    module.ulong  = lookup_types('unsigned long', 'uint', 'u32', 'uint32')
+    module.long   = lookup_types('long', 'int', 'i32', 'int32')
+    module.uchar  = lookup_types('unsigned char', 'ubyte', 'u8', 'uint8')
     module.ushort = lookup_types('unsigned short', 'ushort', 'u16', 'uint16')
-    module.uint   = lookup_types('unsigned int', 'uint', 'u32')
+    module.uint   = lookup_types('unsigned int', 'uint', 'u32', 'uint32')
     module.void   = lookup_types('void', '()')
     module.uint8  = module.uchar
     module.uint16 = module.ushort
     module.uint32 = module.uint
     module.uint64 = lookup_types('unsigned long long', 'ulong', 'u64', 'uint64')
 
-    module.int8   = lookup_types('char', 'i8')
+    module.int8   = lookup_types('char', 'i8', 'int8')
     module.int16  = lookup_types('short', 'i16', 'int16')
-    module.int32  = lookup_types('int', 'i32')
+    module.int32  = lookup_types('int', 'i32', 'int32')
     module.int64  = lookup_types('long long', 'long', 'i64', 'int64')
 
     module.ssize_t = module.long
