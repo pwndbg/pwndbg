d6c13ecdbefb3171fd1bce1289d16af492e2ebb8
Bet4 <0xbet4@gmail.com>
2019-07-23 11:35:52 +0800

    Change the way to check is_remote

--- pwndbg/remote.py
+++ pwndbg/remote.py
@@ -11,23 +11,9 @@ from __future__ import unicode_literals
 
 import gdb
 
+import pwndbg.memoize
 
+
+@pwndbg.memoize.reset_on_exit
 def is_remote():
-    # N.B.: We cannot use "info program" because of:
-    # https://sourceware.org/bugzilla/show_bug.cgi?id=18335
-    #
-    # return 'serial line' in gdb.execute('info program',to_string=True,)
-    info_file = gdb.execute('info file',to_string=True,from_tty=False)
-
-    # target remote
-    if 'Remote serial target' in info_file:
-        return True
-
-    # target extended-remote
-    if 'Extended remote serial target' in info_file:
-        return True
-
-    if 'Debugging a target over a serial line.' in info_file:
-        return True
-
-    return False
+    return "remote" in gdb.execute("maintenance print target-stack", to_string=True)
