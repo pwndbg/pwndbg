b1beacf07fb13e87d17762c89ec3a3412d842df9
苇子 <i@iweizi.me>
2020-03-19 01:29:33 +0800

    fixes #660,  can not get correct arm64 context (#724)

--- pwndbg/regs.py
+++ pwndbg/regs.py
@@ -286,7 +286,7 @@ class module(ModuleType):
         attr = attr.lstrip('$')
         try:
             # Seriously, gdb? Only accepts uint32.
-            if 'eflags' in attr:
+            if 'eflags' in attr or 'cpsr' in attr:
                 value = gdb77_get_register(attr)
                 value = value.cast(pwndbg.typeinfo.uint32)
             else:
