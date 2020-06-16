dd8f4e30398882e4c99b9dd8e519cbfee0b69f31
Les De Ridder <les@lesderid.net>
2020-06-16 08:20:55 +0200

    Fix typos

--- pwndbg/abi.py
+++ pwndbg/abi.py
@@ -145,10 +145,10 @@ def update():
 
     if not linux:
         msg = M.warn(
-            "The bare metal debugging is enabled since the gdb's osabi is '%s' which is not 'GNU/Linux'.\n"
+            "The bare metal debugging is enabled since gdb's osabi is '%s' which is not 'GNU/Linux'.\n"
             "Ex. the page resolving and memory de-referencing ONLY works on known pages.\n"
-            "This option is based ib gdb client compile arguments (by default) and will be corrected if you load an ELF which has the '.note.ABI-tag' section.\n"
-            "If you are debuging a program that runs on Linux ABI, please select the correct gdb client."
+            "This option is based on gdb client compile arguments (by default) and will be corrected if you load an ELF with a '.note.ABI-tag' section.\n"
+            "If you are debugging a program that runs on the Linux ABI, please select the correct gdb client."
             % abi
         )
         print(msg)
