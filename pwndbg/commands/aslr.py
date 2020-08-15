487caa1e77bfd54f2015f87207b7e50de19a94c5
Disconnect3d <dominik.b.czarnota@gmail.com>
2020-08-15 21:58:10 +0200

    Fix #814: better aslr output (#818)

--- pwndbg/commands/aslr.py
+++ pwndbg/commands/aslr.py
@@ -12,17 +12,16 @@ from pwndbg.color import message
 
 options = {'on':'off', 'off':'on'}
 
-parser = argparse.ArgumentParser(description='Inspect or modify ASLR status')
+parser = argparse.ArgumentParser(description='''
+Check the current ASLR status, or turn it on/off.
+
+Does not take effect until the program is restarted.
+''')
 parser.add_argument('state', nargs='?', type=str, choices=options,
                     help="Turn ASLR on or off (takes effect when target is started)")
 
 @pwndbg.commands.ArgparsedCommand(parser)
 def aslr(state=None):
-    """
-    Check the current ASLR status, or turn it on/off.
-
-    Does not take effect until the program is restarted.
-    """
     if state:
         gdb.execute('set disable-randomization %s' % options[state], 
                     from_tty=False, to_string=True)
@@ -30,10 +29,10 @@ def aslr(state=None):
         if pwndbg.proc.alive:
             print("Change will take effect when the process restarts")
 
-    aslr = pwndbg.vmmap.check_aslr()
+    aslr, method = pwndbg.vmmap.check_aslr()
     status = message.off('OFF')
 
     if aslr:
         status = message.on('ON')
 
-    print("ASLR is %s" % status)
+    print("ASLR is %s (%s)" % (status, method))
