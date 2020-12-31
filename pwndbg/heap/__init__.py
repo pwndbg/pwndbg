5639589f5a2e77a289071ed1708bfd9c7c0ffd48
Bet4 <0xbet4@gmail.com>
2020-12-31 11:32:03 +0800

    Remove unimplemented dlmalloc (#874)

--- pwndbg/heap/__init__.py
+++ pwndbg/heap/__init__.py
@@ -8,18 +8,8 @@ current = None
 
 heap_chain_limit = pwndbg.config.Parameter('heap-dereference-limit', 8, 'number of bins to dereference')
 
-@pwndbg.events.new_objfile
+@pwndbg.events.start
 def update():
-    import pwndbg.heap.dlmalloc
     import pwndbg.heap.ptmalloc
-
     global current
-
-
-    if pwndbg.symbol.address('ptmalloc_init'):
-        current = pwndbg.heap.ptmalloc.Heap()
-
-    else:
-        # Default to ptmalloc heap for now until
-        # there are more implementations
-        current = pwndbg.heap.ptmalloc.Heap()
+    current = pwndbg.heap.ptmalloc.Heap()
