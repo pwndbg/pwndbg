00e94a9b4aacbe614004311605d92157359742dc
Disconnect3d <dominik.b.czarnota@gmail.com>
2021-09-14 01:11:22 +0200

    Fix unprinted 'Unknown register' context warning

--- pwndbg/commands/context.py
+++ pwndbg/commands/context.py
@@ -386,7 +386,7 @@ def get_regs(*regs):
             continue
 
         if reg not in pwndbg.regs:
-            message.warn("Unknown register: %r" % reg)
+            print(message.warn("Unknown register: %r" % reg))
             continue
 
         value = pwndbg.regs[reg]
