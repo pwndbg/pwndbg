957788807524c7c646fb748914587811ffd0f33d
Arusekk <arek_koz@o2.pl>
2021-10-02 11:52:46 +0200

    Add basic coverage testing

--- tests.sh
+++ tests.sh
@@ -19,7 +19,7 @@ tests_passed_or_skipped=0
 tests_failed=0
 
 for test_case in ${TESTS_LIST}; do
-    PWNDBG_LAUNCH_TEST="${test_case}" PWNDBG_DISABLE_COLORS=1 gdb --silent --nx --nh --command gdbinit.py --command pytests_launcher.py --eval-command quit
+    COVERAGE_PROCESS_START=.coveragerc PWNDBG_LAUNCH_TEST="${test_case}" PWNDBG_DISABLE_COLORS=1 gdb --silent --nx --nh -ex 'py import coverage;coverage.process_startup()' --command gdbinit.py --command pytests_launcher.py --eval-command quit
     exit_status=$?
 
     if [ ${exit_status} -eq 0 ]; then
