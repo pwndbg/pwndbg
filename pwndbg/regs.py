046d00ee7ee5515dfff0b619203f01c7c9445bce
Tobias Faller <faller@endiio.com>
2021-12-02 20:54:30 +0100

    Added missing bracket

--- pwndbg/regs.py
+++ pwndbg/regs.py
@@ -276,7 +276,7 @@ riscv = RegisterSet( pc      = 'pc',
                                 'ft8', 'ft9', 'ft10', 'ft11'),
                      retval  = ('a0', 'a1', 'fa0', 'fa1', ),
                      args    = ('a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7',
-                                'fa0', 'fa1', 'fa2', 'fa3', 'fa4', 'fa5', 'fa6', 'fa7')
+                                'fa0', 'fa1', 'fa2', 'fa3', 'fa4', 'fa5', 'fa6', 'fa7'))
 
 arch_to_regs = {
     'i386': i386,
