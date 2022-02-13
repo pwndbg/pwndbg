c11ac3710beb7c1df4d1970d17a0b7e5e2d17845
Bet4 <0xbet4@gmail.com>
2022-02-13 21:33:01 +0800

    Show all registers of PowerPC

--- pwndbg/regs.py
+++ pwndbg/regs.py
@@ -170,13 +170,12 @@ i386 = RegisterSet( pc      = 'eip',
 # r13     Small data area pointer register (points to TLS)
 # r14-r30 Registers used for local variables
 # r31     Used for local variables or "environment pointers"
-powerpc = RegisterSet(  retaddr = ('lr','r0'),
+powerpc = RegisterSet(  retaddr = ('lr',),
                         flags   = {'msr':{},'xer':{}},
-                        gpr     = ('r3', 'r4', 'r5', 'r6', 'r7', 'r8', 'r9',
+                        gpr     = ('r0', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8', 'r9',
                                    'r10', 'r11', 'r12', 'r13', 'r14', 'r15', 'r16', 'r17', 'r18', 'r19',
                                    'r20', 'r21', 'r22', 'r23', 'r24', 'r25', 'r26', 'r27', 'r28', 'r29',
-                                   'r30', 'r31'),
-                        misc    = ('cr','lr','r2'),
+                                   'r30', 'r31', 'cr', 'ctr'),
                         args    = ('r3', 'r4', 'r5', 'r6', 'r7', 'r8', 'r9', 'r10'),
                         retval  = 'r3')
 
