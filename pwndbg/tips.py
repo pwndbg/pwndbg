8cc0ab8b48c8a4d020c4b9215355cdc6f30eb0f3
Disconnect3d <dominik.b.czarnota@gmail.com>
2022-07-16 18:42:36 +0200

    Update tips.py

--- pwndbg/tips.py
+++ pwndbg/tips.py
@@ -1,8 +1,11 @@
 from random import choice
 
 TIPS = [
-    "Don't eat yellow snow",
-    "With medium power comes medium responsibility"
+    "GDB and Pwndbg parameters can be shown or set with `show <param>` and `set <param> <value>` GDB commands",
+    "GDB's `apropos <topic>` command displays all registered commands that are related to the given <topic>",
+    "GDB's `follow-fork-mode` parameter can be used to set whether to trace parent or child after fork() calls",
+    "Use Pwndbg's `config` and `theme` commands to tune its configuration and theme colors!",
+    "Pwndbg mirrors some of Windbg commands like eq, ew, ed, eb, es, dq, dw, dd, db, ds for writing and reading memory",
 ]
 
 
