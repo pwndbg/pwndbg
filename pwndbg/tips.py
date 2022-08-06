dbdda35c945f4046d09ba694507286389bd372c9
disconnect3d <dominik.b.czarnota@gmail.com>
2022-08-06 22:26:14 +0200

    tips: add a tip about Pwndbg instruction emulation for better ctx display

--- pwndbg/tips.py
+++ pwndbg/tips.py
@@ -8,6 +8,7 @@ TIPS = [
     "Pwndbg mirrors some of Windbg commands like eq, ew, ed, eb, es, dq, dw, dd, db, ds for writing and reading memory",
     "Pwndbg resolves kernel memory maps by parsing page tables (default) or via `monitor info mem` QEMU gdbstub command (use `set kernel-vmmap-via-page-tables off` for that)",
     "Use the `context` (`ctx` for short) command to display the context once again. You can reconfigure the context layout with `set context-section <sections>` or forward the output to a file/tty via `set context-output <file>`. See also `config context` to configure it further!",
+    "Pwndbg context displays where the program branches to thanks to emulating few instructions into the future. You can disable this with `set emulate off` which may also speed up debugging",
     "Use the `canary` command to see all stack canary/cookie values on the stack (based on the *usual* stack canary value initialized by glibc)",
     "Use the `procinfo` command for better process introspection (than the GDB's `info proc` command)",
 ]
