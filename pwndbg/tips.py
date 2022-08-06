a66b95e7d262a75ea2a5c266381f7e45645f407a
disconnect3d <dominik.b.czarnota@gmail.com>
2022-08-06 22:14:56 +0200

    tips: add tip about how we resolve kernel memory maps

--- pwndbg/tips.py
+++ pwndbg/tips.py
@@ -6,6 +6,7 @@ TIPS = [
     "GDB's `follow-fork-mode` parameter can be used to set whether to trace parent or child after fork() calls",
     "Use Pwndbg's `config` and `theme` commands to tune its configuration and theme colors!",
     "Pwndbg mirrors some of Windbg commands like eq, ew, ed, eb, es, dq, dw, dd, db, ds for writing and reading memory",
+    "Pwndbg resolves kernel memory maps by parsing page tables (default) or via `monitor info mem` QEMU gdbstub command (use `set kernel-vmmap-via-page-tables off` for that)",
 ]
 
 
