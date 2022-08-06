efeb90579d6daf0ab2a19467378a7144c49cd7be
disconnect3d <dominik.b.czarnota@gmail.com>
2022-08-06 22:24:03 +0200

    tips: add a tip about context display

--- pwndbg/tips.py
+++ pwndbg/tips.py
@@ -7,6 +7,7 @@ TIPS = [
     "Use Pwndbg's `config` and `theme` commands to tune its configuration and theme colors!",
     "Pwndbg mirrors some of Windbg commands like eq, ew, ed, eb, es, dq, dw, dd, db, ds for writing and reading memory",
     "Pwndbg resolves kernel memory maps by parsing page tables (default) or via `monitor info mem` QEMU gdbstub command (use `set kernel-vmmap-via-page-tables off` for that)",
+    "Use the `context` (`ctx` for short) command to display the context once again. You can reconfigure the context layout with `set context-section <sections>` or forward the output to a file/tty via `set context-output <file>`. See also `config context` to configure it further!",
     "Use the `canary` command to see all stack canary/cookie values on the stack (based on the *usual* stack canary value initialized by glibc)",
     "Use the `procinfo` command for better process introspection (than the GDB's `info proc` command)",
 ]
