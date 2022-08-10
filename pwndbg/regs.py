b27a59729f221653a5ca6e67c3fdc8f9f21cf2a3
Artur Cygan <arczicygan@gmail.com>
2022-08-10 23:11:19 +0200

    Fix aarch64 regs display (#1054)

--- pwndbg/regs.py
+++ pwndbg/regs.py
@@ -105,10 +105,15 @@ armcm = RegisterSet(  retaddr = ('lr',),
 # FIXME AArch64 does not have a CPSR register
 aarch64 = RegisterSet(  retaddr = ('lr',),
                         flags   = {'cpsr':{}},
-                        frame   = 'x29',
+                        # X29 is the frame pointer register (FP) but setting it
+                        # as frame here messes up the register order to the point
+                        # it's confusing. Think about improving this if frame
+                        # pointer semantics are required for other functionalities.
+                        # frame   = 'x29',
                         gpr     = ('x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9',
                                    'x10', 'x11', 'x12', 'x13', 'x14', 'x15', 'x16', 'x17', 'x18', 'x19',
-                                   'x20', 'x21', 'x22', 'x23', 'x24', 'x25', 'x26', 'x27', 'x28', 'x30'),
+                                   'x20', 'x21', 'x22', 'x23', 'x24', 'x25', 'x26', 'x27', 'x28', 'x29',
+                                   'x30'),
                         misc    = ('w0', 'w1', 'w2', 'w3', 'w4', 'w5', 'w6', 'w7', 'w8', 'w9',
                                    'w10', 'w11', 'w12', 'w13', 'w14', 'w15', 'w16', 'w17', 'w18', 'w19',
                                    'w20', 'w21', 'w22', 'w23', 'w24', 'w25', 'w26', 'w27', 'w28'),
