37560f607f880873b23c5764d0e90c8542b93b0c
Artur Czepiel <czepiel.artur@gmail.com>
2022-08-11 00:07:49 +0200

    refactor xor and memforb + add more tests

--- tests/test_command_xor.py
+++ tests/test_command_xor.py
@@ -1,3 +1,5 @@
+import gdb
+
 import pwndbg.memory
 import pwndbg.regs
 import tests
@@ -7,7 +9,7 @@ from pwndbg.commands.xor import xor
 REFERENCE_BINARY = tests.binaries.get("reference-binary.out")
 
 
-def test_command_xor(start_binary):
+def test_command_xor_with_gdb_execute(start_binary):
     """
     Tests simple xoring
     """
@@ -15,7 +17,35 @@ def test_command_xor(start_binary):
 
     before = pwndbg.regs.rsp
     pwndbg.memory.write(before, b"aaaaaaaa")
-    xor(before, " ", 4)
+    gdb.execute("xor $rsp ' ' 4")
+    after = pwndbg.memory.read(before, 8)
+    assert after == b"AAAAaaaa"
+
+def test_command_xor_with_int(start_binary):
+    """
+    Tests simple xoring
+    """
+    start_binary(REFERENCE_BINARY)
+
+    before = pwndbg.regs.rsp
+    assert isinstance(before, int)
+    pwndbg.memory.write(before, b"aaaaaaaa")
+    gdb.execute(f"xor {before} ' ' 4")
+    after = pwndbg.memory.read(before, 8)
+    assert after == b"AAAAaaaa"
+
+
+def test_command_xor_with_hex(start_binary):
+    """
+    Tests simple xoring
+    """
+    start_binary(REFERENCE_BINARY)
+
+    before = pwndbg.regs.rsp
+    before_hex = hex(before)
+    assert isinstance(before_hex, str)
+    pwndbg.memory.write(before, b"aaaaaaaa")
+    gdb.execute(f"xor {before_hex} ' ' 4")
     after = pwndbg.memory.read(before, 8)
     assert after == b"AAAAaaaa"
 
