087eca9ab978b453745d1a1da36489ebf2472e7d
Gulshan Singh <gsingh2011@gmail.com>
2022-09-08 18:51:02 -0700

    Fix more flake8 errors and warnings

--- lint.sh
+++ lint.sh
@@ -4,8 +4,8 @@ set -o xtrace
 set -o errexit
 
 isort --check-only --diff pwndbg tests
-black --diff --check pwndbg tests
-flake8 --show-source pwndbg tests
+black --diff --check .
+flake8 --show-source .
 
 # Indents are four spaces, binary ops can start a line, and indent switch cases
 shfmt -i 4 -bn -ci -d .
