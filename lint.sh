4f9ce950c753021c49f5acd97e02d36b942836f9
Gulshan Singh <gsingh2011@gmail.com>
2022-09-08 20:24:14 -0700

    Add support for formatting the code directly from lint.sh

--- lint.sh
+++ lint.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 
-set -o xtrace
 set -o errexit
 
-isort --check-only --diff .
-black --diff --check .
+help_and_exit() {
+    echo "Usage: ./lint.sh [-f|--filter]"
+    echo "  -f,  --filter         format code instead of just checking the format"
+    exit 1
+}
+
+if [[ $# -gt 1 ]]; then
+    help_and_exit
+fi
+
+FORMAT=0
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -f | --format)
+            FORMAT=1
+            shift
+            ;;
+        *)
+            help_and_exit
+            ;;
+    esac
+done
+
+set -o xtrace
+
+if [[ $FORMAT == 1 ]]; then
+    isort .
+    black .
+else
+    isort --check-only --diff .
+    black --check --diff .
+fi
+
 flake8 --show-source .
 
 # Indents are four spaces, binary ops can start a line, and indent switch cases
