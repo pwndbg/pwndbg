3ac4c6ac6131ffe35fc186407198bf6e741b99ad
disconnect3d <dominik.b.czarnota@gmail.com>
2022-09-16 21:33:45 +0200

    fix test

--- tests/test_command_vmmap.py
+++ tests/test_command_vmmap.py
@@ -30,7 +30,7 @@ def get_proc_maps():
             addrs, perms, offset, _inode, size, objfile = line.split(maxsplit=6)
             start, end = map(lambda v: '0x' + v, addrs.split("-"))
             offset = offset.lstrip("0")
-            size = hex(int(size))[2:]
+            size = int(end, 16) - int(end, start)
             maps.append([start, end, perms, offset, size, objfile])
 
     return maps
