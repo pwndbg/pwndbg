96d90d18a50cdf022b14faf63190e263f6fd454f
ntsleep <sleep.tight@post.com>
2022-09-23 00:07:14 +0200

    Added test for procinfo command before binary srat

--- tests/test_command_procinfo.py
+++ tests/test_command_procinfo.py
@@ -8,7 +8,6 @@ REFERENCE_BINARY = tests.binaries.get("reference-binary.out")
 def test_command_procinfo(start_binary):
     start_binary(REFERENCE_BINARY)
 
-    gdb.execute("entry")
     bin_path = gdb.execute("pi pwndbg.proc.exe", to_string=True).strip("\n")
     pid = gdb.execute("pi pwndbg.proc.pid", to_string=True).strip("\n")
     result = gdb.execute("procinfo", to_string=True)
@@ -16,3 +15,8 @@ def test_command_procinfo(start_binary):
 
     assert bin_path in res_list[0]
     assert pid in res_list[1]
+
+
+def test_command_procinfo_before_binary_start():
+    result = gdb.execute("procinfo", to_string=True)
+    assert "The program is not being run" in result
