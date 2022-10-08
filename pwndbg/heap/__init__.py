84635ba6e088a74aecf03255dfa5b751541b89cf
lebr0nli <61896187+lebr0nli@users.noreply.github.com>
2022-10-08 11:08:52 +0800

    List the symbols manually instead of using `locals()`

--- pwndbg/heap/__init__.py
+++ pwndbg/heap/__init__.py
@@ -17,7 +17,7 @@ global_max_fast = pwndbg.config.Parameter(
     "global_max_fast", "0", "&global_max_fast for heuristics", "heap"
 )
 
-symbol_list = [local for local in locals().values() if isinstance(local, pwndbg.config.Parameter)]
+symbol_list = [main_arena, thread_arena, mp_, tcache, global_max_fast]
 
 heap_chain_limit = pwndbg.config.Parameter(
     "heap-dereference-limit", 8, "number of bins to dereference", "heap"
