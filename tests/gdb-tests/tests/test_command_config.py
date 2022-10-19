4be16dcd5d22dc8ea907951e10e809c903a67e3f
Gulshan Singh <gsingh2011@gmail.com>
2022-10-19 14:54:37 -0700

    Fix heap_config

--- tests/gdb-tests/tests/test_command_config.py
+++ tests/gdb-tests/tests/test_command_config.py
@@ -8,3 +8,6 @@ def test_config():
     gdb.execute("set banner-separator #")
     # \u2500 is ─
     assert "'#' ('\u2500')" in gdb.execute("theme", to_string=True)
+
+    gdb.execute("set global-max-fast 0x80")
+    assert "'0x80' ('0')" in gdb.execute("heap_config", to_string=True)
