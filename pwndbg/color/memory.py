168677d578d11868c34d3be8385504227ab49859
Gulshan Singh <gsingh2011@gmail.com>
2022-10-23 10:48:28 -0700

    Refactor memory.py to use ColorConfig (#1331)

--- pwndbg/color/memory.py
+++ pwndbg/color/memory.py
@@ -1,43 +1,19 @@
-import pwndbg.color.theme as theme
 import pwndbg.gdblib.vmmap
-from pwndbg.color import generateColorFunction
+from pwndbg.color import ColorConfig
+from pwndbg.color import ColorParamSpec
 from pwndbg.color import normal
-from pwndbg.gdblib import config
 
-config_stack = theme.add_color_param("memory-stack-color", "yellow", "color for stack memory")
-config_heap = theme.add_color_param("memory-heap-color", "blue", "color for heap memory")
-config_code = theme.add_color_param("memory-code-color", "red", "color for executable memory")
-config_data = theme.add_color_param(
-    "memory-data-color", "purple", "color for all other writable memory"
+c = ColorConfig(
+    "memory",
+    [
+        ColorParamSpec("stack", "yellow", "color for stack memory"),
+        ColorParamSpec("heap", "blue", "color for heap memory"),
+        ColorParamSpec("code", "red", "color for executable memory"),
+        ColorParamSpec("data", "purple", "color for all other writable memory"),
+        ColorParamSpec("rodata", "normal", "color for all read only memory"),
+        ColorParamSpec("rwx", "underline", "color added to all RWX memory"),
+    ],
 )
-config_rodata = theme.add_color_param(
-    "memory-rodata-color", "normal", "color for all read only memory"
-)
-config_rwx = theme.add_color_param("memory-rwx-color", "underline", "color added to all RWX memory")
-
-
-def stack(x):
-    return generateColorFunction(config.memory_stack_color)(x)
-
-
-def heap(x):
-    return generateColorFunction(config.memory_heap_color)(x)
-
-
-def code(x):
-    return generateColorFunction(config.memory_code_color)(x)
-
-
-def data(x):
-    return generateColorFunction(config.memory_data_color)(x)
-
-
-def rodata(x):
-    return generateColorFunction(config.memory_rodata_color)(x)
-
-
-def rwx(x):
-    return generateColorFunction(config.memory_rwx_color)(x)
 
 
 def get(address, text=None):
@@ -56,19 +32,19 @@ def get(address, text=None):
     if page is None:
         color = normal
     elif "[stack" in page.objfile:
-        color = stack
+        color = c.stack
     elif "[heap" in page.objfile:
-        color = heap
+        color = c.heap
     elif page.execute:
-        color = code
+        color = c.code
     elif page.rw:
-        color = data
+        color = c.data
     else:
-        color = rodata
+        color = c.rodata
 
     if page and page.rwx:
         old_color = color
-        color = lambda x: rwx(old_color(x))
+        color = lambda x: c.rwx(old_color(x))
 
     if text is None and isinstance(address, int) and address > 255:
         text = hex(int(address))
@@ -80,5 +56,12 @@ def get(address, text=None):
 
 def legend():
     return "LEGEND: " + " | ".join(
-        (stack("STACK"), heap("HEAP"), code("CODE"), data("DATA"), rwx("RWX"), rodata("RODATA"))
+        (
+            c.stack("STACK"),
+            c.heap("HEAP"),
+            c.code("CODE"),
+            c.data("DATA"),
+            c.rwx("RWX"),
+            c.rodata("RODATA"),
+        )
     )
