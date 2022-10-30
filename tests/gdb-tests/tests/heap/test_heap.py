d8fbdc1b888d820f9f9d41d568b10008804fea08
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2022-10-31 04:45:11 +0800

    Support `malloc_par` of GLIBC 2.35 (#1353)

--- tests/gdb-tests/tests/heap/test_heap.py
+++ tests/gdb-tests/tests/heap/test_heap.py
@@ -281,14 +281,10 @@ def test_mp_heuristic(start_binary):
     # Check the address of `main_arena` is correct
     assert pwndbg.heap.current.mp.address == mp_addr_via_debug_symbol
     # Check the struct size is correct
-    # FIXME: We still have bug for GLIBC >= 2.35 in this heuristic because the size of `malloc_par` is changed
-    # So this test will fail for the tests on ubuntu 22.04
-    # TODO: Fix the bug and enable this test
-    if pwndbg.glibc.get_version() < (2, 35):
-        assert (
-            pwndbg.heap.current.mp.type.sizeof
-            == pwndbg.gdblib.typeinfo.lookup_types("struct malloc_par").sizeof
-        )
+    assert (
+        pwndbg.heap.current.mp.type.sizeof
+        == pwndbg.gdblib.typeinfo.lookup_types("struct malloc_par").sizeof
+    )
     pwndbg.heap.current = type(pwndbg.heap.current)()  # Reset the heap object of pwndbg
 
     # Level 2: We check we can get the address of `mp_` by parsing the assembly code of `__libc_free`
@@ -299,13 +295,9 @@ def test_mp_heuristic(start_binary):
     pwndbg.heap.current = type(pwndbg.heap.current)()  # Reset the heap object of pwndbg
 
     # Level 3: We check we can get the address of `mp_` by parsing the memory
-    # FIXME: We still have bug for GLIBC >= 2.35 in this heuristic because the size of `malloc_par` is changed
-    # So this test will fail for the tests on ubuntu 22.04
-    # TODO: Fix the bug and enable this test
-    if pwndbg.glibc.get_version() < (2, 35):
-        with mock_for_heuristic(mock_all=True):
-            # Check the address of `mp_` is correct
-            assert pwndbg.heap.current.mp.address == mp_addr_via_debug_symbol
+    with mock_for_heuristic(mock_all=True):
+        # Check the address of `mp_` is correct
+        assert pwndbg.heap.current.mp.address == mp_addr_via_debug_symbol
 
 
 def test_global_max_fast_heuristic(start_binary):
