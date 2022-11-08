f71a4aa65d8a1d4c0bc82848fab2ce65c23e3f77
CptGibbon <16000770+CptGibbon@users.noreply.github.com>
2022-11-08 12:00:27 -0800

    Reduce heap code redundancy (#1346)
    * Add get_sbrk_heap_region() method
    
    * Use SIZE_BITS in Chunk.real_size()
    
    * Add non_contiguous property to Arena class
    
    * Improve Heap class
    
    * More accurate arena detection
    * Integrate Heap class into Chunk class
    
    * Don't parse bins when no arena in find_fake_fast
    
    * Add active_heap property to Arena class
    
    * Add more functionality to heap classes
    
    * next_chunk method for Chunk class
    * prev property & __str__ method for Heap class
    * heaps property for Arena class
    * arenas command updated to reflect changes to Arena class
    * Use deepcopy() in get_region() to avoid changing vmmap command output
    * Import fiddling to deal with unrelated bug
    
    * Attempt at integration with heap commands
    
    With debug syms looks good, still issues to iron out with heuristics
    
    * Remove redundant heap functions
    
    * Remove redundant functions from tests
    
    * Add system_mem property to Arena class
    
    * thread_arena returns main_arena if single thread

--- tests/gdb-tests/tests/heap/test_heap.py
+++ tests/gdb-tests/tests/heap/test_heap.py
@@ -240,7 +240,7 @@ def test_main_arena_heuristic(start_binary):
     assert pwndbg.heap.current.main_arena.address == main_arena_addr_via_debug_symbol
     # Check the struct size is correct
     assert (
-        pwndbg.heap.current.main_arena.type.sizeof
+        pwndbg.heap.current.main_arena._gdbValue.type.sizeof
         == pwndbg.gdblib.typeinfo.lookup_types("struct malloc_state").sizeof
     )
     pwndbg.heap.current = type(pwndbg.heap.current)()  # Reset the heap object of pwndbg
@@ -381,7 +381,7 @@ def test_thread_arena_heuristic(start_binary):
     # Level 1: We check we can get the address of `thread_arena` from debug symbols and the value of `thread_arena` is correct
     assert pwndbg.heap.current.thread_arena is not None
     # Check the address of `thread_arena` is correct
-    assert pwndbg.heap.current.thread_arena == thread_arena_via_debug_symbol
+    assert pwndbg.heap.current.thread_arena.address == thread_arena_via_debug_symbol
     pwndbg.heap.current = type(pwndbg.heap.current)()  # Reset the heap object of pwndbg
 
     # Level 2: We check we can get the address of `thread_arena` by parsing the assembly code of `__libc_calloc`
@@ -389,7 +389,7 @@ def test_thread_arena_heuristic(start_binary):
     with mock_for_heuristic(["thread_arena"]):
         assert pwndbg.gdblib.symbol.address("thread_arena") is None
         # Check the value of `thread_arena` is correct
-        assert pwndbg.heap.current.thread_arena == thread_arena_via_debug_symbol
+        assert pwndbg.heap.current.thread_arena.address == thread_arena_via_debug_symbol
 
 
 def test_heuristic_fail_gracefully(start_binary):
