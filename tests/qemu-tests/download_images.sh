3b482a6485f78fd68d24671d7995cddb74357ad1
Gulshan Singh <gsingh2011@gmail.com>
2022-12-11 00:14:58 -0800

    Add basic QEMU system testing scripts (#1428)

---
+++ tests/qemu-tests/download_images.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://downloads.yoctoproject.org/releases/yocto/yocto-4.1.1/machines/qemu/qemux86-64/bzImage-qemux86-64.bin
+wget https://downloads.yoctoproject.org/releases/yocto/yocto-4.1.1/machines/qemu/qemux86-64/core-image-minimal-dev-qemux86-64-20221114164338.rootfs.ext4
+
+wget https://downloads.yoctoproject.org/releases/yocto/yocto-4.1.1/machines/qemu/qemuarm64/Image-qemuarm64.bin
+wget https://downloads.yoctoproject.org/releases/yocto/yocto-4.1.1/machines/qemu/qemuarm64/core-image-minimal-dev-qemuarm64-20221114170418.rootfs.ext4
