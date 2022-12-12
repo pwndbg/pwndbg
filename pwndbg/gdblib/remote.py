86d096d544c8c92d6980d0e8deb54388ff30b0dd
lebr0nli <61896187+lebr0nli@users.noreply.github.com>
2022-12-12 18:29:58 +0800

    Fix the bug when using gdbserver
    If we execute the `monitor` command in `is_debug_probe()`, the GDB with normal GDBserver will stuck after first `continue`.
    
    To avoid this, we check if `monitor help` showed that we are using GDBserver.

--- pwndbg/gdblib/remote.py
+++ pwndbg/gdblib/remote.py
@@ -26,6 +26,21 @@ def is_debug_probe():
     Currently detects the Black Magic Probe and the SEGGER J-Link GDB Server.
     """
     try:
+        help_output = gdb.execute("monitor help", to_string=True)
+        if (
+            "GDBserver" in help_output
+            and "Black Magic Probe" not in help_output
+            and "SEGGER J-Link GDB Server" not in help_output
+        ):
+            # We can't directly use the `monitor` command if we are using normal GDBserver, because the `monitor` command will cause GDBserver stuck.
+            # So we check if we are using GDBserver by checking the output of `monitor help`.
+            # TODO: Does this problem only occur with normal GDBserver?
+            # If not, we should find a better way to check what remote server we are using.
+            return False
+    except gdb.error:
+        # Now we check if we are using Black Magic Probe or the SEGGER J-Link GDB Server
+        pass
+    try:
         monitor_output = gdb.execute("monitor", to_string=True)
     except gdb.error:
         # the monitor command might fail, but we don't care since it doesn't fail on the devices we check for.
