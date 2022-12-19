df0e427c98294ed00463e7e56b8606b57c109270
Gulshan Singh <gsgx@google.com>
2022-12-18 16:38:18 -0800

    Added command to list slabs

---
+++ pwndbg/commands/slab.py
@@ -0,0 +1,42 @@
+import argparse
+
+from tabulate import tabulate
+
+import pwndbg.commands
+import pwndbg.gdblib.kernel.slab
+from pwndbg.gdblib.kernel.slab import oo_objects
+from pwndbg.gdblib.kernel.slab import oo_order
+
+parser = argparse.ArgumentParser(description="Prints information about the SLUB allocator")
+parser.add_argument(
+    "filter_",
+    metavar="filter",
+    type=str,
+    nargs="?",
+    help="Only show caches that contain the given filter string",
+)
+
+
+@pwndbg.commands.ArgparsedCommand(parser)
+@pwndbg.commands.OnlyWhenQemuKernel
+@pwndbg.commands.OnlyWithKernelDebugSyms
+def slab(filter_=None):
+    results = []
+    for cache in pwndbg.gdblib.kernel.slab.caches():
+        name = pwndbg.gdblib.memory.string(cache["name"]).decode("ascii")
+        if filter_ and filter_ not in name:
+            continue
+        order = oo_order(int(cache["oo"]["x"]))
+        objects = oo_objects(int(cache["oo"]["x"]))
+        results.append(
+            [
+                name,
+                objects,
+                int(cache["size"]),
+                int(cache["object_size"]),
+                int(cache["inuse"]),
+                order,
+            ]
+        )
+
+    print(tabulate(results, headers=["Name", "# Objects", "Size", "Obj Size", "# inuse", "order"]))
