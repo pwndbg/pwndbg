3c6765169a0ff1b6e7338c3014e9952fbc9c7b56
Gulshan Singh <gsgx@google.com>
2022-12-19 11:53:56 -0800

    Add pcp command

--- pwndbg/commands/__init__.py
+++ pwndbg/commands/__init__.py
@@ -623,6 +623,7 @@ def load_commands() -> None:
     import pwndbg.commands.next
     import pwndbg.commands.p2p
     import pwndbg.commands.patch
+    import pwndbg.commands.pcp
     import pwndbg.commands.peda
     import pwndbg.commands.pie
     import pwndbg.commands.probeleak
