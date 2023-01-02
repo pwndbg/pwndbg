d9e4c95503da389c0a15825ba009c1a8c651ea46
Gulshan Singh <gsingh2011@gmail.com>
2023-01-02 00:08:03 -0800

    Add slab info command (#1447)

--- pwndbg/gdblib/kernel/slab.py
+++ pwndbg/gdblib/kernel/slab.py
@@ -9,6 +9,13 @@ def caches():
         yield slab_cache
 
 
+def get_cache(target_name: str):
+    slab_caches = gdb.lookup_global_symbol("slab_caches").value()
+    for slab_cache in for_each_entry(slab_caches, "struct kmem_cache", "list"):
+        if target_name == slab_cache["name"].string():
+            return slab_cache
+
+
 OO_SHIFT = 16
 OO_MASK = (1 << OO_SHIFT) - 1
 
