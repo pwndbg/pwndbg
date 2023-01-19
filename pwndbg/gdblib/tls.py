bc3fe91cdbe833629ef3603de12857e8794719f8
lebr0nli <61896187+lebr0nli@users.noreply.github.com>
2023-01-19 18:02:02 +0800

    Refactor TLS module
    - Replace unreliable `__errno_location()` trick with `pthread_self()` to acquire TLS address
    - Consolidate heap heuristics checks about TLS within the `pwndbg.gdblib.tls` module for better organization

--- pwndbg/gdblib/tls.py
+++ pwndbg/gdblib/tls.py
@@ -2,6 +2,7 @@
 Getting Thread Local Storage (TLS) information.
 """
 import sys
+from contextlib import contextmanager
 from types import ModuleType
 
 import gdb
@@ -14,74 +15,53 @@ import pwndbg.gdblib.symbol
 import pwndbg.gdblib.vmmap
 
 
+@contextmanager
+def lock_scheduler():
+    already_lock = gdb.parameter("scheduler-locking") == "on"
+    old_config = gdb.parameter("scheduler-locking")
+    if not already_lock:
+        gdb.execute("set scheduler-locking on")
+    yield
+    if not already_lock:
+        gdb.execute("set scheduler-locking %s" % old_config)
+
+
 class module(ModuleType):
     """Getting Thread Local Storage (TLS) information."""
 
-    _errno_offset = None
+    lock_scheduler = staticmethod(lock_scheduler)
 
-    def get_tls_base_via_errno_location(self) -> int:
-        """Heuristically determine the base address of the TLS."""
-        if pwndbg.gdblib.symbol.address(
-            "__errno_location"
-        ) is None or pwndbg.gdblib.arch.current not in (
-            "x86-64",
-            "i386",
-            "arm",
-        ):
-            # Note: We doesn't implement this for aarch64 because its TPIDR_EL0 register seems always work
-            # If oneday we can't get TLS base via TPIDR_EL0, we should implement this for aarch64
+    def is_thread_local_variable_offset(self, offset: int) -> bool:
+        """Check if the offset to TLS is a valid offset for the heap heuristics."""
+        if pwndbg.gdblib.arch.current in ("x86-64", "i386"):
+            is_valid = 0 < -offset < 0x250
+        else:  # elif pwndbg.gdblib.arch.current in ("aarch64", "arm"):
+            is_valid = 0 < offset < 0x250
+        # check alignment
+        return is_valid and offset % pwndbg.gdblib.arch.ptrsize == 0
+
+    def is_thread_local_variable(self, addr: int) -> bool:
+        """Check if the address is a valid thread local variable's address for the heap heuristics."""
+        if not self.address:
+            # Since we can not get the TLS base address, we trust that the address is valid.
+            return True
+        return self.is_thread_local_variable_offset(
+            addr - self.address
+        ) and addr in pwndbg.gdblib.vmmap.find(self.address)
+
+    def call_pthread_self(self) -> int:
+        """Get the address of TLS by calling pthread_self()."""
+        if pwndbg.gdblib.symbol.address("pthread_self") is None:
             return 0
-        already_lock = gdb.parameter("scheduler-locking") == "on"
-        old_config = gdb.parameter("scheduler-locking")
-        if not already_lock:
-            gdb.execute("set scheduler-locking on")
-        errno_addr = int(gdb.parse_and_eval("(int *)__errno_location()"))
-        if not already_lock:
-            gdb.execute("set scheduler-locking %s" % old_config)
-
-        if not self._errno_offset:
-            __errno_location_instr = pwndbg.disasm.near(
-                pwndbg.gdblib.symbol.address("__errno_location"), 5, show_prev_insns=False
-            )
-            if pwndbg.gdblib.arch.current == "x86-64":
-                for instr in __errno_location_instr:
-                    # Find something like: mov rax, qword ptr [rip + disp]
-                    if instr.mnemonic == "mov":
-                        self._errno_offset = pwndbg.gdblib.memory.s64(instr.next + instr.disp)
-                        break
-            elif pwndbg.gdblib.arch.current == "i386":
-                for instr in __errno_location_instr:
-                    # Find something like: mov eax, dword ptr [eax + disp]
-                    # (disp is a negative value)
-                    if instr.mnemonic == "mov":
-                        # base offset is from the first `add eax` after `call __x86.get_pc_thunk.bx`
-                        base_offset_instr = next(
-                            instr for instr in __errno_location_instr if instr.mnemonic == "add"
-                        )
-                        base_offset = base_offset_instr.address + base_offset_instr.operands[1].int
-                        self._errno_offset = pwndbg.gdblib.memory.s32(base_offset + instr.disp)
-                        break
-            elif pwndbg.gdblib.arch.current == "arm":
-                ldr_instr = None
-                for instr in __errno_location_instr:
-                    if not ldr_instr and instr.mnemonic == "ldr":
-                        ldr_instr = instr
-                    elif ldr_instr and instr.mnemonic == "add":
-                        offset = ldr_instr.operands[1].mem.disp
-                        offset = pwndbg.gdblib.memory.s32((ldr_instr.address + 4 & -4) + offset)
-                        self._errno_offset = pwndbg.gdblib.memory.s32(instr.address + 4 + offset)
-                        break
-        if not self._errno_offset:
-            raise OSError("Can not find tls base")
-        return errno_addr - self._errno_offset
+        with lock_scheduler():
+            try:
+                return int(gdb.parse_and_eval("(void *)pthread_self()"))
+            except gdb.error:
+                return 0
 
     @property
     def address(self) -> int:
         """Get the base address of TLS."""
-        if pwndbg.gdblib.arch.current not in ("x86-64", "i386", "aarch64", "arm"):
-            # Not supported yet
-            return 0
-
         tls_base = 0
 
         if pwndbg.gdblib.arch.current == "x86-64":
@@ -91,20 +71,12 @@ class module(ModuleType):
         elif pwndbg.gdblib.arch.current == "aarch64":
             tls_base = int(pwndbg.gdblib.regs.TPIDR_EL0)
 
-        # Sometimes, we need to get TLS base via errno location for the following reason:
+        # Sometimes, we need to get TLS base via pthread_self() for the following reason:
         # For x86-64, fsbase might be 0 if we are remotely debugging and the GDB version <= 8.X
         # For i386, gsbase might be 0 if we are remotely debugging
-        # For arm (32-bit), we doesn't have other choice
+        # For other archs, we can't get the TLS base address via register
         # Note: aarch64 seems doesn't have this issue
-        is_valid_tls_base = (
-            pwndbg.gdblib.vmmap.find(tls_base) is not None
-            and tls_base % pwndbg.gdblib.arch.ptrsize == 0
-        )
-        return tls_base if is_valid_tls_base else self.get_tls_base_via_errno_location()
-
-    def reset(self) -> None:
-        # We should reset the offset when we attach to a new process
-        self._errno_offset = None
+        return tls_base if tls_base else self.call_pthread_self()
 
 
 # To prevent garbage collection
