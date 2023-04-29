ba0b66dc6b384d49cabce575d7f7f58339bffff4
disconnect3d <dominik.b.czarnota@gmail.com>
2023-04-29 15:47:46 +0200

    lib.cache: address PR comments and add debug mode

--- pwndbg/lib/cache.py
+++ pwndbg/lib/cache.py
@@ -9,8 +9,47 @@ from typing import Any
 from typing import Callable
 from typing import Dict
 from typing import Tuple
+from collections import UserDict
 
-debug = 0
+# Set to enable print logging of cache hits/misses/clears
+NO_DEBUG, DEBUG_GET, DEBUG_CLEAR, DEBUG_SET = 0, 1, 2, 4
+# combine the flags with | operator
+debug = DEBUG_CLEAR
+# debug_name can be used to filter cache results by a given name
+debug_name = 'regs'
+
+
+class DebugCacheDict(UserDict):
+    def __init__(self, func, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.hits = 0
+        self.misses = 0
+        self.func = func
+        self.name = f'{func.__module__.split(".")[-1]}.{func.__name__}'
+
+    def __getitem__(self, key):
+        if debug & DEBUG_GET and (not debug_name or debug_name in self.name):
+            print(f"GET {self.name}: {key}")
+        try:
+            value = self.data[key]
+            self.hits += 1
+            return value
+        except KeyError:
+            self.misses += 1
+            raise
+
+    def __setitem__(self, key, value):
+        if debug & DEBUG_SET and (not debug_name or debug_name in self.name):
+            print(f"SET {self.name}: {key}={value}")
+        self.data[key] = value
+
+    def clear(self):
+        if debug & DEBUG_CLEAR and (not debug_name or debug_name in self.name):
+            print(f"CLEAR {self.name} (hits: {self.hits}, misses: {self.misses})")
+        self.data.clear()
+        self.hits = 0
+        self.misses = 0
+
 
 
 class _CacheUntilEvent:
@@ -34,37 +73,6 @@ class _CacheUntilEvent:
         self.caches.append(cache)
 
 
-# This is not used anywhere, but it might be useful in the future.
-# To make it work, the `IS_CACHING` needs to be per-cache, not global.
-# To implement that, we need to:
-# 1. Change the `inner` decorator below to pass `func` to the event
-# 2. Set `func.is_caching=True` in `decorator` below
-# 3. Use `if func.is_caching` instead of `if IS_CACHING` in `decorator`
-# All this would allow the `_CacheWhileRunning` to set the
-# `is_caching` status based on start/exit events.
-# class _CacheWhileRunning(_CacheUntilEvent):
-#    def __init__(self):
-#        super().__init__(event_exit)
-#        self.func = None
-#
-#    def connect_event_hooks(self, event_start, event_exit):
-#        event_start(self.__start_caching)
-#        event_exit(self.clear)
-#
-#    def add_cache(self, cache, func):
-#        super().add_cache(cache)
-#        self.func = func
-#
-#    @pwndbg.gdblib.events.start
-#    @staticmethod
-#    def __start_caching():
-#        self.func.is_caching = True
-#
-#    def clear(self):
-#        super().clear()
-#        self.func.is_caching = False
-
-
 _ALL_CACHE_UNTIL_EVENTS = {
     "stop": _CacheUntilEvent(),
     "exit": _CacheUntilEvent(),
@@ -75,13 +83,14 @@ _ALL_CACHE_UNTIL_EVENTS = {
     "prompt": _CacheUntilEvent(),
     "forever": _CacheUntilEvent(),
 }
+_ALL_CACHE_EVENT_NAMES = tuple(_ALL_CACHE_UNTIL_EVENTS.keys())
 
 
-def connect_clear_caching_events(event_tuples):
+def connect_clear_caching_events(event_dicts):
     """
     Connect given debugger event hooks to correspoonding _CacheUntilEvent instances
     """
-    for (event_name, event_hooks) in event_tuples:
+    for event_name, event_hooks in event_dicts.items():
         _ALL_CACHE_UNTIL_EVENTS[event_name].connect_event_hooks(event_hooks)
 
 
@@ -89,11 +98,17 @@ def connect_clear_caching_events(event_tuples):
 _NOT_FOUND_IN_CACHE = object()
 _KWARGS_SEPARATOR = object()
 
-# Global value whether the results from cache are returned or not. Toggled with the `memoize` command.
+# Global value whether the results from cache are returned or not
 IS_CACHING = True
 
 
 def cache_until(*event_names) -> Callable:
+    if any(event_name not in _ALL_CACHE_EVENT_NAMES for event_name in event_names):
+        raise ValueError(
+            f"Unknown event name[s] passed to the `cache_until` decorator: {event_names}.\n"
+            f"Expected: {_ALL_CACHE_EVENT_NAMES}"
+        )
+
     def inner(func):
         if hasattr(func, "cache"):
             raise ValueError(
@@ -101,20 +116,24 @@ def cache_until(*event_names) -> Callable:
                 "Pass multiple event names to the `cache_until` decorator."
             )
 
-        cache: Dict[Tuple[Any], Any] = {}
+        cache: Dict[Tuple[Any], Any] = {} if not debug else DebugCacheDict(func)
 
         @wraps(func)
         def decorator(*a, **kw):
             if IS_CACHING:
                 key: Tuple[Any] = (a, _KWARGS_SEPARATOR, *kw.items())
+
+                # Check if the value is in the cache; if we have a cache miss,
+                # we return a special singleton object `_NOT_FOUND_IN_CACHE`. This way
+                # we can also cache a result of 'None' from a function
                 value = cache.get(key, _NOT_FOUND_IN_CACHE)
                 if value is not _NOT_FOUND_IN_CACHE:
                     return value
 
                 value = func(*a, **kw)
 
-                # Sanity check; should we maybe do it only in debug?
-                # TODO/FIXME: Should we do similar sanity check for args/kwargs?
+                # Sanity check: its not perfect and won't cover all cases like ([],)
+                # but it should be good enough
                 if isinstance(value, list):
                     print(f"Should not cache mutable types! {func.__name__}")
 
@@ -124,6 +143,8 @@ def cache_until(*event_names) -> Callable:
 
             return func(*a, **kw)
 
+        # Set the cache on the function so it can be cleared on demand
+        # this may be useful for tests
         decorator.cache = cache
 
         # Register the cache for the given event so it can be cleared
