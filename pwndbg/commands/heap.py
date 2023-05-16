c1bb54244f23d70e2c05d61a71053add36b0ba25
Unknown Sentinel <63234449+Unknownsentinel193@users.noreply.github.com>
2023-05-16 18:22:52 +0530

    Update heap.py

--- pwndbg/commands/heap.py
+++ pwndbg/commands/heap.py
@@ -157,7 +157,6 @@ parser.add_argument(
 )
 
 
-
 @pwndbg.commands.ArgparsedCommand(parser, category=CommandCategory.HEAP)
 @pwndbg.commands.OnlyWhenRunning
 @pwndbg.commands.OnlyWithResolvedHeapSyms
@@ -369,9 +368,7 @@ parser.add_argument(
 parser.add_argument(
     "-s", "--simple", action="store_true", help="Simply print malloc_chunk struct's contents."
 )
-parser.add_argument(
-    "--dump", action="store_true", help="Print a hexdump of the chunk bytes."
-)
+parser.add_argument("--dump", action="store_true", help="Print a hexdump of the chunk bytes.")
 
 
 @pwndbg.commands.ArgparsedCommand(parser, category=CommandCategory.HEAP)
