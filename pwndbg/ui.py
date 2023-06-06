79ade42184ee40ac62053f60d18058f6d5d4b558
Tom Tan <lilotom@gmail.com>
2023-06-06 11:50:49 -0700

    Fix banner width for TUI mode with horizontal split for cmd window (#1752)
    * Fix banner width for TUI mode with horizontal split for cmd window
    
    * Fix lint
    
    * Add doc string for get_cmd_window_size

--- pwndbg/ui.py
+++ pwndbg/ui.py
@@ -8,6 +8,8 @@ import struct
 import sys
 import termios
 
+import gdb
+
 import pwndbg.color.context as C
 import pwndbg.gdblib.arch
 from pwndbg.color import ljust_colored
@@ -67,9 +69,39 @@ def get_window_size(target=sys.stdin):
     fallback = (int(os.environ.get("LINES", 20)), int(os.environ.get("COLUMNS", 80)))
     if not target.isatty():
         return fallback
+    rows, cols = get_cmd_window_size()
+    if rows is not None and cols is not None:
+        return rows, cols
     try:
         # get terminal size and force ret buffer len of 4 bytes for safe unpacking by passing equally long arg
         rows, cols = struct.unpack("hh", fcntl.ioctl(target.fileno(), termios.TIOCGWINSZ, b"1234"))
     except Exception:
         rows, cols = fallback
     return rows, cols
+
+
+def get_cmd_window_size():
+    """Get the size of the command window in TUI mode which could be different than the terminal window width \
+    with horizontal split "tui new-layout hsrc { -horizontal src 1 cmd 1 } 1".
+
+    Possible output of "info win" in TUI mode:
+    (gdb) info win
+    Name       Lines Columns Focus
+    src           77     104 (has focus)
+    cmd           77     105
+
+    Output of "info win" in non-TUI mode:
+    (gdb) info win
+    The TUI is not active."""
+    info_out = gdb.execute("info win", to_string=True).split()
+    if "cmd" not in info_out:
+        # if TUI is not enabled, info win will output "The TUI is not active."
+        return None, None
+    # parse cmd window size from the output of "info win"
+    cmd_win_index = info_out.index("cmd")
+    if len(info_out) <= cmd_win_index + 2:
+        return None, None
+    elif not info_out[cmd_win_index + 1].isdigit() and not info_out[cmd_win_index + 2].isdigit():
+        return None, None
+    else:
+        return int(info_out[cmd_win_index + 1]), int(info_out[cmd_win_index + 2])
