1954c1157f1951e0dfa0c88a4adaac9811823e94
ntsleep <sleep.tight@post.com>
2023-07-05 00:17:09 +0200

    Updated heap command to show real size without flag bits

--- pwndbg/commands/heap.py
+++ pwndbg/commands/heap.py
@@ -419,10 +419,11 @@ def malloc_chunk(addr, fake=False, verbose=False, simple=False) -> None:
             if no_match:
                 headers_to_print.append(message.hint("Allocated chunk"))
 
+    size_no_flags = chunk.size & (0xfffffffffffffff - 0b111)
     if verbose:
         fields_to_print.update(["prev_size", "size", "fd", "bk", "fd_nextsize", "bk_nextsize"])
     else:
-        out_fields += f"Size: 0x{chunk.size:02x}\n"
+        out_fields += f"Size: 0x{size_no_flags:02x} (with flag bits: 0x{chunk.size:02x})\n"
 
     prev_inuse, is_mmapped, non_main_arena = allocator.chunk_flags(chunk.size)
     if prev_inuse:
@@ -434,7 +435,13 @@ def malloc_chunk(addr, fake=False, verbose=False, simple=False) -> None:
 
     fields_ordered = ["prev_size", "size", "fd", "bk", "fd_nextsize", "bk_nextsize"]
     for field_to_print in fields_ordered:
-        if field_to_print in fields_to_print:
+        if field_to_print not in fields_to_print:
+            continue
+        if field_to_print == "size":
+            out_fields += (
+                message.system("size") + f": 0x{size_no_flags:02x} (with flag bits: 0x{chunk.size:02x})\n"
+            )
+        else:
             out_fields += (
                 message.system(field_to_print) + f": 0x{getattr(chunk, field_to_print):02x}\n"
             )
