d7d54cb895f6bdf382cc5c36578a71659200aa9d
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2023-07-05 20:32:55 +0800

    Enhance got command (#1771)
    * Refactor the `got` command to support more use cases
    
    - Create some function to parse the information of loaded shared object libraries from `info sharedlibrary`
    - Make got command can show the entries of other libraries loaded in memory
    - Make got command can show more various relocations to support not only the `JUMP_SLOT` type relocation but also supports `IRELATIVE` and `GLOB_DAT` type relocation.
    
    * Update tests for the `got` command
    
    * Update pwndbg/commands/got.py
    
    * Update pwndbg/commands/got.py
    
    * Update pwndbg/commands/got.py
    
    * Update pwndbg/commands/got.py
    
    * Update pwndbg/commands/got.py
    
    * Update pwndbg/commands/got.py
    
    * Update pwndbg/commands/got.py
    
    * Update pwndbg/commands/got.py
    
    * Update the comment
    
    https://github.com/pwndbg/pwndbg/pull/1771#discussion_r1251054080
    
    * Update the tests
    
    * Add some hints for the qemu users
    
    ---------
    
    Co-authored-by: Disconnect3d <dominik.b.czarnota@gmail.com>

--- pwndbg/wrappers/readelf.py
+++ pwndbg/wrappers/readelf.py
@@ -1,29 +1,33 @@
+from enum import Enum
+from typing import Dict
+from typing import List
+
 import pwndbg.wrappers
 
 cmd_name = "readelf"
 
 
+class RelocationType(Enum):
+    # For x86_64, some details about these flag can be found in 4.4.1 Relocation Types in https://www.intel.com/content/dam/develop/external/us/en/documents/mpx-linux64-abi.pdf
+    # The definitions of these flags can be found in this file: https://elixir.bootlin.com/glibc/glibc-2.37/source/elf/elf.h
+    JUMP_SLOT = 1  # e.g.: R_X86_64_JUMP_SLOT
+    GLOB_DAT = 2  # e.g.: R_X86_64_GLOB_DAT
+    IRELATIVE = 3  # e.g.: R_X86_64_IRELATIVE
+
+
 @pwndbg.wrappers.OnlyWithCommand(cmd_name)
-def get_jmpslots():
-    local_path = pwndbg.gdblib.file.get_proc_exe_file()
-    cmd = get_jmpslots.cmd + ["--relocs", local_path]
+def get_got_entry(local_path: str) -> Dict[RelocationType, List[str]]:
+    # --wide is for showing the full information, e.g.: R_X86_64_JUMP_SLOT instead of R_X86_64_JUMP_SLO
+    cmd = get_got_entry.cmd + ["--relocs", "--wide", local_path]
     readelf_out = pwndbg.wrappers.call_cmd(cmd)
 
-    return filter(_extract_jumps, readelf_out.splitlines())
-
-
-def _extract_jumps(line):
-    """
-     Checks for records in `readelf --relocs <binary>` which has type e.g. `R_X86_64_JUMP_SLO`
-     NOTE: Because of that we DO NOT display entries that are not writeable (due to FULL RELRO)
-     as they have `R_X86_64_GLOB_DAT` type.
-
-    It might be good to display them separately in the future.
-    """
-    try:
-        if "JUMP" in line.split()[2]:
-            return line
-        else:
-            return False
-    except IndexError:
-        return False
+    entries: Dict[RelocationType, List[str]] = {category: [] for category in RelocationType}
+    for line in readelf_out.splitlines():
+        if not line or not line[0].isdigit():
+            continue
+        category = line.split()[2]
+        # TODO/FIXME: There's a bug here, somehow the IRELATIVE relocation might point to somewhere in .data.rel.ro, which is not in .got or .got.plt
+        for c in RelocationType:
+            if c.name in category:
+                entries[c].append(line)
+    return entries
