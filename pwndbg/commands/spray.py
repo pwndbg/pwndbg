de4acb2f40da3b0c97353a8b680ffa6440346f7b
duckl1ng <84835312+bog2n@users.noreply.github.com>
2023-07-09 00:01:32 +0200

    Improvements to spray command (#1789)
    * Improvements to spray command
    
    * fix
    
    * Update spray.py
    
    * fixed description
    
    ---------
    
    Co-authored-by: Disconnect3d <dominik.b.czarnota@gmail.com>

--- pwndbg/commands/spray.py
+++ pwndbg/commands/spray.py
@@ -1,26 +1,58 @@
 import argparse
 
 import gdb
-from pwnlib.util.cyclic import cyclic_gen
+from pwnlib.util.cyclic import cyclic
 
+import pwndbg.color.message as M
 import pwndbg.commands
 
-parser = argparse.ArgumentParser(description="Spray memory with a cyclic_gen() generated values")
+parser = argparse.ArgumentParser(description="Spray memory with cyclic() generated values")
 parser.add_argument("addr", help="Address to spray")
-parser.add_argument("length", nargs="?", help="Length of byte sequence", type=int, default=0)
+parser.add_argument(
+    "length",
+    help="Length of byte sequence, when unspecified sprays until the end of vmmap which address belongs to",
+    type=int,
+    nargs="?",
+    default=0,
+)
+parser.add_argument(
+    "--value",
+    help="Value to spray memory with, when prefixed with '0x' treated as hex string encoded big-endian",
+    type=str,
+    required=False,
+)
 
 
 @pwndbg.commands.ArgparsedCommand(parser)
-def spray(addr, length) -> None:
+def spray(addr, length, value) -> None:
     if length == 0:
-        try:
-            last_addr = next(p for p in pwndbg.gdblib.vmmap.get() if addr in p).end
-        except StopIteration:
-            last_addr = addr + 1  # Throw exception later when trying to write
-        length = last_addr - int(addr)
+        page = pwndbg.gdblib.vmmap.find(addr)
+        if page is None:
+            print(
+                M.error(
+                    f"Invalid address {addr}: can't find vmmap containing it to determine the spray length"
+                )
+            )
+            return
+        length = page.end - int(addr)
 
-    valueBytes = cyclic_gen().get(length)
+    value_bytes = b""
+
+    if value:
+        if value.startswith("0x"):
+            value_bytes = int(value, 16).to_bytes((len(value[2:]) + 1) // 2, byteorder="big")
+        else:
+            value_bytes = bytes(value, "utf-8")
+
+        value_length = len(value_bytes)
+        value_bytes = value_bytes * (int(length) // value_length)
+
+        if length % value_length != 0:
+            value_bytes += value_bytes[: (length % value_length)]
+    else:
+        value_bytes = cyclic(length, n=pwndbg.gdblib.arch.ptrsize)
+
     try:
-        pwndbg.gdblib.memory.write(addr, valueBytes)
+        pwndbg.gdblib.memory.write(addr, value_bytes)
     except gdb.MemoryError as e:
-        print(e)
+        print(M.error(e))
