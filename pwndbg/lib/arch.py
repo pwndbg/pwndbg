fad93e7658baefb272b95143fd0a2cc97560cb11
Disconnect3d <dominik.b.czarnota@gmail.com>
2023-07-18 23:30:36 +0200

    Update arch.py

--- pwndbg/lib/arch.py
+++ pwndbg/lib/arch.py
@@ -5,6 +5,9 @@ from typing import Optional
 from typing_extensions import Literal
 
 
+FMT_LITTLE_ENDIAN = {1: "B", 2: "<H", 4: "<I", 8: "<Q"}
+FMT_BIG_ENDIAN = {1: "B", 2: ">H", 4: ">I", 8: ">Q"}
+
 class Arch:
     def __init__(self, arch_name: str, ptrsize: int, endian: Literal["little", "big"]) -> None:
         self.update(arch_name, ptrsize, endian)
@@ -21,10 +24,8 @@ class Arch:
         self.ptrmask = (1 << 8 * ptrsize) - 1
         self.endian = endian
 
-        if self.endian == "little":
-            self.fmts = {1: "B", 2: "<H", 4: "<I", 8: "<Q"}
-        else:
-            self.fmts = {1: "B", 2: ">H", 4: ">I", 8: ">Q"}
+        self.fmts = FMT_LITTLE_ENDIAN if endian == "little" else FMT_BIG_ENDIAN
+        self.fmt = self.fmts[self.ptrsize]
 
         if self.name == "arm" and self.endian == "big":
             self.qemu = "armeb"
@@ -33,15 +34,14 @@ class Arch:
         else:
             self.qemu = self.name
 
-    def pack(self, integer: int, size: Optional[int] = None) -> bytes:
-        fmt = self._get_fmt(size)
-        return struct.pack(fmt, integer & self.ptrmask)
+    def pack(self, integer: int) -> bytes:
+        return struct.pack(self.fmt, integer & self.ptrmask)
 
-    def unpack(self, data: bytes, size: Optional[int] = None) -> int:
-        fmt = self._get_fmt(size)
-        return struct.unpack(fmt, data)[0]
+    def unpack(self, data: bytes) -> int:
+        return struct.unpack(self.fmt, data)[0]
 
-    def _get_fmt(self, size: Optional[int]) -> str:
-        if size is None:
-            size = self.ptrsize
-        return self.fmts[size]
+    def pack_size(self, integer: int, size: int) -> bytes:
+        return struct.pack(self.fmts[size], integer & self.ptrmask)
+
+    def unpack_size(self, data: bytes, size: int) -> int:
+        return struct.unpack(self.fmts[size], data)[0]
