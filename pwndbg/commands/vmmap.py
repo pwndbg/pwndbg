446b20a704fb31799263e597265ddf6db3b8aa8d
Filip Grzywok <f.grzywok@gmail.com>
2023-07-18 23:37:29 +0200

    feat: Implement -A and -B in vmmap.py

--- pwndbg/commands/vmmap.py
+++ pwndbg/commands/vmmap.py
@@ -15,7 +15,6 @@ from pwndbg.commands import CommandCategory
 
 integer_types = (int, gdb.Value)
 
-
 def pages_filter(gdbval_or_str):
     # returns a module filter
     if isinstance(gdbval_or_str, str):
