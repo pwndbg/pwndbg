5d1ed26fda0a5ce3c7c434f2f8402b1ff53ff3b7
Shenhav Mor <shenhavmor10@gmail.com>
2023-07-21 15:06:30 +0300

    add libc 2.12+ functionality for structs.py

--- pwndbg/heap/structs.py
+++ pwndbg/heap/structs.py
@@ -55,6 +55,7 @@ DEFAULT_TOP_PAD = 131072
 DEFAULT_MMAP_MAX = 65536
 DEFAULT_MMAP_THRESHOLD = 128 * 1024
 DEFAULT_TRIM_THRESHOLD = 128 * 1024
+DEFAULT_PAGE_SIZE = 4096
 TCACHE_FILL_COUNT = 7
 
 
@@ -290,6 +291,53 @@ class c_malloc_state_2_26(Structure):
     ]
 
 
+class c_malloc_state_2_12(Structure):
+    """
+    This class represents malloc_state struct for GLIBC < 2.22 as a ctypes struct.
+
+    https://github.com/bminor/glibc/blob/glibc-2.23/malloc/malloc.c#L1686-L1724
+
+    struct malloc_state {
+    /* Serialize access.  */
+    mutex_t mutex;
+
+    /* Flags (formerly in max_fast).  */
+    int flags;
+
+    #if THREAD_STATS
+    /* Statistics for locking.  Only used if THREAD_STATS is defined.  */
+    long stat_lock_direct, stat_lock_loop, stat_lock_wait;
+    #endif
+
+    /* Fastbins */
+    mfastbinptr      fastbinsY[NFASTBINS];
+
+    /* Base of the topmost chunk -- not otherwise kept in a bin */
+    mchunkptr        top;
+
+    /* The remainder from the most recent split of a small request */
+    mchunkptr        last_remainder;
+
+    /* Normal bins packed as described above */
+    mchunkptr        bins[NBINS * 2 - 2];
+
+    /* Bitmap of bins */
+    unsigned int     binmap[BINMAPSIZE];
+
+    /* Linked list */
+    struct malloc_state *next;
+
+    #ifdef PER_THREAD
+    /* Linked list for free arenas.  */
+    struct malloc_state *next_free;
+    #endif
+
+    /* Memory allocated from the system in this arena.  */
+    INTERNAL_SIZE_T system_mem;
+    INTERNAL_SIZE_T max_system_mem;
+    };"""
+
+
 class c_malloc_state_2_27(Structure):
     """
     This class represents malloc_state struct for GLIBC >= 2.27 as a ctypes struct.
@@ -366,8 +414,10 @@ class MallocState(CStruct2GDB):
 
     if pwndbg.glibc.get_version() >= (2, 27):
         _c_struct = c_malloc_state_2_27
-    else:
+    elif pwndbg.glibc.get_version() >= (2, 23):
         _c_struct = c_malloc_state_2_26
+    else:
+        _c_struct = c_malloc_state_2_12
     sizeof = ctypes.sizeof(_c_struct)
 
 
@@ -594,6 +644,63 @@ class c_malloc_par_2_23(Structure):
         ("sbrk_base", c_pvoid),
     ]
 
+class c_malloc_par_2_12(Structure):
+    """
+    This class represents the malloc_par struct for GLIBC < 2.15 as a ctypes struct.
+
+    https://github.com/bminor/glibc/blob/glibc-2.12/malloc/malloc.c#L2402-L2433
+
+    struct malloc_par {
+    /* Tunable parameters */
+    unsigned long    trim_threshold;
+    INTERNAL_SIZE_T  top_pad;
+    INTERNAL_SIZE_T  mmap_threshold;
+    #ifdef PER_THREAD
+    INTERNAL_SIZE_T  arena_test;
+    INTERNAL_SIZE_T  arena_max;
+    #endif
+
+    /* Memory map support */
+    int              n_mmaps;
+    int              n_mmaps_max;
+    int              max_n_mmaps;
+    /* the mmap_threshold is dynamic, until the user sets
+        it manually, at which point we need to disable any
+        dynamic behavior. */
+    int              no_dyn_threshold;
+
+    /* Cache malloc_getpagesize */
+    unsigned int     pagesize;
+
+    /* Statistics */
+    INTERNAL_SIZE_T  mmapped_mem;
+    /*INTERNAL_SIZE_T  sbrked_mem;*/
+    /*INTERNAL_SIZE_T  max_sbrked_mem;*/
+    INTERNAL_SIZE_T  max_mmapped_mem;
+    INTERNAL_SIZE_T  max_total_mem; /* only kept for NO_THREADS */
+
+    /* First address handed out by MORECORE/sbrk.  */
+    char*            sbrk_base;
+    };
+    """
+
+    _fields_ = [
+        ("trim_threshold", c_size_t),
+        ("top_pad", c_size_t),
+        ("mmap_threshold", c_size_t),
+        ("arena_test", c_size_t),
+        ("arena_max", c_size_t),
+        ("n_mmaps", ctypes.c_int32),
+        ("n_mmaps_max", ctypes.c_int32),
+        ("max_n_mmaps", ctypes.c_int32),
+        ("no_dyn_threshold", ctypes.c_int32),
+        ("pagesize", c_size_t),
+        ("mmapped_mem", c_size_t),
+        ("max_mmapped_mem", c_size_t),
+        ("max_total_mem", c_size_t),
+        ("sbrk_base", c_pvoid),
+    ]
+
 
 class c_malloc_par_2_24(Structure):
     """
@@ -796,8 +903,10 @@ class MallocPar(CStruct2GDB):
         _c_struct = c_malloc_par_2_26
     elif pwndbg.glibc.get_version() >= (2, 24):
         _c_struct = c_malloc_par_2_24
-    else:
+    elif pwndbg.glibc.get_version() >= (2, 15):
         _c_struct = c_malloc_par_2_23
+    else:
+        _c_struct = c_malloc_par_2_12
     sizeof = ctypes.sizeof(_c_struct)
 
 
@@ -824,8 +933,10 @@ DEFAULT_MP_.n_mmaps_max = DEFAULT_MMAP_MAX
 DEFAULT_MP_.mmap_threshold = DEFAULT_MMAP_THRESHOLD
 DEFAULT_MP_.trim_threshold = DEFAULT_TRIM_THRESHOLD
 DEFAULT_MP_.arena_test = 2 if pwndbg.gdblib.arch.ptrsize == 4 else 8
-if MallocPar._c_struct != c_malloc_par_2_23:
+if (MallocPar._c_struct != c_malloc_par_2_23) and (MallocPar._c_struct != c_malloc_par_2_12):
     # the only difference between 2.23 and the rest is the lack of tcache
     DEFAULT_MP_.tcache_count = TCACHE_FILL_COUNT
     DEFAULT_MP_.tcache_bins = TCACHE_MAX_BINS
     DEFAULT_MP_.tcache_max_bytes = (TCACHE_MAX_BINS - 1) * MALLOC_ALIGN + MINSIZE - SIZE_SZ
+if(MallocPar._c_struct == c_malloc_par_2_12):
+    DEFAULT_MP_.pagesize = DEFAULT_PAGE_SIZE
