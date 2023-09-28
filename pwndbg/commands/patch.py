164231ffa9066d83fd28bd097171d9e8db580f9a
disconnect3d <dominik.b.czarnota@gmail.com>
2023-09-28 21:43:27 +0200

    patch, patch_list: improve UX/display

--- pwndbg/commands/patch.py
+++ pwndbg/commands/patch.py
@@ -18,11 +18,12 @@ patches = {}
 parser = argparse.ArgumentParser(description="Patches given instruction with given code or bytes.")
 parser.add_argument("address", type=int, help="The address to patch")
 parser.add_argument("ins", type=str, help="instruction[s]")
+parser.add_argument("-q", "--quiet", action="store_true", help="don't print anything")
 
 
 @pwndbg.commands.ArgparsedCommand(parser)
 @pwndbg.commands.OnlyWhenRunning
-def patch(address, ins) -> None:
+def patch(address, ins, quiet) -> None:
     # Make sure that any gdb.Value object is converted to int
     address = int(address)
 
@@ -35,6 +36,10 @@ def patch(address, ins) -> None:
     pwndbg.gdblib.memory.write(address, new_mem)
 
     pwndbg.lib.cache.clear_caches()
+
+    if not quiet:
+        colored_addr = pwndbg.color.memory.get(address)
+        print(f"Patched {len(new_mem)} bytes at {colored_addr}")
 
 
 parser2 = argparse.ArgumentParser(description="Revert patch at given address.")
@@ -76,14 +81,19 @@ def patch_list() -> None:
         print(message.hint("No patches to list"))
         return
 
-    print(message.hint("Patches:"))
+    print(pwndbg.color.context.banner("Patches:"))
     for addr, (old, new) in patches.items():
         old_insns = disasm(old, byte=False, offset=False)
         new_insns = disasm(new, byte=False, offset=False)
 
+        colored_addr = pwndbg.color.memory.get(addr)
+
+        old_insns, new_insns = map(
+            pwndbg.color.syntax_highlight.syntax_highlight, (old_insns, new_insns)
+        )
+
         print(
-            message.hint("Patch at"),
-            message.warn("%#x:" % addr),
+            message.hint(f"{colored_addr} ({len(new)} bytes)"),
             message.hint("\n  from:"),
             message.warn(" ".join(old_insns.replace("\n", "; ").split())),
             message.hint("\n  to  :"),
