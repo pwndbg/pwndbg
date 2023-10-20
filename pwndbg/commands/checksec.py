afd6b0cc0e9e0c5776cc2fc3c41b1c051a7a3b40
CristhianMotoche <cristhian.motoche@gmail.com>
2023-10-19 20:43:51 -0500

    attempt: Colorize checksec output

--- pwndbg/commands/checksec.py
+++ pwndbg/commands/checksec.py
@@ -1,11 +1,31 @@
 from __future__ import annotations
+from typing import cast
 
 import pwndbg.commands
 import pwndbg.gdblib.file
 import pwndbg.wrappers.checksec
 
+NEW_LINE = '\n'
+
+class colors():
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    BLUE = '\033[34m'
+    RESET = '\033[0m'
+
+def color_line(line: str) -> str:
+    return (
+        line
+        .replace('*', colors.BLUE + '*' + colors.RESET)
+        .replace(':', ':' + colors.GREEN)
+        .replace('No', colors.RED + 'No')
+    ) + colors.RESET
+
+def color_lines(lines: list[str]) -> str:
+    return NEW_LINE.join(list(map(color_line, lines)))
+
 
 @pwndbg.commands.ArgparsedCommand("Prints out the binary security settings using `checksec`.")
 @pwndbg.commands.OnlyWithFile
 def checksec() -> None:
-    print(pwndbg.wrappers.checksec.get_raw_out(pwndbg.gdblib.file.get_proc_exe_file()))
+    print(color_lines(cast(str, pwndbg.wrappers.checksec.get_raw_out(pwndbg.gdblib.file.get_proc_exe_file())).split(NEW_LINE)))
