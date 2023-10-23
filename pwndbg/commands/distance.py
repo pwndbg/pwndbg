1719922a86c84c95903fb8d0e31a333e32add315
OB <obarroncs@gmail.com>
2023-10-22 23:28:12 -0700

    Distance command can now print the offset to the addresses's page base

--- pwndbg/commands/distance.py
+++ pwndbg/commands/distance.py
@@ -2,24 +2,49 @@ from __future__ import annotations
 
 import argparse
 
+import pwndbg.color.memory as M
 import pwndbg.commands
 import pwndbg.gdblib.arch
 from pwndbg.commands import CommandCategory
 
-parser = argparse.ArgumentParser(description="Print the distance between the two arguments.")
+parser = argparse.ArgumentParser(
+    description="Print the distance between the two arguments, or print the offset to the address's page base."
+)
 parser.add_argument("a", type=int, help="The first address.")
-parser.add_argument("b", type=int, help="The second address.")
+parser.add_argument("b", nargs="?", default=None, type=int, help="The second address.")
 
 
 @pwndbg.commands.ArgparsedCommand(parser, category=CommandCategory.MEMORY)
 def distance(a, b) -> None:
     """Print the distance between the two arguments"""
-    a = int(a) & pwndbg.gdblib.arch.ptrmask
-    b = int(b) & pwndbg.gdblib.arch.ptrmask
 
-    distance = b - a
+    if b is None:
+        total_pages = pwndbg.gdblib.vmmap.get()
 
-    print(
-        "%#x->%#x is %#x bytes (%#x words)"
-        % (a, b, distance, distance // pwndbg.gdblib.arch.ptrsize)
-    )
+        # Find the page the address belongs to
+        for page in total_pages:
+            if a >= page.vaddr and a < page.end:
+                # a is a gdb.Value, explicitely convert to it
+                distance = int(a) - page.vaddr
+
+                display_text = "%#x->%#x is %#x bytes (%#x words)" % (
+                    page.vaddr,
+                    a,
+                    distance,
+                    distance // pwndbg.gdblib.arch.ptrsize,
+                )
+
+                print(M.get(page.vaddr, text=display_text))
+                return
+
+        print("%#x does not belong to a mapped page in memory" % (a))
+    else:
+        a = int(a) & pwndbg.gdblib.arch.ptrmask
+        b = int(b) & pwndbg.gdblib.arch.ptrmask
+
+        distance = b - a
+
+        print(
+            "%#x->%#x is %#x bytes (%#x words)"
+            % (a, b, distance, distance // pwndbg.gdblib.arch.ptrsize)
+        )
