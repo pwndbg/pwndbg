01e6657a4377271a20ebc4b53af6d7406c6a8ad8
OB <obarroncs@gmail.com>
2023-11-22 19:15:20 -0800

    sorted lists

--- pwndbg/commands/sigreturn.py
+++ pwndbg/commands/sigreturn.py
@@ -20,10 +20,10 @@ from pwndbg.lib.regs import i386
 # Grab frame values from pwntools. Offsets are defined as the offset to stack pointer when syscall instruction is called
 # Offsets and names are from Linux kernel source. For example x86_64 is defined in CONFIG_X86_64 struct rt_sigframe (Linux Kernel /arch/x86/include/asm/sigframe.h)
 SIGRETURN_FRAME_LAYOUTS: dict[str, list[Tuple[int, str]]] = {
-    "x86-64": [(-8, "&pretcode")] + list(pwnlib.rop.srop.registers["amd64"].items()),
-    "i386": list(pwnlib.rop.srop.registers["i386"].items()),
-    "aarch64": list(pwnlib.rop.srop.registers["aarch64"].items()),
-    "arm": list(pwnlib.rop.srop.registers["arm"].items()),
+    "x86-64": sorted([(-8, "&pretcode")] + list(pwnlib.rop.srop.registers["amd64"].items())),
+    "i386": sorted(list(pwnlib.rop.srop.registers["i386"].items())),
+    "aarch64": sorted(list(pwnlib.rop.srop.registers["aarch64"].items())),
+    "arm": sorted(list(pwnlib.rop.srop.registers["arm"].items())),
 }
 
 # Always print these registers (as well as flag register, eflags / cpsr)
