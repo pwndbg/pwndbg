427bf8c96ea75163981ecefecdbffa4ea84ffdd5
intrigus-lgtm <60750685+intrigus-lgtm@users.noreply.github.com>
2023-12-04 11:44:16 +0100

    Port gdb-tests from bash to python (#1916)
    * [WIP] Port gdb-tests from bash to python
    
    * Use threads instead of processes
    
    * Port gdb tests to python
    
    * Linting
    
    * Fix coverage "again"
    
    * Remove bash tests
    
    ---------
    
    Co-authored-by: intrigus <abc123zeus@live.de>

--- tests.sh
+++ tests.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Check some basic test dependencies
-if ! command -v env_parallel &> /dev/null; then
-    echo 'Error: The `env_parallel` command could not be found. You should run `setup-dev.sh` to install development dependencies.'
-    echo '(Alternatively, run ./tests.sh with `--serial` to skip using parallel test running. However, if `env_parallel` is missing, it is likely that other dependencies like the `zig` compiler are also missing)'
-    exit
-fi
-
 # Run integration tests
-(cd tests/gdb-tests && ./tests.sh $@)
+(cd tests/gdb-tests && python3 tests.py $@)
 
 # Run unit tests
 # coverage run -m pytest tests/unit-tests
