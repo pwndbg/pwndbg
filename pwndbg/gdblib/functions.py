6d151e346c856650856c5459f54f32b381b5bbc6
HackSys Team <ashfaq_ansari1989@hotmail.com>
2023-12-22 07:16:25 -0500

    FIX: more type hints update

--- pwndbg/gdblib/functions.py
+++ pwndbg/gdblib/functions.py
@@ -7,20 +7,24 @@ This file might be changed into a module in the future.
 from __future__ import annotations
 
 import functools
+from typing import Any
+from typing import Callable
+from typing import List
 
 import gdb
 
+import pwndbg.gdblib.elf
 import pwndbg.gdblib.proc
 
-functions = []
+functions: List[_GdbFunction] = []
 
 
-def GdbFunction(only_when_running=False):
+def GdbFunction(only_when_running: bool = False) -> Callable[..., Any]:
     return functools.partial(_GdbFunction, only_when_running=only_when_running)
 
 
 class _GdbFunction(gdb.Function):
-    def __init__(self, func, only_when_running) -> None:
+    def __init__(self, func: Callable[..., Any], only_when_running: bool) -> None:
         self.name = func.__name__
         self.func = func
         self.only_when_running = only_when_running
@@ -32,7 +36,7 @@ class _GdbFunction(gdb.Function):
         functools.update_wrapper(self, func)
         self.__doc__ = func.__doc__
 
-    def invoke(self, *args):
+    def invoke(self, *args: gdb.Value) -> Any:
         if self.only_when_running and not pwndbg.gdblib.proc.alive:
             # Returning empty string is a workaround that we can't stop e.g. `break *$rebase(offset)`
             # Thx to that, gdb will print out 'evaluation of this expression requires the target program to be active'
@@ -40,12 +44,12 @@ class _GdbFunction(gdb.Function):
 
         return self.func(*args)
 
-    def __call__(self, *args):
+    def __call__(self, *args: gdb.Value) -> Any:
         return self.invoke(*args)
 
 
 @GdbFunction(only_when_running=True)
-def rebase(addr):
+def rebase(addr: gdb.Value | int) -> int:
     """Return rebased address."""
     base = pwndbg.gdblib.elf.exe().address
     return base + int(addr)
