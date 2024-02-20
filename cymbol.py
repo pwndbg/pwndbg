15bd48b05fe4b3211f91106d864597c8d7232672
Austin Dunnigan <30908498+audi99@users.noreply.github.com>
2024-02-20 11:51:20 -0500

    typo/error

--- cymbol.py
+++ cymbol.py
@@ -140,6 +140,7 @@ def add_custom_structure(custom_structure_name: str) -> None:
     # Avoid checking for file existance. Call the decorator wrapper directly.
     load_custom_structure.__wrapped__(custom_structure_name, pwndbg_custom_structure_path)
 
+<<<<<<< HEAD
 
 def add_custom_structure_from_file(header_file_path: str) -> None:
     with open(header_file_path, "r") as file:
@@ -157,24 +158,8 @@ def add_custom_structure_from_file(header_file_path: str) -> None:
 
     print(message.success(f"Custom structure from {header_file_path} added."))
     load_custom_structure(custom_structure_name)
-
-
-def add_custom_structure_from_file(header_file_path: str) -> None:
-    with open(header_file_path, "r") as file:
-        custom_structures_source = file.read().strip()
-
-    if not custom_structures_source:
-        print(message.notice("The header file is empty or does not contain valid structures."))
-        return
-
-    custom_structure_name = os.path.splitext(os.path.basename(header_file_path))[0]
-    pwndbg_custom_structure_path = os.path.join(pwndbg_cachedir, custom_structure_name) + ".c"
-
-    with open(pwndbg_custom_structure_path, "w") as f:
-        f.write(custom_structures_source)
-
-    print(message.success(f"Custom structure from {header_file_path} added."))
-    load_custom_structure(custom_structure_name)
+=======
+>>>>>>> parent of 005f1ec6 (extend cymbol command to support struct loading from header files)
 
 
 @OnlyWhenStructFileExists
@@ -280,14 +265,6 @@ parser.add_argument(
     "--show",
     metavar="name",
     help="Show the source code of an existing custom structure",
-    default=None,
-    type=str,
-)
-parser.add_argument(
-    "-f",
-    "--file",
-    metavar="header_file",
-    help="Path to a C header file containing struct definitions",
     default=None,
     type=str,
 )
