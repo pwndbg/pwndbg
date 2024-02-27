739e47c9e32acf7aac6ec439e3b9adb9db248ad7
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2024-02-27 23:21:40 +0800

    Call `pwndbg.lib.tempfile.cachedir` only once

--- pwndbg/gdblib/onegadget.py
+++ pwndbg/gdblib/onegadget.py
@@ -52,6 +52,7 @@ ONEGADGET_COLOR = {
     "light_green": lambda x: colorize(x, "\x1b[38;5;82m"),
     "light_purple": lambda x: colorize(x, "\x1b[38;5;153m"),
 }
+ONEGADGET_CACHEDIR = pwndbg.lib.tempfile.cachedir("onegadget")
 
 
 class CheckSatResult(Enum):
@@ -269,9 +270,7 @@ def run_onegadget() -> str:
         pwndbg.glibc.get_libc_filename_from_info_sharedlibrary()
     )
     # We need cache because onegadget might be slow
-    cache_file = os.path.join(
-        pwndbg.lib.tempfile.cachedir("onegadget"), compute_file_hash(libc_path)
-    )
+    cache_file = os.path.join(ONEGADGET_CACHEDIR, compute_file_hash(libc_path))
     if os.path.exists(cache_file):
         # Cache hit
         with open(cache_file, "r") as f:
