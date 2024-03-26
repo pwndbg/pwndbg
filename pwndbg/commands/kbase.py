2f7a4f528d393cedbe98783b01ccb0c6cfa51151
Charif Mostafa <charif@chrf.dev>
2024-03-26 15:32:48 +0100

    add comments

--- pwndbg/commands/kbase.py
+++ pwndbg/commands/kbase.py
@@ -24,11 +24,11 @@ def kbase() -> None:
 
     arch_name = pwndbg.gdblib.arch.name
     if arch_name == "x86-64":
-        # 0x48 first opcode in older kernels
-        # 0xFC first opcode in newer kernels
+        # first opcodes: 0x48: 5.x - 6.1.x, 0x49: > 6.6, 0xFC: 6.7.1
         magic = [0x48, 0x49, 0xFC]
     elif arch_name == "aarch64":
-        # First byte of "MZ" header
+        # searches for the first byte of the "MZ" header
+        # or the first opcode of the executable mapping (fixed offset 0x10000)
         magic = [0x4D, 0x5F, 0xE0, 0xE9]
     else:
         print(M.error(f"kbase does not support the {arch_name} architecture"))
@@ -38,6 +38,8 @@ def kbase() -> None:
     for mapping in mappings:
         # TODO: Check alignment
 
+        # only search in kernel mappings:
+        # https://www.kernel.org/doc/html/v5.3/arm64/memory.html
         if not mapping.vaddr & (0xFFFF << 48):
             continue
         if not mapping.execute:
@@ -54,6 +56,7 @@ def kbase() -> None:
         if b in magic:
             base = mapping.vaddr
             if b != 0x4D:
+                # "MZ" header not found subtract 0x10000 from the following mapping to get the kbase
                 base -= 0x10000
             print(M.success(f"Found virtual base address: {base:#x}"))
             break
