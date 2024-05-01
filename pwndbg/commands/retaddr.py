1ff11c01f23930d33ea14a8a1f555c3e3e8e0041
Gulshan Singh <gsingh2011@gmail.com>
2024-04-30 22:09:10 -0700

    Add pwndbg.gdblib.stack.callstack and use it in retaddr

--- pwndbg/commands/retaddr.py
+++ pwndbg/commands/retaddr.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import gdb
-
 import pwndbg.chain
 import pwndbg.commands
 import pwndbg.gdblib.arch
@@ -15,19 +13,12 @@ from pwndbg.commands import CommandCategory
 )
 @pwndbg.commands.OnlyWhenRunning
 def retaddr() -> None:
+    addresses = pwndbg.gdblib.stack.callstack()
+
     sp = pwndbg.gdblib.regs.sp
     stack = pwndbg.gdblib.vmmap.find(sp)
 
-    # Enumerate all return addresses
-    frame = gdb.newest_frame()
-    addresses = []
-    while frame:
-        addr = int(frame.pc())
-        if pwndbg.gdblib.memory.is_readable_address(addr):
-            addresses.append(addr)
-        frame = frame.older()
-
-    # Find all of them on the stack
+    # Find all return addresses on the stack
     start = stack.vaddr
     stop = start + stack.memsz
     while addresses and start < sp < stop:
