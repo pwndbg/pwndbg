d5a3de38b7d8fb0d03836a2fa65d3411814dc983
Gulshan Singh <gsingh2011@gmail.com>
2024-05-02 23:49:08 -0700

    Add start_and_break_on decorator

--- tests/gdb-tests/tests/__init__.py
+++ tests/gdb-tests/tests/__init__.py
@@ -1,3 +1,31 @@
 from __future__ import annotations
 
+import functools
+
+import gdb
+
+import tests
+
 from . import binaries
+
+
+def start_and_break_on(binary, bps, *args):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            path = binaries.get(binary)
+
+            gdb.execute("file " + path)
+            gdb.execute("set exception-verbose on")
+            gdb.execute("starti " + " ".join(args))
+
+            if bps is not None and len(bps) > 0:
+                for bp in bps:
+                    gdb.execute(f"break {bp}")
+                gdb.execute("continue")
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
