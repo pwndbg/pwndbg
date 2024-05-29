8ef4a46f9f4a6852832fa8ceb6eb5a8bd06d2348
Gulshan Singh <gsingh2011@gmail.com>
2024-05-29 00:18:27 -0700

    Only check for "/ls" in `which` test

--- tests/unit-tests/test_which.py
+++ tests/unit-tests/test_which.py
@@ -21,7 +21,7 @@ from pwndbg.lib.which import which
 
 
 def test_basic():
-    assert which("ls") in ["/usr/bin/ls", "/usr/sbin/ls", "/bin/ls"]
+    assert which("ls").endswith("/ls")
 
 
 def test_nonexistent():
