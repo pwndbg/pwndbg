d6abb33e02fb7968827f21798d36c2496cfe3948
Gulshan Singh <gsingh2011@gmail.com>
2024-05-29 00:33:37 -0700

    Fix and re-enable unit tests (#2182)
    * Add gdb_version to mock gdblib
    
    * Re-enable unit tests
    
    * Only collect unit test coverage if --cov is passed
    
    * Source venv before running tests in github action
    
    * Add venv path PATH in to Dockerfile
    
    * Only check for "/ls" in `which` test

--- tests.sh
+++ tests.sh
@@ -3,5 +3,17 @@
 # Run integration tests
 (cd tests/gdb-tests && python3 tests.py $@)
 
+COV=0
 # Run unit tests
-# coverage run -m pytest tests/unit-tests
+for arg in "$@"; do
+    if [ "$arg" == "--cov" ]; then
+        COV=1
+        break
+    fi
+done
+
+if [ $COV -eq 1 ]; then
+    coverage run -m pytest tests/unit-tests
+else
+    pytest tests/unit-tests
+fi
