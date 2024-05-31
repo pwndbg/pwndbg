b0636e0145de852380450fd7e51e58dea1d0f693
B1N4RY-P4R45173 <kopakaajay123@gmail.com>
2024-05-31 16:56:31 +0530

    Added a file to mark dev installation and also modified gdbinit.py to calculate and verify the hash of poetry.lock

--- gdbinit.py
+++ gdbinit.py
@@ -5,6 +5,11 @@ import os
 import site
 import sys
 import time
+import hashlib
+import subprocess
+import os
+
+from pwndbg import config
 from glob import glob
 from os import environ
 from os import path
@@ -16,8 +21,46 @@ if environ.get("PWNDBG_PROFILE") == "1":
     _start_time = time.time()
     _profiler.enable()
 
+
 # Get virtualenv's site-packages path
 venv_path = os.environ.get("PWNDBG_VENV_PATH")
+
+def calculate_hash(file_path):
+    with open(file_path, 'rb') as f:
+        file_hash = hashlib.sha256()
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+    return file_hash.hexdigest()
+
+def run_poetry_install(dev=False):
+    command = ['poetry', 'install']
+    if dev:
+        command = ['poetry', 'install', '--with', 'dev']
+    subprocess.run(command, check=True)
+
+PWNDBG_VENV_PATH = os.getenv('PWNDBG_VENV_PATH')
+POETRY_LOCK_PATH = os.path.join(os.path.dirname(__file__), 'poetry.lock')
+POETRY_LOCK_HASH_PATH = os.path.join(PWNDBG_VENV_PATH, 'poetry.lock.hash')
+DEV_MARKER_PATH = os.path.join(PWNDBG_VENV_PATH, 'dev.marker')
+
+# verify virtual environment path exists
+os.makedirs(PWNDBG_VENV_PATH, exist_ok=True)
+
+current_hash = calculate_hash(POETRY_LOCK_PATH)
+if os.path.exists(POETRY_LOCK_HASH_PATH):
+    with open(POETRY_LOCK_HASH_PATH, 'r') as f:
+        stored_hash = f.read().strip()
+else:
+    stored_hash = None
+
+dev_mode = os.path.exists(DEV_MARKER_PATH) # checks if dev.Marker exists
+
+# if hashes doesn't match, run the appropriate command based dev.marker file 
+if current_hash != stored_hash:
+    run_poetry_install(dev=dev_mode)
+    with open(POETRY_LOCK_HASH_PATH, 'w') as f:
+        f.write(current_hash)
+
 if venv_path == "PWNDBG_PLEASE_SKIP_VENV" or path.exists(path.dirname(__file__) + "/.skip-venv"):
     pass
 else:
