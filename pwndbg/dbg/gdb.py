681c5c2a7691a18cc8fa3790030fdb3776ae8914
Matheus Branco Borella <dark.ryu.550@gmail.com>
2024-06-12 22:41:03 -0300

    Add required structures to the Debugger API

--- pwndbg/dbg/gdb.py
+++ pwndbg/dbg/gdb.py
@@ -15,6 +15,90 @@ from pwndbg.gdblib import gdb_version
 from pwndbg.gdblib import load_gdblib
 from pwndbg.gdblib import prompt
 
+class GDBType(pwndbg.dbg_mod.Type):
+    CODE_MAPPING = {
+        gdb.TYPE_CODE_INT: pwndbg.dbg_mod.TypeCode.INT,
+        gdb.TYPE_CODE_UNION: pwndbg.dbg_mod.TypeCode.UNION,
+        gdb.TYPE_CODE_STRUCT: pwndbg.dbg_mod.TypeCode.STRUCT,
+        gdb.TYPE_CODE_ENUM: pwndbg.dbg_mod.TypeCode.ENUM,
+        gdb.TYPE_CODE_TYPEDEF: pwndbg.dbg_mod.TypeCode.TYPEDEF,
+        gdb.TYPE_CODE_PTR: pwndbg.dbg_mod.TypeCode.POINTER,
+        gdb.TYPE_CODE_ARRAY: pwndbg.dbg_mod.TypeCode.ARRAY,
+    }
+
+    def __init__(self, inner: gdb.Type):
+        self.inner = inner
+
+    @property
+    @override
+    def alignof(self):
+        return self.inner.alignof
+
+    @property
+    @override
+    def code(self):
+        assert self.inner.code in CODE_MAPPING, "missing mapping for type code"
+        return CODE_MAPPING[self.inner.code]
+
+    @override
+    def fields(self):
+        return [pwndbg.dbg_mod.TypeField(
+            field.bitpos,
+            field.name,
+            field.type,
+            field.parent_type,
+            field.enumval,
+            field.artificial,
+            field.is_base_class,
+            field.bitsize) for field in self.inner.fields]
+
+    @override
+    def array(self):
+        return GDBType(self.inner.array())
+
+    @override
+    def pointer(self):
+        return GDBType(self.inner.pointer())
+
+    @override
+    def strip_typedefs(self):
+        return GDBType(self.inner.strip_typedefs())
+
+    @override
+    def target(self):
+        return GDBType(self.inner.target())
+
+class GDBValue(pwndbg.dbg_mod.Value):
+    def __init__(self, inner: gdb.Value):
+        self.inner = value
+
+    @property
+    @override
+    def address(self):
+        return GDBValue(self.inner.address)
+
+    @property
+    @override
+    def is_optimized_out(self):
+        return self.inner.is_optimized_out
+
+    @property
+    @override
+    def type(self):
+        return GDBType(self.inner.type)
+
+
+    @override
+    def dereference(self):
+        return GDBValue(self.inner.dereference())
+
+    @override
+    def string(self):
+        return self.inner.string()
+
+    @override
+    def fetch_lazy(self):
+        self.inner.fetch_lazy()
 
 class GDB(pwndbg.dbg_mod.Debugger):
     @override
