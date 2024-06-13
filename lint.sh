b98aa073097af0d65df0ef503113778509e9b35f
Gulshan Singh <gsingh2011@gmail.com>
2024-06-12 23:34:47 -0700

    Make sure the vermin linter runs on all files

--- lint.sh
+++ lint.sh
@@ -80,7 +80,7 @@ else
 fi
 
 # Checking minimum python version
-vermin -vvv --no-tips -t=3.8- --eval-annotations --violations ./pwndbg/
+vermin -vvv --no-tips -t=3.8- --eval-annotations --violations ${LINT_FILES}
 
 # mypy is run in a separate step on GitHub Actions
 if [[ -z "$GITHUB_ACTIONS" ]]; then
