c43cfceec100b84d59a8d5f13001d7b1a474ad2b
OB <obarroncs@gmail.com>
2024-07-07 02:55:04 -0700

    revert path change

--- tests/qemu-tests/tests.sh
+++ tests/qemu-tests/tests.sh
@@ -180,7 +180,7 @@ process_output() {
     fi
 
     read -r testname result < <(
-        echo "$output" | grep -Po '(^qemu-tests/tests/[^ ]+)|(\x1b\[3.m(PASSED|FAILED|SKIPPED|XPASS|XFAIL)\x1b\[0m)' \
+        echo "$output" | grep -Po '(^tests/[^ ]+)|(\x1b\[3.m(PASSED|FAILED|SKIPPED|XPASS|XFAIL)\x1b\[0m)' \
             | tr '\n' ' ' \
             | cut -d ' ' -f 1,2
     )
