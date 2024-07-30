d32157a675180e97fadf5668f3123bdb95b44282
Matheus Branco Borella <dark.ryu.550@gmail.com>
2024-07-30 16:09:28 -0300

    Revert pwndbg/lldblib/__init__.py to upstream version

--- pwndbg/lldblib/__init__.py
+++ pwndbg/lldblib/__init__.py
@@ -1,15 +1,9 @@
 from __future__ import annotations
 
-from typing import Any
 
-import lldb
+def register_class_as_cmd(debugger, cmd, c):
+    mod = c.__module__
+    name = c.__qualname__
+    name = f"{mod if mod else ''}.{name}"
 
-
-def register_class_as_cmd(debugger: lldb.SBDebugger, cmd: str, handler: Any, path: str = None):
-    name = path
-    if not name:
-        mod = handler.__module__
-        name = handler.__qualname__
-        name = f"{mod if mod else ''}.{name}"
-
-    debugger.HandleCommand(f"command script add -c {name} -s synchronous {cmd}")
+    print(debugger.HandleCommand(f"command script add -c {name} -s synchronous {cmd}"))
