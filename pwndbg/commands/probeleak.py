d22b0597c42e69da1d0e1b730fdf8f427b8cc5da
0xhebi <21058883+0xhebi@users.noreply.github.com>
2024-08-11 22:11:53 +0200

    added example for #1967 feature

--- pwndbg/commands/probeleak.py
+++ pwndbg/commands/probeleak.py
@@ -56,6 +56,8 @@ Examples:
     probeleak $rsp 0x64 --max-dist 0x10 - as above, but pointers may point 0x10 bytes outside of memory page
     probeleak $rsp 0x64 --point-to libc --max-ptrs 1 --flags rwx - leaks 0x64 bytes starting at stack pointer and \
 search for one valid pointer which points to a libc rwx page
+    probeleak $rsp 0x1200 --page-name libc --permissions r - search for pointers by page name and with permissions \
+that would include read "r" 
 """,
 )
 parser.add_argument("address", nargs="?", default="$sp", help="Leak memory address")
