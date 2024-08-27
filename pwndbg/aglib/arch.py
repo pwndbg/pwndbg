5e03196aee77afe6ac9fb1951d0a75d6fe56bf4a
Disconnect3d <dominik.b.czarnota@gmail.com>
2024-08-28 01:40:11 +0200

    Update pwndbg/aglib/arch.py

--- pwndbg/aglib/arch.py
+++ pwndbg/aglib/arch.py
@@ -9,7 +9,7 @@ from pwndbg.lib.arch import Arch
 
 # We will optimize this module in the future, by having it work in the same
 # way the `gdblib` version of it works, and that will come at the same
-# time this module gets expanded to have the full feature set of its `gdlib`
+# time this module gets expanded to have the full feature set of its `gdblib`
 # counterpart. For now, though, this should be good enough.
 
 
