9cf181eecc12842f5be10e31fd55cbd52e6792a5
Matheus Branco Borella <dark.ryu.550@gmail.com>
2024-08-27 20:45:28 -0300

    Remove outdated comment about `pwndbg.aglib.arch`

--- pwndbg/aglib/arch.py
+++ pwndbg/aglib/arch.py
@@ -7,11 +7,6 @@ import pwnlib
 import pwndbg
 from pwndbg.lib.arch import Arch
 
-# We will optimize this module in the future, by having it work in the same
-# way the `gdblib` version of it works, and that will come at the same
-# time this module gets expanded to have the full feature set of its `gdblib`
-# counterpart. For now, though, this should be good enough.
-
 
 ARCHS = (
     "x86-64",
