ef9d219e7fc1e73fd9d1a1ff2ed79ef8aae9b4b2
Disconnect3d <dominik.b.czarnota@gmail.com>
2024-08-28 01:39:54 +0200

    Update pwndbg/aglib/arch.py

--- pwndbg/aglib/arch.py
+++ pwndbg/aglib/arch.py
@@ -10,7 +10,7 @@ from pwndbg.lib.arch import Arch
 # We will optimize this module in the future, by having it work in the same
 # way the `gdblib` version of it works, and that will come at the same
 # time this module gets expanded to have the full feature set of its `gdlib`
-# coutnerpart. For now, though, this should be good enough.
+# counterpart. For now, though, this should be good enough.
 
 
 ARCHS = (
