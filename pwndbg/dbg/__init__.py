7ed76041b9cdc7e58a6a62be82a827d17135df8c
Itay Snir <63191638+itaysnir@users.noreply.github.com>
2024-09-03 18:27:50 +0300

    Add __repr__ and __str__ methods to GDBValue (#2408)
    * Update __init__.py
    
    * Update gdb.py

--- pwndbg/dbg/__init__.py
+++ pwndbg/dbg/__init__.py
@@ -720,6 +720,12 @@ class Value:
         """
         raise NotImplementedError()
 
+    def __repr__(self):
+        raise NotImplementedError()
+
+    def __str__(self):
+        raise NotImplementedError()
+
 
 class CommandHandle:
     """
