abe3c6905b2222672b12afd1a713a5b2447f9f2e
cfreal <folcharles@gmail.com>
2024-09-12 08:34:21 +0200

    Fixed bug where some error message in plist displayed 0x0x{address} due to incorrect formatting

--- pwndbg/commands/plist.py
+++ pwndbg/commands/plist.py
@@ -416,7 +416,7 @@ def plist(
 
             print(f"{target_address:#x} {symbol}: {value}")
         except gdb.error as e:
-            print(message.error(f"Cannot dereference 0x{address:#x} for list link #{i + 1}: {e}"))
+            print(message.error(f"Cannot dereference {address:#x} for list link #{i + 1}: {e}"))
             print(message.error("Is the linked list corrupted or is the sentinel value wrong?"))
             return
 
