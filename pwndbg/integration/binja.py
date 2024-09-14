30bc78dfee90e7cecb7456f451829b7c1747a072
gfelber <34159565+gfelber@users.noreply.github.com>
2024-09-14 20:58:01 +0200

    fixed binja integration issues
    + ensure current process is alive, for needed events
    + handle ConnectionErrors correctly

--- pwndbg/integration/binja.py
+++ pwndbg/integration/binja.py
@@ -153,9 +153,14 @@ def init_bn_rpc_client() -> None:
 
 
 def with_bn(fallback: K = None) -> Callable[[Callable[P, T]], Callable[P, T | K]]:
+    global _bn
+
     def decorator(func: Callable[P, T]) -> Callable[P, T | K]:
+        global _bn
+
         @functools.wraps(func)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> T | K:
+            global _bn
             if _bn is None:
                 init_bn_rpc_client()
 
@@ -163,8 +168,9 @@ def with_bn(fallback: K = None) -> Callable[[Callable[P, T]], Callable[P, T | K]
                 if _bn is not None:
                     return func(*args, **kwargs)
             except ConnectionRefusedError:
-                _bn = None
                 print(message.error("[!] Binary Ninja connection refused"))
+                _bn = None
+
             return fallback
 
         return wrapper
@@ -206,7 +212,7 @@ def base():
 @pwndbg.dbg.event_handler(EventType.STOP)
 @with_bn()
 def auto_update_pc() -> None:
-    if not pwndbg.gdblib.procs.alive:
+    if not pwndbg.gdblib.proc.alive:
         return
     pc = pwndbg.gdblib.regs.pc
     if bn_autosync.value:
@@ -222,7 +228,7 @@ _managed_bps: Dict[int, gdb.Breakpoint] = {}
 @pwndbg.dbg.event_handler(EventType.CONTINUE)
 @with_bn()
 def auto_update_bp() -> None:
-    if not pwndbg.gdblib.procs.alive:
+    if not pwndbg.gdblib.proc.alive:
         return
     bps: List[int] = _bn.get_bp_tags()
     binja_bps = {r2l(addr) for addr in bps}
