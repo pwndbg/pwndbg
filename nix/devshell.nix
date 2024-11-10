73f0787e8284d590175e57d9971d3d8403fcb0f7
patryk4815 <patryk.sondej@gmail.com>
2024-11-10 17:55:37 +0100

    nix fmt

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -1,7 +1,6 @@
 # This should be kept in sync with setup-dev.sh and lint.sh requirements
 {
-  pkgs ?
-    # If pkgs is not defined, instantiate nixpkgs from locked commit
+  pkgs ? # If pkgs is not defined, instantiate nixpkgs from locked commit
     let
       lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
       nixpkgs = fetchTarball {
@@ -17,7 +16,12 @@
 }:
 let
   pyEnv = import ./pyenv.nix {
-    inherit pkgs python3 inputs isLLDB;
+    inherit
+      pkgs
+      python3
+      inputs
+      isLLDB
+      ;
     lib = pkgs.lib;
     isDev = true;
   };
@@ -26,22 +30,27 @@ in
   default = pkgs.mkShell {
     NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
     # Anything not handled by the poetry env
-    nativeBuildInputs = (with pkgs; [
-      # from setup-dev.sh
-      nasm
-      gcc
-      curl
-      gdb
-      parallel
-      qemu
-      netcat-openbsd
-      zig_0_10 # matches setup-dev.sh
-      go
+    nativeBuildInputs =
+      (with pkgs; [
+        # from setup-dev.sh
+        nasm
+        gcc
+        curl
+        gdb
+        parallel
+        qemu
+        netcat-openbsd
+        zig_0_10 # matches setup-dev.sh
+        go
 
-      pyEnv
-    ]) ++ pkgs.lib.optionals isLLDB (with pkgs; [
-      lldb_19
-    ]);
+        pyEnv
+      ])
+      ++ pkgs.lib.optionals isLLDB (
+        with pkgs;
+        [
+          lldb_19
+        ]
+      );
     shellHook = ''
       export PWNDBG_VENV_PATH="PWNDBG_PLEASE_SKIP_VENV"
       export ZIGPATH="${pkgs.lib.getBin pkgs.zig_0_10}/bin/"
