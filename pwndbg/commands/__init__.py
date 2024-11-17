3509e012b6f7ef2012e214dff0ad7f2a2ae9b675
patryk4815 <patryk.sondej@gmail.com>
2024-11-17 05:57:19 +0100

    commands/gdt.py: port to aglib

--- pwndbg/commands/__init__.py
+++ pwndbg/commands/__init__.py
@@ -696,7 +696,6 @@ def load_commands() -> None:
         import pwndbg.commands.branch
         import pwndbg.commands.cymbol
         import pwndbg.commands.dt
-        import pwndbg.commands.gdt
         import pwndbg.commands.godbg
         import pwndbg.commands.got
         import pwndbg.commands.got_tracking
@@ -737,6 +736,7 @@ def load_commands() -> None:
     import pwndbg.commands.dumpargs
     import pwndbg.commands.elf
     import pwndbg.commands.flags
+    import pwndbg.commands.gdt
     import pwndbg.commands.ghidra
     import pwndbg.commands.heap
     import pwndbg.commands.hex2ptr
