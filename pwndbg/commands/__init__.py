ab0d591278b0d1e7ca814116561c0f7a776bbf0b
patryk4815 <patryk.sondej@gmail.com>
2024-11-17 03:01:26 +0100

    Port commands to agnostic: tls, threads (partially)

--- pwndbg/commands/__init__.py
+++ pwndbg/commands/__init__.py
@@ -730,7 +730,6 @@ def load_commands() -> None:
         import pwndbg.commands.slab
         import pwndbg.commands.start
         import pwndbg.commands.tips
-        import pwndbg.commands.tls
         import pwndbg.commands.version
 
     import pwndbg.commands.canary
@@ -763,6 +762,7 @@ def load_commands() -> None:
     import pwndbg.commands.sigreturn
     import pwndbg.commands.spray
     import pwndbg.commands.telescope
+    import pwndbg.commands.tls
     import pwndbg.commands.valist
     import pwndbg.commands.vmmap
     import pwndbg.commands.windbg
