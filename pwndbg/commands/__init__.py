5cbb8636aea6da80f4a93a2eb0253d506d9bfa6c
patryk4815 <patryk.sondej@gmail.com>
2024-11-17 05:55:52 +0100

    commands/ghidra.py: port to aglib

--- pwndbg/commands/__init__.py
+++ pwndbg/commands/__init__.py
@@ -697,7 +697,6 @@ def load_commands() -> None:
         import pwndbg.commands.cymbol
         import pwndbg.commands.dt
         import pwndbg.commands.gdt
-        import pwndbg.commands.ghidra
         import pwndbg.commands.godbg
         import pwndbg.commands.got
         import pwndbg.commands.got_tracking
@@ -738,6 +737,7 @@ def load_commands() -> None:
     import pwndbg.commands.dumpargs
     import pwndbg.commands.elf
     import pwndbg.commands.flags
+    import pwndbg.commands.ghidra
     import pwndbg.commands.heap
     import pwndbg.commands.hex2ptr
     import pwndbg.commands.hexdump
