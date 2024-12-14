07d03cf9a0ad63cc2d58677335183c00957a9018
patryk4815 <patryk.sondej@gmail.com>
2024-12-14 02:12:56 +0100

    add readline test

--- tests/gdb-tests/tests/test_readline.py
+++ tests/gdb-tests/tests/test_readline.py
@@ -2,13 +2,8 @@ from __future__ import annotations
 
 import sys
 
-import mocks.gdb  # noqa: F401
-import mocks.gdblib  # noqa: F401
 
-import pwndbg  # noqa: F401
-
-
-def test_readline_not_imported():
+def test_readline_import_error():
     """
     Importing CPython readline breaks GDB's use of GNU readline.
     This breaks GDB tab autocomplete.
@@ -18,4 +13,8 @@ def test_readline_not_imported():
 
     For more info see https://github.com/pwndbg/pwndbg/issues/2232
     """
+    try:
+        import readline  # noqa: F401
+    except ImportError:
+        pass
     assert "readline" not in sys.modules
