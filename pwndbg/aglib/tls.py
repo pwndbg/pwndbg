4e9fd535eaad159552acf6c9d3d7d51a4e0957a1
patryk4815 <patryk.sondej@gmail.com>
2024-12-15 08:58:44 +0100

    fix tls for aarch64 and arm

--- pwndbg/aglib/tls.py
+++ pwndbg/aglib/tls.py
@@ -27,9 +27,9 @@ def __call_pthread_self() -> int:
 
 def find_address_with_pthread_self() -> int:
     """Get the address of TLS with pthread_self()."""
-    if pwndbg.aglib.arch.current not in ("x86-64", "i386", "arm"):
-        # Note: we should support aarch64 if it's possible that TPIDR_EL0 register can not be accessed.
+    if pwndbg.aglib.arch.current not in ("x86-64", "i386", "arm", "aarch64"):
         return 0
+
     result = __call_pthread_self()
     if result <= 0:
         # pthread_self() is not valid
@@ -43,10 +43,13 @@ def find_address_with_pthread_self() -> int:
     # For i386 and x86-64, the return value of the pthread_self() is the address of TLS, because the value is self reference of the TLS: https://elixir.bootlin.com/glibc/glibc-2.37/source/nptl/pthread_create.c#L671
     # But for arm, the implementation of THREAD_SELF is different, we need to add sizeof(struct pthread) to the result to get the address of TLS.
 
-    if pwndbg.aglib.arch.current == "arm":
-        # 0x4c0 is sizeof(struct pthread)
-        # TODO: we might need to adjust the value if the size of struct pthread is changed in the future.
-        result += 0x4C0
+    if pwndbg.aglib.arch.current in ("arm", "aarch64"):
+        pthread_type = pwndbg.aglib.typeinfo.load("struct pthread")
+        if pthread_type is None:
+            # Type 'pthread' not found
+            return 0
+        result += pthread_type.sizeof
+
     return result
 
 
@@ -57,6 +60,8 @@ def find_address_with_register() -> int:
     elif pwndbg.aglib.arch.current == "i386":
         return int(pwndbg.aglib.regs.gsbase)
     elif pwndbg.aglib.arch.current == "aarch64":
-        return int(pwndbg.aglib.regs.TPIDR_EL0)
+        # FIXME: cleanup/remove `TPIDR_EL0` register, it was renamed to `tpidr` since GDB14+
+        return int(pwndbg.aglib.regs.tpidr or pwndbg.aglib.regs.TPIDR_EL0 or 0)
+
     # TODO: is it possible that we can get the address of TLS with register on arm?
     return 0
