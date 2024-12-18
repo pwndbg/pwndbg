a7516979bbd8ede0a1c94b7f7e37f106e7ca945d
patryk4815 <patryk.sondej@gmail.com>
2024-12-18 03:48:16 +0100

    fixy

--- pwndbg/commands/pcplist.py
+++ pwndbg/commands/pcplist.py
@@ -39,7 +39,7 @@ def print_zone(zone: int, list_num=None) -> None:
         count = 0
         for e in for_each_entry(pcp["lists"][i], "struct page", "lru"):
             count += 1
-            print(int(e))
+            print(e.value_to_human_readable())
 
         if count == 0:
             print("EMPTY")
