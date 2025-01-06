b3d7bf7c5e68fa977429c0c9993654305a87c636
Patryk Sondej <patryk.sondej@grupawp.pl>
2025-01-06 16:35:01 +0100

    Fix docker

--- pwndbg/aglib/file.py
+++ pwndbg/aglib/file.py
@@ -113,7 +113,7 @@ def get_file(path: str, try_local_path: bool = False) -> str:
                 f"pwndbg.aglib.file.get_file({path}) returns local path as we can't download file"
             )
         )
-        raise OSError(f"Path '{local_path}' does not exist", errno.ENODEV)
+        raise OSError(f"File '{local_path}' does not exist", errno.ENODEV)
 
     # FIXME: get_sysroot, if nonempty only then get-local-file by default
     #   GDB is only getting local files when `set sysroot /` in remote debugging
