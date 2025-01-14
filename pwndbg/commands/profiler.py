4e401d196737be5a8535f765b5cabc9d54e3e05c
Richard Liu <richy.liu.2002@gmail.com>
2025-01-14 19:29:59 +0000

    add profiler usage and allow changing output file

--- pwndbg/commands/profiler.py
+++ pwndbg/commands/profiler.py
@@ -13,14 +13,19 @@ parser = argparse.ArgumentParser(description="Utilities for profiling pwndbg.")
 subparsers = parser.add_subparsers(dest="command")
 parser_start = subparsers.add_parser("start", prog="profiler start")
 parser_stop = subparsers.add_parser("stop", prog="profiler stop")
+parser_stop.add_argument(
+    "--file", type=str, default="pwndbg.pstats", help="Output file for profile data."
+)
 
 
 @pwndbg.commands.ArgparsedCommand(parser, category=CommandCategory.PWNDBG)
-def profiler(command) -> None:
+def profiler(command, file="pwndbg.pstats") -> None:
     if command == "start":
         print("Starting profiler.")
         pwndbg.profiling.profiler.start()
     elif command == "stop":
-        fname = "pwndbg.pstats"
-        print("Stopped profiler. Wrote results to", fname)
-        pwndbg.profiling.profiler.stop(fname)
+        print("Stopped profiler. Wrote results to", file)
+        print(
+            "To analyze the results, use the ./profiling/print_stats.py script in the pwndbg repo."
+        )
+        pwndbg.profiling.profiler.stop(file)
