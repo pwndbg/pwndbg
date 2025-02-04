7f75735526ac9e4917adc0708444420c99370130
Dominik 'Disconnect3d' Czarnota <dominik.b.czarnota@gmail.com>
2025-02-04 01:38:27 +0100

    tests.sh: print glibc version via ldd

--- tests.sh
+++ tests.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Print ldd and so glibc version
+echo "Running ldd to see ldd and so glibc version"
+ldd --version
+
 # Run integration tests
 (cd tests && python3 tests.py $@)
 exit_code=$?
