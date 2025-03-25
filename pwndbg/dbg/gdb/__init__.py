42310367e6dc95756f8133d1fca9b0107988a37e
OB <obarroncs@gmail.com>
2025-03-25 12:01:25 -0700

    Allow partial read of memory that wraps around address space boundary back to 0

--- pwndbg/dbg/gdb/__init__.py
+++ pwndbg/dbg/gdb/__init__.py
@@ -451,6 +451,14 @@ class GDBProcess(pwndbg.dbg_mod.Process):
             else:
                 stop_addr = int(message.split()[-1], 0)
 
+            # Handle case of memory read that wraps around the memory space back to 0, where high memory was readable but memory at 0 was not.
+            # Example: 2-byte read at 0xFFFF_FFFF in a 32-bit address space.
+            # GDB returns error: "Cannot access memory at address 0x0"
+            if stop_addr == 0 and stop_addr < addr:
+                # We could read from the top-portion of memory, but not after wrapping around
+                # Because we are doing a partial read, read until the max address
+                return self.read_memory(addr, pwndbg.aglib.arch.ptrmask - addr)
+
             if stop_addr != addr:
                 return self.read_memory(addr, stop_addr - addr)
 
