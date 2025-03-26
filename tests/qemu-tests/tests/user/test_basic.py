a994d02c538e844dad9f668f62a9e1f00647988d
OB <obarroncs@gmail.com>
2025-03-26 13:35:16 -0700

    clean up cross-arch makefile, skip the slow tests unless an environment variable is set

--- tests/qemu-tests/tests/user/test_basic.py
+++ tests/qemu-tests/tests/user/test_basic.py
@@ -1,13 +1,26 @@
 from __future__ import annotations
 
+import os
 from typing import Literal
 
 import gdb
+import pytest
 import user
 
 import pwndbg.aglib.proc
 import pwndbg.commands.context
 import pwndbg.lib.arch
+
+# The tests in this file execute for a long time - they can take 5-15 minutes to run, depending on the machine
+# They check for any crashes in the instruction enhancement code that may arise through
+# when displaying the context.
+# These are worth running after large changes in the instruction enhancement code and updates to Unicorn/Capstone.
+
+SKIP_SLOW_TESTS = os.getenv("PWNDBG_RUN_SLOW_TESTS") is not None
+
+REASON_SKIPPING = (
+    "Test skipped because it is slow - run these tests when changing instruction enhancement code"
+)
 
 
 # Step through a binary, running "ctx" each time the program stops
@@ -32,22 +45,27 @@ def helper(
         pwndbg.commands.context.context()
 
 
+@pytest.mark.skipif(SKIP_SLOW_TESTS is False, reason=REASON_SKIPPING)
 def test_basic_aarch64(qemu_start_binary):
     helper(qemu_start_binary, "basic.aarch64.out", "aarch64")
 
 
+@pytest.mark.skipif(SKIP_SLOW_TESTS is False, reason=REASON_SKIPPING)
 def test_basic_arm(qemu_start_binary):
     helper(qemu_start_binary, "basic.arm.out", "arm")
 
 
+@pytest.mark.skipif(SKIP_SLOW_TESTS is False, reason=REASON_SKIPPING)
 def test_basic_riscv64(qemu_start_binary):
     helper(qemu_start_binary, "basic.riscv64.out", "riscv64")
 
 
+@pytest.mark.skipif(SKIP_SLOW_TESTS is False, reason=REASON_SKIPPING)
 def test_basic_mips64(qemu_start_binary):
     # pwnlib.context.endian defaults to "little", but these MIPS binaries are compiled to big endian.
     helper(qemu_start_binary, "basic.mips64.out", "mips64", endian="big")
 
 
+@pytest.mark.skipif(SKIP_SLOW_TESTS is False, reason=REASON_SKIPPING)
 def test_basic_mips32(qemu_start_binary):
     helper(qemu_start_binary, "basic.mips32.out", "mips", endian="big")
