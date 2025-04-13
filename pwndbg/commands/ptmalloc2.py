72c11f66456a1a9746f7e4268987de22db0d2c5e
Lzu Tao <taolzu@gmail.com>
2025-04-14 01:34:18 +0700

    gate the hint for gdb only

--- pwndbg/commands/ptmalloc2.py
+++ pwndbg/commands/ptmalloc2.py
@@ -144,12 +144,13 @@ def print_no_arena_found_error(tid=None) -> None:
             f"No arena found for thread {message.hint(tid)} (the thread hasn't performed any allocations)."
         )
     )
-    print(
-        message.hint(
-            "Use `set libthread-db-search-path <path>` if the path contains a compatible libthread_db.\n"
-            "Then rerun the program."
+    if pwndbg.dbg.name() == pwndbg.dbg_mod.DebuggerType.GDB:
+        print(
+            message.hint(
+                "Use `set libthread-db-search-path <path>` if the path contains a compatible libthread_db.\n"
+                "Then rerun the program."
+            )
         )
-    )
 
 
 def print_no_tcache_bins_found_error(tid: int | None = None) -> None:
