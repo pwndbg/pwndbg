c0ff720e9b20a407e6ac1baa6b4ebbd79bc52a3d
CamBlackwell <cameronblackwell89@gmail.com>
2025-05-05 22:39:46 -0400

    fix: reformatted and everything is good now phew

--- tests/tests.py
+++ tests/tests.py
@@ -244,21 +244,35 @@ def run_tests_and_print_stats(
         print("******** COVERAGE REPORT ********")
         print("*********************************")
     try:
-        combine_cmd = ["python", "-m", "coverage", "combine", "--data-file", os.path.join(root_dir, ".cov/coverage")]
+        combine_cmd = [
+            "python",
+            "-m",
+            "coverage",
+            "combine",
+            "--data-file",
+            os.path.join(root_dir, ".cov/coverage"),
+        ]
         subprocess.run(combine_cmd, check=False)
 
-        coverage_report_cmd = ["python", "-m", "coverage", "report", "--data-file", os.path.join(root_dir, ".cov/coverage")]
+        coverage_report_cmd = [
+            "python",
+            "-m",
+            "coverage",
+            "report",
+            "--data-file",
+            os.path.join(root_dir, ".cov/coverage"),
+        ]
         subprocess.run(coverage_report_cmd, check=True)
     except Exception as e:
         print(f"Error generating coverage report: {e}")
     print("")
 
     if stats.fail_tests != 0:
-         print("\nFailing tests:")
-         for test_case in stats.fail_tests_names:
-             print(f"- {test_case}")
-         sys.exit(1)
- 
+        print("\nFailing tests:")
+        for test_case in stats.fail_tests_names:
+            print(f"- {test_case}")
+        sys.exit(1)
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Run tests.")
