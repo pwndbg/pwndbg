0cc163aef317628628c71f8f6e1c8ad334549bfe
jxuanli <junxua01@student.ubc.ca>
2025-05-11 12:59:25 -0700

    added new ParsedBuddyArgs class and THBs support and improved overall handling

--- pwndbg/commands/pcplist.py
+++ pwndbg/commands/pcplist.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import argparse
 import logging
+from dataclasses import dataclass
 from typing import List
 from typing import Tuple
 
@@ -20,19 +21,18 @@ log = logging.getLogger(__name__)
 MAX_PG_FREE_LIST_STR_RESULT_CNT = 0x10
 MAX_PG_FREE_LIST_CNT = 0x1000
 NONE_TUPLE = (None, None)
+# https://elixir.bootlin.com/linux/v6.13.12/source/include/linux/mmzone.h#L52
+MIGRATE_PCPTYPES = 3
 
 
-def pcp_typecheck(pcp: str):
-    str_opts = {"none", "only", "all"}
-    nr_pcp_lists = pwndbg.aglib.kernel.npcplist()
-    if pcp is None:
-        return "all"  # default
-    if pcp in str_opts:
-        return pcp
-    if pcp.isdigit() and int(pcp) < nr_pcp_lists:
-        return pcp
-    raise argparse.ArgumentTypeError(f"Only {str_opts} and numbers < {nr_pcp_lists} are allowed.")
-
+@dataclass
+class ParsedBuddyArgs:
+    zone: pwndbg.dbg_mod.Value | None
+    order: int | None
+    mtype: str | None
+    cpu: int | None
+    sections: List[Tuple[str, str]]
+    indent: IndentContextManager
 
 def cpu_limitcheck(cpu: str):
     if cpu is None:
@@ -65,7 +65,7 @@ parser.add_argument(
     default=None,
     help="",
 )
-parser.add_argument("-p", "--pcp", type=pcp_typecheck, dest="pcp", default="all", help="")
+parser.add_argument("-p", "--pcp-only", action="store_true", dest="pcp_only", default=False, help="")
 parser.add_argument("-c", "--cpu", type=cpu_limitcheck, dest="cpu", default=None)
 
 
@@ -98,9 +98,7 @@ def traverse_pglist(
             )
         counter += 1
         if page in seen_pages:
-            msgs.append(
-                f"Cyclic doubly linked list detected: {indent.addr_hex(physmap_addr)} [page: {indent.aux_hex(page)}, phys: {indent.aux_hex(phys_addr)}]"
-            )
+            msgs.append(f"Cyclic doubly linked list detected: {results[-1]}")
             break
         seen_pages.add(page)
         if counter == MAX_PG_FREE_LIST_CNT:
@@ -125,18 +123,22 @@ def print_pglist(
     name: str | None,
     sections: List[Tuple[str, str]],
     indent: IndentContextManager,
-) -> bool:
-    if len(sections) != 2:
-        log.warning("The number of sections is not 2!")
-        return False
+):
+    if len(sections) != 3:
+        log.warning(f"The number ({len(sections)}) of sections is not 2!")
+        return
     results, counter, msgs = traverse_pglist(free_list, indent)
     if not results or len(results) == 0 or counter == 0:
-        return False
+        return
     # this needs to be done after passing the previous if-statement buf before the first print within `with indent`
+    print_section(sections[0], indent)
+    sections[0] = NONE_TUPLE
     with indent:
-        print_section(sections[0], indent)
+        print_section(sections[1], indent)
+        sections[1] = NONE_TUPLE
         with indent:
-            print_section(sections[1], indent)
+            print_section(sections[2], indent)
+            sections[2] = NONE_TUPLE
             with indent:
                 indent.print(
                     f"- {indent.prefix(name)} (contains {indent.aux_hex(counter)} elements)"
@@ -148,80 +150,77 @@ def print_pglist(
                         for msg in msgs:
                             indent.print(msg)
                         print()
-    return True
 
 
 def print_mtypes(
     free_list: pwndbg.dbg_mod.Value,
-    sections: List[Tuple[str, str]],
-    indent: IndentContextManager,
-    mtype: str | None,
-) -> bool:
-    for i, name in enumerate(static_str_arr("migratetype_names")):
-        if mtype is not None and name != mtype:
-            continue
-        _free_list = free_list[i]
-        if print_pglist(_free_list, name, sections, indent):
-            sections = [NONE_TUPLE, NONE_TUPLE]
-    # returns if it succeeded or not
-    return sections[0] == NONE_TUPLE and sections[1] == NONE_TUPLE
-
-
-def print_pcp_lists(
-    zone: pwndbg.dbg_mod.Value | None, indent: IndentContextManager, choice: str, cpu: int | None
+    pba: ParsedBuddyArgs,
+    nr_types: int | None = None,
 ):
-    if choice == "none":
-        return
-    pcp = per_cpu(zone["per_cpu_pageset"], cpu)
-    sections = [
-        ("per_cpu_pageset", f"number of pages {indent.aux_hex(int(pcp["count"]))}"),
-        NONE_TUPLE,
-    ]
-    for i in range(pwndbg.aglib.kernel.npcplist()):
-        if choice.isdigit() and int(choice) != i:
+    names = static_str_arr("migratetype_names")
+    if nr_types is None:
+        nr_types = len(names)
+    for i in range(nr_types):
+        name = names[i]
+        if pba.mtype is not None and name != pba.mtype:
             continue
-        if print_pglist(pcp["lists"][i], f"PCP List {i}", sections, indent):
-            sections[0] = NONE_TUPLE  # do not reprint section info multiple times
+        print_pglist(free_list[i], name, pba.sections, pba.indent)
 
 
-def print_free_area(
-    zone: pwndbg.dbg_mod.Value | None,
-    indent: IndentContextManager,
-    order: int | None,
-    mtype: str | None,
-):
-    free_area = zone["free_area"]
-    sections: List[Tuple[str, str]] = [("free_area", None), NONE_TUPLE]
-    for i in range(len(free_area)):
-        if order is not None and i != order:
+def print_pcp_set(pba: ParsedBuddyArgs):
+    pcp = per_cpu(pba.zone["per_cpu_pageset"], pba.cpu)
+    pba.sections[1] = ("per_cpu_pageset", f"number of pages {pba.indent.aux_hex(int(pcp["count"]))}")
+    nr_pcp_lists = pwndbg.aglib.kernel.npcplist()
+    pcp_lists = pcp["lists"]
+    for i in range(0, nr_pcp_lists, MIGRATE_PCPTYPES):
+        # https://elixir.bootlin.com/linux/v6.13.12/source/include/linux/mmzone.h#L660
+        order = i // MIGRATE_PCPTYPES
+        if pba.order is not None and pba.order != order:
             continue
-        free_list = free_area[i]["free_list"]
-        nr_free = int(free_area[i]["nr_free"])
-        sections[1] = (
-            f"Order {i}",
-            f"nr_free: {indent.aux_hex(nr_free)}, size: {indent.aux_hex(0x1000 * (2 ** i))}",
+        free_list = pcp_lists[order * MIGRATE_PCPTYPES].address
+        nr_types = MIGRATE_PCPTYPES
+        if order == MIGRATE_PCPTYPES + 1:
+            order = 11 # THPs are 2MB
+            nr_types = nr_pcp_lists % MIGRATE_PCPTYPES
+        pba.sections[2] = (
+            f"Order {order}",
+            f"size: {pba.indent.aux_hex(0x1000 * (1 << order))}",
         )
-        if print_mtypes(free_list, sections, indent, mtype):
-            sections[0] = NONE_TUPLE  # do not reprint section info multiple times
+        print_mtypes(free_list, pba, nr_types)
+
+
+def print_free_area(pba: ParsedBuddyArgs):
+    free_area = pba.zone["free_area"]
+    pba.sections[1] = ("free_area", None)
+    for order in range(len(free_area)):
+        if pba.order is not None and pba.order != order:
+            continue
+        free_list = free_area[order]["free_list"]
+        nr_free = int(free_area[order]["nr_free"])
+        pba.sections[2] = (
+            f"Order {order}",
+            f"nr_free: {pba.indent.aux_hex(nr_free)}, size: {pba.indent.aux_hex(0x1000 * (1 << order))}",
+        )
+        print_mtypes(free_list, pba)
 
 
 @pwndbg.commands.Command(parser, category=CommandCategory.KERNEL)
 @pwndbg.commands.OnlyWhenQemuKernel
 @pwndbg.commands.OnlyWithKernelDebugSyms
 @pwndbg.commands.OnlyWhenPagingEnabled
-def pcplist(zone: str, pcp: str, order: int, mtype: str, cpu: int) -> None:
-    indent = IndentContextManager()
+def pcplist(zone: str, pcp_only: bool, order: int, mtype: str, cpu: int) -> None:
     node_data = pwndbg.aglib.symbol.lookup_symbol("node_data")
     if not node_data:
         log.warning("WARNING: Symbol 'node_data' not found")
         return
+    pba = ParsedBuddyArgs(None, order, mtype, cpu, [NONE_TUPLE] * 3, IndentContextManager())
     # TODO: this command currently only supports one node which should be the common case
     zones = node_data.dereference()[0]["node_zones"]
     for i, name in enumerate(static_str_arr("zone_names")):
         if zone is not None and zone != name:
             continue
-        indent.print(indent.prefix(f"Zone {name}"))
-        if mtype is None and order is None:
-            print_pcp_lists(zones[i], indent, pcp, cpu)
-        if pcp != "only" and not pcp.isdigit():
-            print_free_area(zones[i], indent, order, mtype)
+        pba.zone = zones[i]
+        pba.sections[0] = (f"Zone {name}", None)
+        print_pcp_set(pba)
+        if not pcp_only:
+            print_free_area(pba)
