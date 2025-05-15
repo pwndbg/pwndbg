e44aeb3bd4287146429a03ccac495ac998f19a28
k4lizen <124312252+k4lizen@users.noreply.github.com>
2025-05-16 01:47:58 +0200

    reorg .sh files

---
+++ scripts/_docs/build_all_docs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This may perform verification instead of building
+# depending on PWNDBG_GEN_DOC_JUST_VERIFY.
+uv run --group docs python ./scripts/_docs/build_command_docs.py
+# uv run --group docs python ./scripts/_docs/build_configuration_docs.py
+# uv run --group docs python ./scripts/_docs/build_function_docs.py
--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-export PWNDBG_DOCGEN_DBGNAME="gdb"
+# Extract the documentation.
+echo "Extracting docs.."
+./scripts/_docs/extract_all_docs.sh || exit 1
 
-uv run --group docs gdb --batch -nx -ix ./gdbinit.py \
-    -iex "set exception-verbose on" \
-    -ix ./scripts/_docs/extract_command_docs.py \
-    -nx
-
-export PWNDBG_DOCGEN_DBGNAME="lldb"
-
-uv run --group docs --extra lldb python pwndbg-lldb.py <<EOF
-set show-tips off
-command script import ./scripts/_docs/extract_command_docs.py
-EOF
+# Build the documentation.
+echo "Building docs.."
+./scripts/_docs/build_all_docs.sh || exit 1
