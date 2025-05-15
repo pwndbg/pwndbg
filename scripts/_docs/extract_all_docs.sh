0de4be386892bcdec81371eee686945f57ad69b8
k4lizen <124312252+k4lizen@users.noreply.github.com>
2025-05-16 01:47:58 +0200

    reorg .sh files

---
+++ scripts/_docs/extract_all_docs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Extract from sources all the information necessary to build
+# the documentation. Do this from each debugger.
+
+export PWNDBG_DOCGEN_DBGNAME="gdb"
+uv run --group docs gdb --batch -nx -ix ./gdbinit.py \
+    -iex "set exception-verbose on" \
+    -ix ./scripts/_docs/extract_command_docs.py \
+    -nx
+
+export PWNDBG_DOCGEN_DBGNAME="lldb"
+uv run --group docs --extra lldb python pwndbg-lldb.py <<EOF
+set show-tips off
+command script import ./scripts/_docs/extract_command_docs.py
+EOF
+
+exit 1
