1fc02d2cc32143a778813b4be4d4c239942a5236
k4lizen <124312252+k4lizen@users.noreply.github.com>
2025-05-16 02:04:34 +0200

    rename bash scripts so they use - instead of _

--- scripts/verify-docs.sh
+++ scripts/verify-docs.sh
@@ -2,9 +2,9 @@
 
 # Extract the documentation.
 echo "Extracting docs.."
-./scripts/_docs/extract_all_docs.sh || exit 1
+./scripts/_docs/extract-all-docs.sh || exit 1
 
 # Verify the documentation.
 echo "Verifying docs.."
 export PWNDBG_DOCGEN_VERIFY=1
-./scripts/_docs/build_all_docs.sh || exit 1
+./scripts/_docs/build-all-docs.sh || exit 1
