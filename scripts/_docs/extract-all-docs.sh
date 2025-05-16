48b4ff9baf54d87a8565899c7894ac93787d9860
k4lizen <124312252+k4lizen@users.noreply.github.com>
2025-05-16 19:13:43 +0200

    indent extract-all-docs

--- scripts/_docs/extract-all-docs.sh
+++ scripts/_docs/extract-all-docs.sh
@@ -13,7 +13,7 @@ uv run --group docs gdb --batch -nx -ix ./gdbinit.py \
 
 export PWNDBG_DOCGEN_DBGNAME="lldb"
 {
-uv run --group docs --extra lldb python pwndbg-lldb.py <<EOF
+    uv run --group docs --extra lldb python pwndbg-lldb.py << EOF
 set show-tips off
 command script import ./scripts/_docs/extract_command_docs.py
 command script import ./scripts/_docs/extract_configuration_docs.py
