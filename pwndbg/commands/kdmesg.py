20c46916a879fdcb32c44a388f2fc1032f056a81
AC01010 <achang01010@gmail.com>
2025-06-09 10:48:30 -0400

    Fix lint errors

--- pwndbg/commands/kdmesg.py
+++ pwndbg/commands/kdmesg.py
@@ -1,9 +1,12 @@
-import pwndbg.commands
+from __future__ import annotations
+
 import pwndbg.color.message as message
+import pwndbg.commands
+
 
 @pwndbg.commands.Command(
-    "Displays the kernel ring buffer (dmesg) contents.", 
-    category=pwndbg.commands.CommandCategory.KERNEL
+    "Displays the kernel ring buffer (dmesg) contents.",
+    category=pwndbg.commands.CommandCategory.KERNEL,
 )
 @pwndbg.commands.OnlyWhenQemuKernel
 @pwndbg.commands.OnlyWhenPagingEnabled
@@ -12,22 +15,30 @@ def kdmesg() -> None:
     prb_addr = pwndbg.aglib.symbol.lookup_symbol_addr("printk_rb_static")
 
     if prb_addr is None:
-        print("The printk_rb_static symbol was not found. This may indicate that the kernel is an older version or that the symbol is not available in the current build.")
+        print(
+            "The printk_rb_static symbol was not found. This may indicate that the kernel is an older version or that the symbol is not available in the current build."
+        )
         return
 
     try:
-        printk_ringbuffer_type = pwndbg.aglib.memory.get_typed_pointer_value("struct printk_ringbuffer", prb_addr)
+        printk_ringbuffer_type = pwndbg.aglib.memory.get_typed_pointer_value(
+            "struct printk_ringbuffer", prb_addr
+        )
         desc_ring_addr = printk_ringbuffer_type["desc_ring"].address
         text_data_ring_addr = printk_ringbuffer_type["text_data_ring"].address
 
-        desc_ring_type = pwndbg.aglib.memory.get_typed_pointer_value("struct prb_desc_ring", desc_ring_addr)
+        desc_ring_type = pwndbg.aglib.memory.get_typed_pointer_value(
+            "struct prb_desc_ring", desc_ring_addr
+        )
         desc_ring_count = 1 << int(desc_ring_type["count_bits"])
         descs = int(desc_ring_type["descs"])
         infos = int(desc_ring_type["infos"])
 
-        text_data_ring_type = pwndbg.aglib.memory.get_typed_pointer_value("struct prb_data_ring", text_data_ring_addr)
+        text_data_ring_type = pwndbg.aglib.memory.get_typed_pointer_value(
+            "struct prb_data_ring", text_data_ring_addr
+        )
         text_data_sz = 1 << int(text_data_ring_type["size_bits"])
-        text_data_addr = text_data_ring_type["data"]
+        text_data_addr = int(text_data_ring_type["data"])
 
         size_long = pwndbg.aglib.typeinfo.lookup_types("long").sizeof
         desc_flags_shift = size_long * 8 - 2
@@ -52,9 +63,9 @@ def kdmesg() -> None:
             info_addr = infos + info_off
 
             desc = pwndbg.aglib.memory.get_typed_pointer_value("struct prb_desc", desc_addr)
-            
+
             state = 3 & (int(desc["state_var"]["counter"]) >> desc_flags_shift)
-            if state != 1 and state != 2: # desc_committed or desc_finalized
+            if state != 1 and state != 2:  # desc_committed or desc_finalized
                 if did == head_id:
                     break
                 did = (did + 1) & desc_id_mask
@@ -78,7 +89,7 @@ def kdmesg() -> None:
                     text_len = end - text_start
 
                 text_data = pwndbg.aglib.memory.read(text_data_addr + text_start, text_len)
-                text = text_data.decode(encoding='utf8', errors='replace')
+                text = text_data.decode(encoding="utf8", errors="replace")
 
             time_stamp = int(info["ts_nsec"])
 
