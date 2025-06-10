ba48cf4f8c9235d2f5934657c70ee74bcac0354d
AC01010 <achang01010@gmail.com>
2025-06-10 17:33:08 -0400

    Fix lint errors

--- pwndbg/commands/kdmesg.py
+++ pwndbg/commands/kdmesg.py
@@ -131,7 +131,11 @@ def kdmesg(ctime: bool = False) -> None:
                     return
 
                 if pwndbg.aglib.typeinfo.load("struct tk_data") is None:
-                    print(message.error("`struct tk_data` is not defined in the current debug symbols."))
+                    print(
+                        message.error(
+                            "`struct tk_data` is not defined in the current debug symbols."
+                        )
+                    )
                     return
 
                 tk_core = pwndbg.aglib.memory.get_typed_pointer_value(
