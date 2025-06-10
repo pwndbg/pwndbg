bd3bfa5f87a18693b532571f743c8d78f552198c
AC01010 <achang01010@gmail.com>
2025-06-10 16:53:18 -0400

    Fix -T arg to reflect current time, not offset

--- pwndbg/commands/kdmesg.py
+++ pwndbg/commands/kdmesg.py
@@ -120,8 +120,12 @@ def kdmesg(ctime: bool = False) -> None:
 
             # Format and print the message.
             if ctime:
+                tk_core_addr = pwndbg.aglib.symbol.lookup_symbol_addr("tk_core")
+                tk_core = pwndbg.aglib.memory.get_typed_pointer_value("struct tk_data", tk_core_addr)
+                epoch_time = int(tk_core["timekeeper"]["xtime_sec"])
+
                 for line in text.splitlines():
-                    print(f"[{time.ctime(int(info['ts_nsec']) / 1e9)}] {line}")
+                    print(f"[{time.ctime(int(info['ts_nsec'] ) / 1e9 + epoch_time)}] {line}")
             else:
                 for line in text.splitlines():
                     print(f"[{int(info['ts_nsec']) / 1e9:12.6f}] {line}")
