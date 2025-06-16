7f3e7eb2794b2f17aeebf7c5c2affb3edb5ddc33
AC01010 <achang01010@gmail.com>
2025-06-16 14:25:46 -0400

    Fix lint errors

--- tests/library/qemu-system/tests/test_commands_kernel.py
+++ tests/library/qemu-system/tests/test_commands_kernel.py
@@ -47,6 +47,7 @@ def test_command_kdmesg():
         or "`struct tk_data` is not defined in the current debug symbols." in res
     )
 
+
 def test_command_ktask():
     if not pwndbg.aglib.kernel.has_debug_syms():
         res = gdb.execute("ktask", to_string=True)
