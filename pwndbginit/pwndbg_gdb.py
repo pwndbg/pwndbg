239de41daecec261881a2cc9a23c5b957aba62fd
Patryk Sondej <patryk.sondej@gmail.com>
2025-06-19 23:25:59 +0200

    debug

--- pwndbginit/pwndbg_gdb.py
+++ pwndbginit/pwndbg_gdb.py
@@ -16,15 +16,20 @@ def get_gdb_version(path: str) -> Tuple[str, str]:
         capture_output=True,
         text=True
     )
-    print(result.stdout)
     return tuple(result.stdout.strip().split(' ', 2))
 
 def main():
-    try:
-        from gdb_for_pwndbg.gdb import gdb_path
-    except ImportError:
-        gdb_path = shutil.which("gdb")
+    gdb_argv = [sys.argv[0], "-q", "-nx", "-iex", "py import pwndbginit.gdbinit", *sys.argv[1:]]
+    sys.argv = gdb_argv
 
+    try:
+        from gdb_for_pwndbg.gdb import main
+        main()
+        return
+    except ImportError:
+        pass
+
+    gdb_path = shutil.which("gdb")
     if not gdb_path:
         print(f"ERROR: Could not find gdb for pwndbg in {gdb_path}")
         sys.exit(1)
@@ -40,7 +45,7 @@ def main():
         print(f"ERROR: GDB is compiled for Python {have}, but your Python interpreter is {expected}")
         sys.exit(1)
 
-    os.execve(gdb_path, [sys.argv[0]] + ["-q", "-nx", "-iex", "py import pwndbginit.gdbinit"] + sys.argv[1:], env=envs)
+    os.execve(gdb_path, sys.argv, env=envs)
 
 if __name__ == "__main__":
     main()
