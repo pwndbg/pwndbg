fde9178b1f83c32a6a46e11a51d1bc347e7f16da
Matheus Branco Borella <4922458+mbrla0@users.noreply.github.com>
2025-06-19 18:08:43 +0000

    Add base LLDB tests

--- tests/library/dbg/tests/__init__.py
+++ tests/library/dbg/tests/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import functools
+from inspect import signature
 from typing import Callable
 from typing import Coroutine
 
@@ -11,12 +12,18 @@ from host import Controller
 def pwndbg_test(
     test: Callable[..., Coroutine[Any, Any, None]],
 ) -> Callable[..., None]:
+    @functools.wraps(test)
     def inner_test(*args, **kwargs):
         async def _test(controller: Controller) -> None:
-            test(controller, *args, **kwargs)
+            await test(controller, *args, **kwargs)
 
         print(f"[+] Launching test {test.__name__} asynchronously")
         host.start(_test)
         pass
+
+    # Remove the controller from the signature, as seen by Pytest.
+    sig = signature(inner_test)
+    sig = sig.replace(parameters=tuple(sig.parameters.values())[1:])
+    inner_test.__signature__ = sig
 
     return inner_test
--- tests/library/dbg/tests/test_test.py
+++ tests/library/dbg/tests/test_test.py
@@ -4,10 +4,30 @@ import host
 import pytest
 from host import Controller
 
+from . import pwndbg_test
+
 
 @pytest.mark.xfail
-def test_starts_no_decorator() -> None:
+def test_starts_no_decorator_xfail() -> None:
     async def run(ctrl: Controller):
         raise RuntimeError("should fail!")
 
     host.start(run)
+
+
+def test_starts_no_decorator() -> None:
+    async def run(ctrl: Controller):
+        pass
+
+    host.start(run)
+
+
+@pytest.mark.xfail
+@pwndbg_test
+async def test_starts_xfail(ctrl: Controller) -> None:
+    raise RuntimeError("should fail")
+
+
+@pwndbg_test
+async def test_starts(ctrl: Controller) -> None:
+    pass
