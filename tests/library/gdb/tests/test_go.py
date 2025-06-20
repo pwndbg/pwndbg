80d92329d7303110443a4730b905f984c8db7060
Disconnect3d <dominik.b.czarnota@gmail.com>
2025-06-20 17:09:23 +0200

    Skip tests instead of xfail

--- tests/library/gdb/tests/test_go.py
+++ tests/library/gdb/tests/test_go.py
@@ -49,11 +49,11 @@ def helper_test_dump(start_binary, filename):
     assert third.strip() == """([3]complex64) [(1.1 + 2.2i), (-2.5 - 5.0i), (4.2 - 2.1i)]"""
 
 
-@pytest.mark.xfail(reason="TODO/FIXME: Needs fix for Go 1.24")
+@pytest.mark.skip(reason="TODO/FIXME: Needs fix for Go 1.24")
 def test_go_dumping_x64(start_binary):
     helper_test_dump(start_binary, GOSAMPLE_X64)
 
 
-@pytest.mark.xfail(reason="TODO/FIXME: Needs fix for Go 1.24")
+@pytest.mark.skip(reason="TODO/FIXME: Needs fix for Go 1.24")
 def test_go_dumping_x86(start_binary):
     helper_test_dump(start_binary, GOSAMPLE_X86)
