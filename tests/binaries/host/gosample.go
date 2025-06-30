86081901aeb9d0ec091312ba1a38605ff072bebf
Abraxas <user1092@protonmail.com>
2025-06-30 21:28:17 +0200

    fix: Add missing blank line for linting + restore gosample.go for tests

---
+++ tests/binaries/host/gosample.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func testFunc(x interface{}) *interface{} {
+	fmt.Println(x)
+	return &x // leak x to force it to be allocated somewhere
+}
+
+func main() {
+	testFunc(map[uint8]uint64{1: 2, 3: 4, 5: 6})
+	testFunc(map[string]int{"a": 1, "b": 2, "c": 3})
+	testFunc([]struct {
+		a int
+		b string
+	}{{a: 1, b: "first"}, {a: 2, b: "second"}})
+	testFunc([3]complex64{1.1 + 2.2i, -2.5 - 5i, 4.2 - 2.1i})
+}
