da31e9184efbe773c9b88e0bd52eded6be5747d0
Matt <4922458+mbrla0@users.noreply.github.com>
2025-07-03 19:36:30 -0300

    Add even even more tests

---
+++ tests/library/dbg/tests/test_go.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+from host import Controller
+
+import tests
+
+GOSAMPLE_X64 = tests.get_binary("gosample.x64")
+GOSAMPLE_X86 = tests.get_binary("gosample.x86")
+
+
+async def helper_test_dump(ctrl: Controller, target: str) -> None:
+    gdb.execute("set environment GOMAXPROCS=1")
+
+    await ctrl.launch(filename, env={"GOMAXPROCS": "1"})
+
+    await ctrl.execute("b gosample.go:6")
+    await ctrl.cont()
+
+    dump = await ctrl.execute_and_capture("go-dump any &x")
+    assert dump.strip() == """(map[uint8]uint64) &{1: 2, 3: 4, 5: 6}"""
+    await ctrl.cont()
+
+    dump = await ctrl.execute_and_capture("go-dump any &x")
+    assert dump.strip() == """(map[string]int) &{"a": 1, "b": 2, "c": 3}"""
+    await ctrl.cont()
+
+    dump = await ctrl.execute_and_capture("go-dump any &x")
+    assert (
+        dump.strip()
+        == """([]struct { a int; b string }) [struct {a: 1, b: "first"}, struct {a: 2, b: "second"}]"""
+    )
+    await ctrl.cont()
+
+    dump = await ctrl.execute_and_capture("go-dump -f 1 any &x")
+    assert dump.strip() == """([3]complex64) [(1.1 + 2.2i), (-2.5 - 5.0i), (4.2 - 2.1i)]"""
+
+
+@tests.pwndbg_test
+async def test_go_dumping_x64(ctrl: Controller) -> None:
+    await helper_test_dump(start_binary, GOSAMPLE_X64)
+
+
+@tests.pwndbg_test
+async def test_go_dumping_x86(ctrl: Controller) -> None:
+    await helper_test_dump(start_binary, GOSAMPLE_X86)
---
+++ tests/library/dbg/tests/test_misc.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+STACK_COMMANDS = [
+    ("canary", [], "Stack", "Print out the current stack canary."),
+    # The aliases 'do' and 'dow' were added to support the help consistency test.
+    ("down", ["do", "dow"], "Misc", "Select and print stack frame called by this one."),
+    ("retaddr", [], "Stack", "Print out the stack addresses that contain return addresses."),
+    ("stack", [], "Stack", "Dereferences on stack data with specified count and offset."),
+    ("up", [], "Misc", "Select and print stack frame that called this one."),
+]
+
+
+@tests.pwndbg_test
+async def test_list_and_filter_commands_filter(_ctrl: Controller):
+    from pwndbg.commands.misc import list_and_filter_commands
+
+    for cmd in STACK_COMMANDS:
+        assert cmd in list_and_filter_commands("stack")
+
+
+@tests.pwndbg_test
+async def test_list_and_filter_commands_full_list(_ctrl: Controller):
+    import pwndbg.commands
+    from pwndbg.commands.misc import list_and_filter_commands
+
+    all_commands = list_and_filter_commands("")
+
+    def get_doc(c):
+        return c.description.splitlines()[0]
+
+    cmd_name_docs = [
+        (c.command_name, c.aliases, c.category, get_doc(c)) for c in pwndbg.commands.commands
+    ]
+    cmd_name_docs.sort()
+
+    assert all_commands == cmd_name_docs
