a0bb2dca28db4a0a62060700ab8beef06cb803d2
Patryk Sondej <patryk.sondej@gmail.com>
2025-08-19 02:32:36 +0200

    lldb: fix lookup symbol for specific objfile

--- pwndbg/dbg/lldb/__init__.py
+++ pwndbg/dbg/lldb/__init__.py
@@ -1405,7 +1405,10 @@ class LLDBProcess(pwndbg.dbg_mod.Process):
         variables_types: Dict[Tuple[int, str], LLDBType] = {}
 
         if type in (pwndbg.dbg_mod.SymbolLookupType.VARIABLE, pwndbg.dbg_mod.SymbolLookupType.ANY):
-            variables: lldb.SBValueList = (objfile or self.target).FindGlobalVariables(name, 0)
+            if objfile:
+                variables: lldb.SBValueList = objfile.FindGlobalVariables(self.target, name, 0)
+            else:
+                variables: lldb.SBValueList = self.target.FindGlobalVariables(name, 0)
             var: lldb.SBValue
             for var in variables:
                 # LLDB[1] is attempting to resolve a TLS variable, but it fails with the following error:
