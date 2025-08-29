4d15b4e3fd84ecb9f96fc693a76fffe1d489bf74
jxuanli <junxua01@student.ubc.ca>
2025-08-29 10:50:45 -0700

    klookup

--- pwndbg/commands/klookup.py
+++ pwndbg/commands/klookup.py
@@ -9,28 +9,28 @@ from pwndbg.commands import CommandCategory
 
 parser = argparse.ArgumentParser(description="Lookup kernel symbols")
 
-parser.add_argument("symbol", type=str, help="Address or symbol name to lookup")
+parser.add_argument("symbol", type=str, nargs="?", help="Address or symbol name to lookup")
 
 
-@pwndbg.commands.Command(parser, category=CommandCategory.KERNEL)
+@pwndbg.commands.Command(parser, aliases=["kallsyms", "ks"], category=CommandCategory.KERNEL)
 @pwndbg.commands.OnlyWhenQemuKernel
 @pwndbg.commands.OnlyWhenPagingEnabled
 def klookup(symbol: str) -> None:
     ksyms = pwndbg.aglib.kernel.kallsyms.get()
+    syms = []
     try:
         symbol_addr = int(symbol, 0)
         for ksym, v in ksyms.items():
             if v[0] == symbol_addr:
-                print(message.success(f"{symbol_addr:#x} = {ksym}"))
-                return
-        print(message.error(f"No symbol found at {symbol_addr:#x}"))
-    except ValueError:
-        found = False
+                syms.append((ksym, v[0], v[1]))
+        if len(syms) == 0:
+            print(message.error(f"No symbol found at {symbol_addr:#x}"))
+    except (ValueError, TypeError):
         for ksym, v in ksyms.items():
-            if symbol not in ksym:
+            if symbol is not None and symbol not in ksym:
                 continue
-            found = True
-            addr = v[0]
-            print(message.success(f"{addr:#x} = {ksym}"))
-        if not found:
+            syms.append((ksym, v[0], v[1]))
+        if len(syms) == 0:
             print(message.error(f"No symbol found for {symbol}"))
+    for sym_name, sym_addr, sym_type in syms:
+        print(message.success(f"{sym_addr:#x} {sym_type} {sym_name}"))
