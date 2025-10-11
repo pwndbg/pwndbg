74e0f3872270deba75680411af2072592fd7fde5
OB <obarroncs@gmail.com>
2025-10-11 13:31:18 -0400

    Simplify old aarch64 test

--- tests/library/qemu_user/tests/test_aarch64.py
+++ tests/library/qemu_user/tests/test_aarch64.py
@@ -779,28 +779,30 @@ REFERENCE_BINARY = get_binary("reference-binary.aarch64.out")
 
 def test_aarch64_reference(qemu_start_binary):
     qemu_start_binary(REFERENCE_BINARY, "aarch64")
-    gdb.execute("break break_here")
+    gdb.execute("break *main")
     assert pwndbg.aglib.symbol.lookup_symbol("main") is not None
     gdb.execute("continue")
 
+    # verify call argument are enriched
+    gdb.execute("stepuntilasm bl")
+    assembly = gdb.execute("nearpc", to_string=True)
+    assert "'Not enough args'" in assembly
+    
     gdb.execute("argv", to_string=True)
     assert gdb.execute("argc", to_string=True).strip() == "1"
     gdb.execute("auxv", to_string=True)
     assert (
         gdb.execute("cpsr", to_string=True, from_tty=False).strip()
-        == "cpsr 0x0 [ n z c v q pan il d a i f el sp ]"
+        == "cpsr 0x60000000 [ n Z C v q pan il d a i f el sp ]"
     )
     gdb.execute("context", to_string=True)
     gdb.execute("hexdump", to_string=True)
     gdb.execute("telescope", to_string=True)
 
-    # TODO: Broken
     gdb.execute("retaddr", to_string=True)
 
-    # Broken
     gdb.execute("procinfo", to_string=True)
 
-    # Broken
     gdb.execute("vmmap", to_string=True)
 
     gdb.execute("piebase", to_string=True)
