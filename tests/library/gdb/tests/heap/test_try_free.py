5e0ba60117e755ad467e32a3c73aaafc2864e5c4
Patryk Sondej <patryk.sondej@gmail.com>
2025-12-02 04:07:12 +0100

    rename heap binaries

--- tests/library/gdb/tests/heap/test_try_free.py
+++ tests/library/gdb/tests/heap/test_try_free.py
@@ -12,7 +12,7 @@ import pwndbg.aglib.heap
 from .. import get_binary
 
 HEAP_BINARY = get_binary("heap_bugs.x86-64.out")
-HEAP_CODE = get_binary("heap_bugs.c")
+HEAP_CODE = get_binary("heap_bugs.x86-64.c")
 _, OUTPUT_FILE = tempfile.mkstemp()
 
 
