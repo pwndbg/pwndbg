2543cb8e7ce2a940ad55eb79d9e4ef113f7a1c6c
Patryk Sondej <patryk.sondej@gmail.com>
2025-12-06 23:18:45 +0100

    fixy

--- tests/library/dbg/tests/test_command_flags.py
+++ tests/library/dbg/tests/test_command_flags.py
@@ -9,7 +9,6 @@ from . import pwndbg_test
 REFERENCE_BINARY = get_binary("reference-binary.native.out")
 
 
-# todo: x86-64 only
 @pwndbg_test
 async def test_flags_command(ctrl: Controller) -> None:
     import pwndbg.aglib.regs
