fd0f68519caef66b266882fd3baf428dc22d3509
Patryk Sondej <patryk.sondej@gmail.com>
2025-12-07 02:17:49 +0100

    fixy

--- tests/library/gdb/tests/test_context_commands.py
+++ tests/library/gdb/tests/test_context_commands.py
@@ -71,7 +71,7 @@ def test_context_disasm_show_fd_filepath(start_binary):
     line_call_read, line_fd, line_buf, line_nbytes, *_rest = lines_after_call_read
 
     line_fd = line_fd.strip()
-    assert re.match(r"fd:\s+3 \([a-z/]*/tests/binaries/host/use-fds.native.out\)", line_fd)
+    assert re.match(r"fd:\s+3 \(.*?/tests/binaries/host/use-fds.native.out\)", line_fd)
 
     line_buf = line_buf.strip()
     assert re.match(r"buf:\s+0x[0-9a-f]+ ◂— 0", line_buf)
