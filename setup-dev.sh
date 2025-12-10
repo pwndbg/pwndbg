c674281703970b3153c821a470aafad617b9f230
Patryk Sondej <patryk.sondej@gmail.com>
2025-12-10 21:28:08 +0100

    add fedora41

--- setup-dev.sh
+++ setup-dev.sh
@@ -158,12 +158,15 @@ EOF
 install_dnf() {
     sudo dnf upgrade || true
     sudo dnf install -y \
+        make \
         nasm \
         gcc \
         curl \
         wget \
-        gdb \
+        musl-gcc \
+        g++ \
         parallel \
+        qemu-system-x86 \
         qemu-system-arm \
         qemu-user
 
