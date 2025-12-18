b5618c74f13ab680d28fa2c7368cef97add99776
k4lizen <124312252+k4lizen@users.noreply.github.com>
2025-12-18 16:47:15 +0100

    upd comment

--- pwndbg/lib/cache.py
+++ pwndbg/lib/cache.py
@@ -177,7 +177,7 @@ IS_CACHING_DISABLED_FOR: EventSet = 0
 def cache_until(*event_names: str) -> Callable[[Callable[P, T]], CachedFunction[T]]:
     """
     All possible values of the function arguments must be hashable
-    (so e.g. `frame: Frame | None = None` is not allowed).
+    (so e.g. `var: MyUnhashableType | None = None` is not allowed, but may fail rarely).
     The return value of the function should not be mutable, must not be mutated.
     """
     if any(event_name not in _ALL_CACHE_EVENT_NAMES for event_name in event_names):
