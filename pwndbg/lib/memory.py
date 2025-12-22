1aecfb7df2d819eb11158a708896f513c03bf8e2
Ac3 <balikwojciech1@gmail.com>
2025-12-22 18:48:38 +0100

    Reading vmmap data from /proc/$TID/smaps(fall back to /proc/$TID/smaps if not available)

--- pwndbg/lib/memory.py
+++ pwndbg/lib/memory.py
@@ -4,6 +4,7 @@ Reading, writing, and describing memory.
 
 from __future__ import annotations
 
+from ast import List
 import os
 from os.path import relpath
 
@@ -103,6 +104,8 @@ class Page:
         arch_ptrsize: int,
         objfile: str = "",
         in_darwin_shared_cache: bool = False,
+        protection_key: int | None = None,
+        vm_flags: List[str] | None = None,
     ) -> None:
         self.vaddr = start
         self.memsz = size
@@ -111,6 +114,8 @@ class Page:
         self.objfile = objfile
         self.in_darwin_shared_cache = in_darwin_shared_cache
         self.arch_ptrsize = arch_ptrsize
+        self.protection_key = protection_key
+        self.vm_flags = vm_flags
 
         # if self.rwx:
         # self.flags = self.flags ^ 1
