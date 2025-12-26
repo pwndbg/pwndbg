0a820d53b78c8746f36857841fca7874eda9a5e6
Patryk Sondej <patryk.sondej@gmail.com>
2025-12-26 05:42:17 +0100

    Fix `gdb.execute(.., to_string=True)` in `ipi` context

--- pwndbg/commands/ipython_interactive.py
+++ pwndbg/commands/ipython_interactive.py
@@ -17,15 +17,19 @@ from pwndbg.commands import CommandCategory
 
 @contextmanager
 def switch_to_ipython_env():
-    """We need to change stdout/stderr to the default ones, otherwise we can't use tab or autocomplete"""
-    # Save GDB's excepthook
     saved_excepthook = sys.excepthook
-    # Switch to default stdout/stderr
-    with pwndbg.lib.stdio.stdio:
-        yield
-    # Restore Python's default ps1, ps2, and excepthook for GDB's `pi` command
-    sys.ps1 = ">>> "
-    sys.ps2 = "... "
+    try:
+        saved_ps = sys.ps1, sys.ps2
+    except AttributeError:
+        saved_ps = None
+    yield
+    # Restore Python's default `ps1`, `ps2`, and `excepthook`
+    # to ensure proper behavior of the GDB repl.
+    if saved_ps is not None:
+        sys.ps1, sys.ps2 = saved_ps
+    else:
+        del sys.ps1
+        del sys.ps2
     sys.excepthook = saved_excepthook
 
 
