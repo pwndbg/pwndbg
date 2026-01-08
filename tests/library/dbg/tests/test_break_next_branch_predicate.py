5c8713cb0fac589128f05a76e6b47512b282fc98
nishantxscooby <officialnishant21@gmail.com>
2026-01-09 02:02:03 +0530

    tests: replace monkeypatched predicate test with real behavior test

--- tests/library/dbg/tests/test_break_next_branch_predicate.py
+++ tests/library/dbg/tests/test_break_next_branch_predicate.py
@@ -1,44 +1,11 @@
-import asyncio
-import types
-
-from pwndbg.aglib.next import break_next_branch
+import pytest
+import pwndbg.aglib.next
 
 
-class DummyEC:
-    async def cont(self, *args, **kwargs):
-        return
-
-
-def test_break_next_branch_predicate_false(monkeypatch):
-    class DummyIns:
-        def __init__(self):
-            self.address = 0xdeadbeef
-
-    def fake_next_branch(address=None, including_current=False):
-        return DummyIns()
-
-    fake_regs = types.SimpleNamespace(pc=0x0, reg_write=lambda *a, **k: None)
-
-    class DummyInferior:
-        def break_at(self, *args, **kwargs):
-            class CM:
-                def __enter__(self):
-                    return None
-                def __exit__(self, exc_type, exc, tb):
-                    return False
-            return CM()
-
-
-    monkeypatch.setattr("pwndbg.aglib.next.next_branch", fake_next_branch)
-    monkeypatch.setattr("pwndbg.aglib.regs", fake_regs)
-    monkeypatch.setattr("pwndbg.dbg.selected_inferior", lambda: DummyInferior())
-
-    def pred():
+@pytest.mark.asyncio
+async def test_break_next_branch_predicate_false(ctrl):
+    async def predicate():
         return False
 
-    ec = DummyEC()
-    result = asyncio.run(
-        break_next_branch(ec, including_current=True, predicate=pred)
-    )
-
-    assert result is None
+    ins = await pwndbg.aglib.next.break_next_branch(ctrl, predicate=predicate)
+    assert ins is None
