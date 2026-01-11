13e1d547aea7c196b05533eb36c59aec91794249
k4lizen <124312252+k4lizen@users.noreply.github.com>
2026-01-11 04:36:56 +0100

    ops

--- pwndbg/commands/integration.py
+++ pwndbg/commands/integration.py
@@ -436,8 +436,8 @@ def soft_connection_check(also_sync: bool) -> bool:
         if not pwndbg.integration.manager.is_connected():
             return False
 
-    # Give space to the actual command output.
-    print()
+        # Give space to the actual command output.
+        print()
 
     return True
 
