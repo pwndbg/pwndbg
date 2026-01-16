c29eb61f4095206d290888c1044d012944d1b70b
k4lizen <124312252+k4lizen@users.noreply.github.com>
2026-01-16 20:33:54 +0100

    clean up libcinfo

--- pwndbg/commands/libcinfo.py
+++ pwndbg/commands/libcinfo.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import pwndbg.color.memory
 import pwndbg.commands
 import pwndbg.libc
 import pwndbg.libc.facade
@@ -11,15 +12,30 @@ from pwndbg.commands import CommandCategory
 )
 @pwndbg.commands.OnlyWhenRunning
 def libcinfo():
-    # FIXME: What if some info isn't available?
+    try:
+        version = pwndbg.libc.facade.version()
+        version_str = ".".join(map(str, version))
+    except NotImplementedError:
+        version_str = "no version information"
 
-    version = pwndbg.libc.facade.version()
-    version_str = ".".join(map(str, version))
+    if version_str == "-1.-1":
+        version_str = "no version information"
 
+    print(f"libc: {pwndbg.libc.which().value}")
     print(f"libc version: {version_str}")
+
     urls = pwndbg.libc.urls()
     print("URLs:")
     print("    project homepage:      ", urls.homepage)
     print("    read the source:       ", urls.versioned_readable_source)
     print("    download the archive:  ", urls.versioned_compressed_source)
     print("    git clone              ", urls.git)
+    print("Mappings:")
+    print("    libc is at:            ", pwndbg.color.memory.get(pwndbg.libc.addr()))
+    print("          ", pwndbg.libc.filepath())
+    print("    ld is at:              ", pwndbg.color.memory.get(pwndbg.libc.loader_addr()))
+    print("          ", pwndbg.libc.loader_filepath())
+    print("Symbolication:")
+    print("    has exported symbols: ", "yes" if pwndbg.libc.has_exported_symbols() else "no")
+    print("    has internal symbols: ", "yes" if pwndbg.libc.has_internal_symbols() else "no")
+    print("    has debug info:       ", "yes" if pwndbg.libc.has_debug_info() else "no")
