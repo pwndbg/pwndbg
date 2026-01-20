407166a5e2633fc7e4ff9fcfd781d82dfeab7c1c
carlpinto25 <carlpinto25@gmail.com>
2026-01-20 11:11:23 +0530

    rough attempt at fixing

--- pwndbg/lib/memory.py
+++ pwndbg/lib/memory.py
@@ -140,7 +140,7 @@ class Page:
 
     @property
     def is_memory_mapped_file(self) -> bool:
-        return len(self.objfile) != 0 and self.objfile[0] != "["
+        return len(self.objfile) > 0 and (self.objfile[0] != "[" or self.objfile.startswith("[anon_"))
 
     @property
     def read(self) -> bool:
