638a52409318bfb8be9b8d3d113c9bcc1fd6597f
OB <obarroncs@gmail.com>
2026-02-10 21:09:30 -0500

    Remove copy pasted comment

--- tests/binaries/host/branch_visualization.x86-64.asm
+++ tests/binaries/host/branch_visualization.x86-64.asm
@@ -1,12 +1,5 @@
 global _start
 
-; This binary is there to test
-; emulate vs nearpc/u/pdisas commands
-; The emulate should show just jump and one nop
-; The rest should show jump and two nops
-;
-; Motivated by https://github.com/pwndbg/pwndbg/issues/315
-
 _start:
 A:
     mov     eax, 0
