3340743aa5cce977c6ce97ab66b750f1a7321a05
Ket_ch0p <96146875+Tomatotech90@users.noreply.github.com>
2026-06-13 16:45:35 -0400

    Create test_command_tls.py

---
+++ tests/library/gdb/test_command_tls.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import gdb
+
+from . import get_binary
+
+TLS_BINARY = get_binary("tls.x86-64.out")
+
+
+def test_tls_region_labeled_in_vmmap(start_binary):
+    start_binary(TLS_BINARY)
+
+    gdb.execute("break break_here")
+    gdb.execute("run")
+
+    vmmap_output = gdb.execute("vmmap", to_string=True)
+
+    assert "[tls]" in vmmap_output
