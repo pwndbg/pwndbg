b72737a18f9bc26a021fe35aa3b537ef3d669fcb
Ket_ch0p <96146875+Tomatotech90@users.noreply.github.com>
2026-06-13 18:00:33 -0400

    Update test_command_tls.py

--- tests/library/gdb/tests/test_command_tls.py
+++ tests/library/gdb/tests/test_command_tls.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
 import gdb
+import pytest
 
 from . import get_binary
 
 TLS_BINARY = get_binary("tls.x86-64.out")
 
 
-def test_tls_region_labeled_in_vmmap(start_binary):
+def test_tls_region_labeled_in_vmmap(start_binary: pytest.FixtureRequest) -> None:
     start_binary(TLS_BINARY)
 
     gdb.execute("break break_here")
