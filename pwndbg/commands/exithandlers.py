da841a91eab03c58a7c79e0685df1409da4009ea
h4kk4p4kk4 <38348883+h4kk4p4kk4@users.noreply.github.com>
2026-06-16 02:13:38 +0100

    refactor: replace pwndbg.color.message with from pwndbg.color import message

--- pwndbg/commands/exithandlers.py
+++ pwndbg/commands/exithandlers.py
@@ -18,10 +18,10 @@ import pwndbg.aglib.symbol
 import pwndbg.aglib.tls
 import pwndbg.chain
 import pwndbg.color.memory
-import pwndbg.color.message
 import pwndbg.commands
 import pwndbg.dintegration
 import pwndbg.emu.emulator
+from pwndbg.color import message
 
 
 @dataclass
@@ -94,7 +94,7 @@ def _get_pointer_guard() -> int | None:
             or pwndbg.aglib.tls.find_address_with_pthread_self()
         )
         if tls_addr is None:
-            print(pwndbg.color.message.error("Failed to get TLS address"))
+            print(message.error("Failed to get TLS address"))
             return None
         pointer_guard_offset = pwndbg.aglib.arch.ptrsize * 6
         return pwndbg.aglib.memory.read_pointer_width(tls_addr + pointer_guard_offset)
@@ -104,24 +104,20 @@ def _get_pointer_guard() -> int | None:
         ) or pwndbg.aglib.symbol.lookup_symbol("__pointer_chk_guard_local")
         if pointer_chk_guard is None:
             print(
-                pwndbg.color.message.error(
+                message.error(
                     "Could not find __pointer_chk_guard or __pointer_chk_guard_local symbols"
                 )
             )
             return None
         return pwndbg.aglib.memory.read_pointer_width(int(pointer_chk_guard))
-    print(
-        pwndbg.color.message.error(
-            f"Don't know how to get pointer_guard on {pwndbg.aglib.arch.name}"
-        )
-    )
+    print(message.error(f"Don't know how to get pointer_guard on {pwndbg.aglib.arch.name}"))
     return None
 
 
 def _get_exit_funcs_from_emulator() -> int | None:
     exit_addr = pwndbg.aglib.symbol.lookup_symbol("exit")
     if exit_addr is None:
-        print(pwndbg.color.message.error("Failed to get address of exit"))
+        print(message.error("Failed to get address of exit"))
         return None
     emulator = pwndbg.emu.emulator.Emulator()
     if pwndbg.aglib.arch.name == "i386":
@@ -131,11 +127,11 @@ def _get_exit_funcs_from_emulator() -> int | None:
         emulator.until_jump()  # call __run_exit_handlers
         esp = emulator.read_register("esp")
         if esp is None:
-            print(pwndbg.color.message.error("Failed to read ESP register"))
+            print(message.error("Failed to read ESP register"))
             return None
         exit_funcs_ptr_bytes = emulator.read_memory(esp + 8, 4)
         if exit_funcs_ptr_bytes is None:
-            print(pwndbg.color.message.error("Failed to read &__exit_funcs from stack"))
+            print(message.error("Failed to read &__exit_funcs from stack"))
             return None
         exit_funcs_ptr = typing.cast(int, u32(exit_funcs_ptr_bytes, "little"))
 
@@ -143,16 +139,12 @@ def _get_exit_funcs_from_emulator() -> int | None:
         emulator.until_jump(int(exit_addr))
         abi = pwndbg.aglib.arch.function_abi
         if abi is None:
-            print(pwndbg.color.message.error("arch.function_abi is None"))
+            print(message.error("arch.function_abi is None"))
             return None
         second_arg_reg = abi.register_arguments[1]
         exit_funcs_ptr = emulator.read_register(second_arg_reg)
         if exit_funcs_ptr is None:
-            print(
-                pwndbg.color.message.error(
-                    f"Failed to read second argument register ({second_arg_reg})"
-                )
-            )
+            print(message.error(f"Failed to read second argument register ({second_arg_reg})"))
             return None
     return exit_funcs_ptr
 
@@ -163,7 +155,7 @@ def _get_tls_dtor_list_offset_from_emulator_x86_64(
     while True:
         inst = pwndbg.aglib.disasm.disassembly.get_one_instruction(emulator.pc())
         if inst is None:
-            print(pwndbg.color.message.error("Failed to disassembly __call_tls_dtors"))
+            print(message.error("Failed to disassembly __call_tls_dtors"))
             return None
         read, _ = inst.cs_insn.regs_access()
         read_names: list[str] = [str(inst.cs_insn.reg_name(r)) for r in read]
@@ -171,7 +163,7 @@ def _get_tls_dtor_list_offset_from_emulator_x86_64(
             offset_reg = [r for r in read_names if r != "fs"][0]
             offset = emulator.read_register(offset_reg)
             if offset is None:
-                print(pwndbg.color.message.error(f"Failed to read offset from {offset_reg}"))
+                print(message.error(f"Failed to read offset from {offset_reg}"))
                 return None
             return typing.cast(int, u64(p64(offset, sign="unsigned"), sign="signed"))
         emulator.single_step()
@@ -183,7 +175,7 @@ def _get_tls_dtor_list_offset_from_emulator_i386(
     while True:
         inst = pwndbg.aglib.disasm.disassembly.get_one_instruction(emulator.pc())
         if inst is None:
-            print(pwndbg.color.message.error("Failed to disassemble __call_tls_dtors"))
+            print(message.error("Failed to disassemble __call_tls_dtors"))
             return None
         read, _ = inst.cs_insn.regs_access()
         read_names: list[str] = [str(inst.cs_insn.reg_name(r)) for r in read]
@@ -191,7 +183,7 @@ def _get_tls_dtor_list_offset_from_emulator_i386(
             offset_reg = [r for r in read_names if r != "gs"][0]
             offset = emulator.read_register(offset_reg)
             if offset is None:
-                print(pwndbg.color.message.error(f"Failed to read offset from {offset_reg}"))
+                print(message.error(f"Failed to read offset from {offset_reg}"))
                 return None
             return typing.cast(int, u32(p32(offset, sign="unsigned"), sign="signed"))
         emulator.single_step()
@@ -203,16 +195,12 @@ def _get_tls_dtor_list_offset_from_emulator_aarch64(
     while True:
         inst = pwndbg.aglib.disasm.disassembly.get_one_instruction(emulator.pc())
         if inst is None:
-            print(pwndbg.color.message.error("Failed to disassemble __call_tls_dtors"))
+            print(message.error("Failed to disassemble __call_tls_dtors"))
             return None
         if inst.mnemonic.lower() == "mrs" and "tpidr_el0" in inst.cs_insn.op_str.lower():
             _, written = inst.cs_insn.regs_access()
             if len(written) < 1:
-                print(
-                    pwndbg.color.message.error(
-                        "Failed to get write operand for mrs tpidr_el0 instruction"
-                    )
-                )
+                print(message.error("Failed to get write operand for mrs tpidr_el0 instruction"))
                 return None
             tls_base_reg = str(inst.cs_insn.reg_name(written[0]))
             emulator.update_pc(inst.next)  # unicorn seems to not like emulating mrs
@@ -220,7 +208,7 @@ def _get_tls_dtor_list_offset_from_emulator_aarch64(
             while True:
                 inst = pwndbg.aglib.disasm.disassembly.get_one_instruction(emulator.pc())
                 if inst is None:
-                    print(pwndbg.color.message.error("Failed to disassemble __call_tls_dtors"))
+                    print(message.error("Failed to disassemble __call_tls_dtors"))
                     return None
                 read, _ = inst.cs_insn.regs_access()
                 read_names = [str(inst.cs_insn.reg_name(r)) for r in read]
@@ -228,9 +216,7 @@ def _get_tls_dtor_list_offset_from_emulator_aarch64(
                     offset_reg = [r for r in read_names if r != tls_base_reg][0]
                     offset = emulator.read_register(offset_reg)
                     if offset is None:
-                        print(
-                            pwndbg.color.message.error(f"Failed to read offset from {offset_reg}")
-                        )
+                        print(message.error(f"Failed to read offset from {offset_reg}"))
                         return None
                     return typing.cast(int, u64(p64(offset, sign="unsigned"), sign="signed"))
                 emulator.single_step()
@@ -243,18 +229,18 @@ def _get_tls_dtor_list_offset_from_emulator_arm(
     bl_inst_addr, _ = emulator.until_jump()  # until what I think is a call to __aeabi_read_tp
     bl_inst = pwndbg.aglib.disasm.disassembly.get_one_instruction(bl_inst_addr)
     if bl_inst is None:
-        print(pwndbg.color.message.error("Failed to disassemble __call_tls_dtors"))
+        print(message.error("Failed to disassemble __call_tls_dtors"))
         return None
     emulator.update_pc(bl_inst.next)
     r0_reg_code = emulator.get_reg_enum("r0")
     if r0_reg_code is None:
-        print(pwndbg.color.message.error("Failed to get unicorn register code for r0"))
+        print(message.error("Failed to get unicorn register code for r0"))
         return None
     emulator.uc.reg_write(r0_reg_code, tls_addr)  # type: ignore[no-untyped-call, unused-ignore]
     while True:
         inst = pwndbg.aglib.disasm.disassembly.get_one_instruction(emulator.pc())
         if inst is None:
-            print(pwndbg.color.message.error("Failed to disassemble __call_tls_dtors"))
+            print(message.error("Failed to disassemble __call_tls_dtors"))
             return None
         read, _ = inst.cs_insn.regs_access()
         read_names = [str(inst.cs_insn.reg_name(r)) for r in read]
@@ -262,7 +248,7 @@ def _get_tls_dtor_list_offset_from_emulator_arm(
             offset_reg = [r for r in read_names if r != "r0"][0]
             offset = emulator.read_register(offset_reg)
             if offset is None:
-                print(pwndbg.color.message.error(f"Failed to read offset from {offset_reg}"))
+                print(message.error(f"Failed to read offset from {offset_reg}"))
                 return None
             return typing.cast(int, u32(p32(offset, sign="unsigned"), sign="signed"))
         emulator.single_step()
@@ -271,14 +257,14 @@ def _get_tls_dtor_list_offset_from_emulator_arm(
 def _get_tls_dtor_list_from_emulator() -> int | None:
     call_tls_dtors = pwndbg.aglib.symbol.lookup_symbol("__call_tls_dtors")
     if call_tls_dtors is None:
-        print(pwndbg.color.message.error("Failed to get address of __call_tls_dtors"))
+        print(message.error("Failed to get address of __call_tls_dtors"))
         return None
     tls_addr = (
         pwndbg.aglib.tls.find_address_with_register()
         or pwndbg.aglib.tls.find_address_with_pthread_self()
     )
     if tls_addr is None:
-        print(pwndbg.color.message.error("Failed to get TLS address"))
+        print(message.error("Failed to get TLS address"))
         return None
     emulator = pwndbg.emu.emulator.Emulator()
     emulator.update_pc(int(call_tls_dtors))
@@ -295,7 +281,7 @@ def _get_tls_dtor_list_from_emulator() -> int | None:
 
     if offset is None:
         print(
-            pwndbg.color.message.error(
+            message.error(
                 "Failed to get TLS offset to tls_dtor_list from emulating __call_tls_dtors"
             )
         )
@@ -362,14 +348,14 @@ parser = argparse.ArgumentParser(description="List currently registered glibc ex
 def exithandlers() -> None:
     pointer_guard = _get_pointer_guard()
     if pointer_guard is None:
-        print(pwndbg.color.message.error("Failed to get pointer_guard"))
+        print(message.error("Failed to get pointer_guard"))
         return
-    print(f"pointer_guard: {pwndbg.color.message.notice(hex(pointer_guard))}")
+    print(f"pointer_guard: {message.notice(hex(pointer_guard))}")
     exit_funcs_ptr = (
         pwndbg.aglib.symbol.lookup_symbol("__exit_funcs") or _get_exit_funcs_from_emulator()
     )
     if exit_funcs_ptr is None:
-        print(pwndbg.color.message.error("Failed to get address of __exit_funcs"))
+        print(message.error("Failed to get address of __exit_funcs"))
         return
     exit_funcs = pwndbg.aglib.memory.read_pointer_width(int(exit_funcs_ptr))
     print(f"\n__exit_funcs: {pwndbg.color.memory.get(exit_funcs)}")
@@ -382,7 +368,7 @@ def exithandlers() -> None:
         pwndbg.aglib.symbol.lookup_symbol("tls_dtor_list") or _get_tls_dtor_list_from_emulator()
     )
     if tls_dtor_list is None:
-        print(pwndbg.color.message.error("Failed to locate tls_dtor_list"))
+        print(message.error("Failed to locate tls_dtor_list"))
         return
     print(f"\ntls_dtor_list: {pwndbg.color.memory.get(tls_dtor_list)}")
     tls_dtors = _list_tls_dtors(pointer_guard, int(tls_dtor_list))
