ee6c1068747e139d49df7394263b0a7338e8afc0
OB <obarroncs@gmail.com>
2026-08-21 19:55:23 -0400

    Fix the tests once and for all

--- tests/library/dbg/tests/test_emulate.py
+++ tests/library/dbg/tests/test_emulate.py
@@ -102,31 +102,35 @@ def compare_output_without_emu(expected_output):
     assert nearpc(back_lines=5, total_lines=11, linear=True) == expected_output
 
 
-SYSCALLS_BINARY = get_binary("syscalls.x86-64.out")
+STEPSYSCALL_X64_BINARY = get_binary("stepsyscall.x86-64.out")
 
 
 @pwndbg_test
 async def test_backwards_backwards_disassemble_heuristic(ctrl: Controller) -> None:
 
-    await ctrl.launch(SYSCALLS_BINARY)
+    await ctrl.launch(STEPSYSCALL_X64_BINARY)
 
     # Enable the heuristic
     await ctrl.execute_and_capture("set heuristic-backwards-disasm on")
 
-    dis = await ctrl.execute_and_capture("nearpc $rip+20 -r 4 -t 11")
+    dis = await ctrl.execute_and_capture("nearpc -n -t 11")
 
     expected = (
-        "   0x400080 <_start>       mov    eax, 0                 EAX => 0\n"
-        "   0x400085 <_start+5>     mov    edi, 0x1337            EDI => 0x1337\n"
-        "   0x40008a <_start+10>    mov    esi, 0xdeadbeef        ESI => 0xdeadbeef\n"
-        "   0x40008f <_start+15>    mov    ecx, 0x10              ECX => 0x10\n"
-        " ► 0x400094 <_start+20>    syscall\n"
-        "   0x400096 <_start+22>    mov    eax, 0xa               EAX => 0xa\n"
-        "   0x40009b <_start+27>    int    0x80 <SYS_unlink>\n"
-        "   0x40009d                add    byte ptr [rax], al\n"
-        "   0x40009f                add    byte ptr [rax], al\n"
-        "   0x4000a1                add    byte ptr [rax], al\n"
-        "   0x4000a3                add    byte ptr [rax], al\n"
+        "   0x4000bd <do_read+5>               mov    edi, 0               EDI => 0\n"
+        "   0x4000c2 <do_read+10>              movabs rsi, __bss_start     RSI => 0x800001c (__bss_start)\n"
+        "   0x4000cc <do_read+20>              mov    edx, 1               EDX => 1\n"
+        "   0x4000d1 <syscall_read_label>      syscall\n"
+        "   0x4000d3 <syscall_read_label+2>    ret   \n"
+        " \n"
+        " ► 0x4000d4 <_start>                  nop   \n"
+        "   0x4000d5 <_start+1>                jmp    label1                      <label1>\n"
+        " \n"
+        "   0x4000d7 <_start+3>                nop   \n"
+        "   0x4000d8 <label1>                  call   write_stdout                <write_stdout>\n"
+        " \n"
+        "   0x4000dd <label1+5>                call   write_stderr                <write_stderr>\n"
+        " \n"
+        "   0x4000e2 <exit>                    mov    eax, 0x3c     EAX => 0x3c\n"
     )
 
     assert dis == expected
@@ -147,30 +151,35 @@ async def test_backwards_linear_cache_populate_when_disassemble_from_pc(ctrl: Co
     This is an edge case, because if the user does `nearpc random_address`, we don't want to add next couple instructions
     to the linear backward cache, because "random_address" may not be aligned to a real address. This would corrupt the cache,
     causing future disassembles at "random_address+offset" to not be able to disassemble backwards correctly (they would use the corrupted cache, and not the heuristic)
+
+    This is NOT testing the backwards disassembly itself. It is rather testing that the method of populating the linear cache works.
     """
 
-    await ctrl.launch(SYSCALLS_BINARY)
+    await ctrl.launch(STEPSYSCALL_X64_BINARY)
 
     # This filling up caches, disassembling from PC
-    dis = await ctrl.execute_and_capture("nearpc -t 11")
+    dis = await ctrl.execute_and_capture("nearpc -n -t 11")
 
     await ctrl.step_instruction()
 
-    dis = await ctrl.execute_and_capture("nearpc -t 11")
+    dis = await ctrl.execute_and_capture("nearpc -n -t 11")
 
     # This should be able to disassemble backwards using the backwards caches!
     expected = (
-        "   0x400080 <_start>       mov    eax, 0                 EAX => 0\n"
-        " ► 0x400085 <_start+5>     mov    edi, 0x1337            EDI => 0x1337\n"
-        "   0x40008a <_start+10>    mov    esi, 0xdeadbeef        ESI => 0xdeadbeef\n"
-        "   0x40008f <_start+15>    mov    ecx, 0x10              ECX => 0x10\n"
-        "   0x400094 <_start+20>    syscall <SYS_read>\n"
-        "   0x400096 <_start+22>    mov    eax, 0xa               EAX => 0xa\n"
-        "   0x40009b <_start+27>    int    0x80 <SYS_unlink>\n"
-        "   0x40009d                add    byte ptr [rax], al\n"
-        "   0x40009f                add    byte ptr [rax], al\n"
-        "   0x4000a1                add    byte ptr [rax], al\n"
-        "   0x4000a3                add    byte ptr [rax], al\n"
+        "   0x4000d4 <_start>                nop   \n"
+        " ► 0x4000d5 <_start+1>              jmp    label1                      <label1>\n"
+        " \n"
+        "   0x4000d7 <_start+3>              nop   \n"
+        "   0x4000d8 <label1>                call   write_stdout                <write_stdout>\n"
+        " \n"
+        "   0x4000dd <label1+5>              call   write_stderr                <write_stderr>\n"
+        " \n"
+        "   0x4000e2 <exit>                  mov    eax, 0x3c              EAX => 0x3c\n"
+        "   0x4000e7 <exit+5>                mov    edi, 0                 EDI => 0\n"
+        "   0x4000ec <syscall_exit_label>    syscall <SYS_exit>\n"
+        "   0x4000ee                         add    byte ptr [rax], al\n"
+        "   0x4000f0                         add    byte ptr [rax], al\n"
+        "   0x4000f2                         add    byte ptr [rax], al\n"
     )
     assert dis == expected
 
@@ -187,52 +196,77 @@ async def test_backwards_linear_cache_misaligned_disasm(ctrl: Controller) -> Non
     # Do not allow the disassembly section to run to populate the caches
     await ctrl.execute_and_capture("set context-sections ''")
 
-    await ctrl.launch(SYSCALLS_BINARY)
+    await ctrl.launch(STEPSYSCALL_X64_BINARY)
 
-    # This filling up caches, disassembling from misaligned address
-    await ctrl.execute_and_capture("nearpc $rip+3 -t 11")
+    # Disassemble from misaligned address
+    dis_0 = await ctrl.execute_and_capture("nearpc $rip-2 -t 11")
+
+    # Misaligned disassembly!
+    expected_0 = (
+        " ► 0x4000d2 <syscall_read_label+1>    add    eax, 0x1eb90c3\n"
+        "   0x4000d7 <_start+3>                nop   \n"
+        "   0x4000d8 <label1>                  call   write_stdout                <write_stdout>\n"
+        " \n"
+        "   0x4000dd <label1+5>                call   write_stderr                <write_stderr>\n"
+        " \n"
+        "   0x4000e2 <exit>                    mov    eax, 0x3c              EAX => 0x3c\n"
+        "   0x4000e7 <exit+5>                  mov    edi, 0                 EDI => 0\n"
+        "   0x4000ec <syscall_exit_label>      syscall <SYS_exit>\n"
+        "   0x4000ee                           add    byte ptr [rax], al\n"
+        "   0x4000f0                           add    byte ptr [rax], al\n"
+        "   0x4000f2                           add    byte ptr [rax], al\n"
+        "   0x4000f4                           add    byte ptr [rax], al\n"
+    )
+
+    assert dis_0 == expected_0
 
     # Disable the heuristic
     await ctrl.execute_and_capture("set heuristic-backwards-disasm off")
     await ctrl.execute_and_capture("set context-disasm-back-linear-lines 0")
 
     # Without the heuristic, we disassemble straightline
-    dis = await ctrl.execute_and_capture("nearpc $rip+10 -t 11")
+    dis = await ctrl.execute_and_capture("nearpc $rip+4 -t 11")
 
     expected = (
-        " ► 0x40008a <_start+10>    mov    esi, 0xdeadbeef        ESI => 0xdeadbeef\n"
-        "   0x40008f <_start+15>    mov    ecx, 0x10              ECX => 0x10\n"
-        "   0x400094 <_start+20>    syscall\n"
-        "   0x400096 <_start+22>    mov    eax, 0xa               EAX => 0xa\n"
-        "   0x40009b <_start+27>    int    0x80 <SYS_unlink>\n"
-        "   0x40009d                add    byte ptr [rax], al\n"
-        "   0x40009f                add    byte ptr [rax], al\n"
-        "   0x4000a1                add    byte ptr [rax], al\n"
-        "   0x4000a3                add    byte ptr [rax], al\n"
-        "   0x4000a5                add    byte ptr [rax], al\n"
-        "   0x4000a7                add    byte ptr [rax], al\n"
+        " ► 0x4000d8 <label1>                call   write_stdout                <write_stdout>\n"
+        " \n"
+        "   0x4000dd <label1+5>              call   write_stderr                <write_stderr>\n"
+        " \n"
+        "   0x4000e2 <exit>                  mov    eax, 0x3c              EAX => 0x3c\n"
+        "   0x4000e7 <exit+5>                mov    edi, 0                 EDI => 0\n"
+        "   0x4000ec <syscall_exit_label>    syscall <SYS_exit>\n"
+        "   0x4000ee                         add    byte ptr [rax], al\n"
+        "   0x4000f0                         add    byte ptr [rax], al\n"
+        "   0x4000f2                         add    byte ptr [rax], al\n"
+        "   0x4000f4                         add    byte ptr [rax], al\n"
+        "   0x4000f6                         add    byte ptr [rax], al\n"
+        "   0x4000f8                         add    byte ptr [rax], al\n"
     )
     assert dis == expected
 
     # Enable the heuristic
     await ctrl.execute_and_capture("set heuristic-backwards-disasm on")
 
-    # Now, nearpc from an address after it. The caches should NOT be filled.
-    # Instead, the heuristic will be used to disassemble backwards, to correctly find "start_"
-    dis_2 = await ctrl.execute_and_capture("nearpc $rip+10 -r 2 -t 11")
+    # The caches should NOT be filled.
+    # Instead, the heuristic will be used to disassemble backwards
+    dis_2 = await ctrl.execute_and_capture("nearpc $rip+4 -n -t 11")
 
     # This should be able to disassemble backwards using the backwards caches!
     expected_2 = (
-        "   0x400080 <_start>       mov    eax, 0                 EAX => 0\n"
-        "   0x400085 <_start+5>     mov    edi, 0x1337            EDI => 0x1337\n"
-        " ► 0x40008a <_start+10>    mov    esi, 0xdeadbeef        ESI => 0xdeadbeef\n"
-        "   0x40008f <_start+15>    mov    ecx, 0x10              ECX => 0x10\n"
-        "   0x400094 <_start+20>    syscall\n"
-        "   0x400096 <_start+22>    mov    eax, 0xa               EAX => 0xa\n"
-        "   0x40009b <_start+27>    int    0x80 <SYS_unlink>\n"
-        "   0x40009d                add    byte ptr [rax], al\n"
-        "   0x40009f                add    byte ptr [rax], al\n"
-        "   0x4000a1                add    byte ptr [rax], al\n"
-        "   0x4000a3                add    byte ptr [rax], al\n"
+        "   0x4000d1 <syscall_read_label>      syscall\n"
+        "   0x4000d3 <syscall_read_label+2>    ret   \n"
+        " \n"
+        "   0x4000d4 <_start>                  nop   \n"
+        "   0x4000d5 <_start+1>                jmp    label1                      <label1>\n"
+        " \n"
+        "   0x4000d7 <_start+3>                nop   \n"
+        " ► 0x4000d8 <label1>                  call   write_stdout                <write_stdout>\n"
+        " \n"
+        "   0x4000dd <label1+5>                call   write_stderr                <write_stderr>\n"
+        " \n"
+        "   0x4000e2 <exit>                    mov    eax, 0x3c              EAX => 0x3c\n"
+        "   0x4000e7 <exit+5>                  mov    edi, 0                 EDI => 0\n"
+        "   0x4000ec <syscall_exit_label>      syscall <SYS_exit>\n"
+        "   0x4000ee                           add    byte ptr [rax], al\n"
     )
     assert dis_2 == expected_2
