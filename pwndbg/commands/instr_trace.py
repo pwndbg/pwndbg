1b7021877e0f59a8c4318be0b76dacf8a8a516ca
turtle <christopheralonso1@gmail.com>
2026-09-02 15:36:25 -0400

    changed output format to support raw format, lighthouse format and full which includes symbols+lighthouse format

--- pwndbg/commands/instr_trace.py
+++ pwndbg/commands/instr_trace.py
@@ -16,13 +16,13 @@ parser = argparse.ArgumentParser(
 Dumps an instruction trace to disk.
 
 This command will execute consecutive single steps from the current pc, until the end_address is reached or the program exits.
-Saving each pc to a file on disk. If -v is passed the backing file of the memory region will also be saved.
+Saving each pc to a file on disk. Multiple formats can be provided such as lighthouse, full and raw.
 """
 )
 parser.add_argument("trace_file", type=str, help="A path to write the trace file.")
 parser.add_argument("end_address", type=int, help="The end address to stop tracing.")
 parser.add_argument(
-    "-v", "--verbose", default=False, action="store_true", help="Flag to dump backing file."
+    "format", type=str, choices=["raw", "lighthouse", "full"], help="The type of trace output"
 )
 
 
@@ -30,50 +30,70 @@ async def _step_and_log(
     ec: pwndbg.dbg_mod.ExecutionController,
     file_handle: io.TextIOWrapper,
     vmmap_object: pwndbg.dbg_mod.MemoryMap,
-    end_address: int | None = None,
+    end_address: int = 0,
+    format: str = "raw",
 ) -> None:
-    if end_address == 0:
-        return
     count = 0
     while pwndbg.aglib.proc.alive():
         current_pc = pwndbg.aglib.regs_mod.regs.pc
         if current_pc == end_address:
             break
-        current_symbol = pwndbg.aglib.symbol.resolve_addr(int(current_pc))
+        if current_pc is None:
+            message.error("instr-trace: Failed to get current PC. Exiting.")
+            return
+
         await ec.single_step()
-        if vmmap_object is not None:
-            page_ = vmmap_object.lookup_page(current_pc)
+        page_ = vmmap_object.lookup_page(current_pc)
 
-            if page_:
-                log_line = f"[{hex(current_pc)}]!{current_symbol} : {page_.objfile:<8}\n"
-            else:
-                log_line = f"[{hex(current_pc)}]!{current_symbol}\n"
+        # if page not available fallback to raw
+        if page_ is None:
+            message.error("instr-trace: failed to lookup page falling back to raw format")
+            log_line = f"{hex(current_pc)}"
             file_handle.write(log_line)
+            count += 1
+            continue
 
-        else:
-            log_line = f"[{hex(current_pc)}]!{current_symbol}\n"
-            file_handle.write(log_line)
+        # format line
+        match format:
+            case "raw":
+                # raw format
+                log_line = f"{hex(current_pc)}\n"
+            case "lighthouse":
+                # lighthouse coverage format module+offset https://github.com/gaasedelen/lighthouse
+                offset = hex(page_.end - current_pc)
+                filename = page_.objfile.split("/")[-1]
+                log_line = f"{filename}+{offset}\n"
+            case "full":
+                # symbols included
+                current_symbol = pwndbg.aglib.symbol.resolve_addr(int(current_pc))
+                filename = page_.objfile.split("/")[-1]
+                log_line = f"{filename}!{current_symbol}\n"
+            case _:
+                # should never be possible as we assert in command input
+                message.error(
+                    f"instr-trace: format {format} not in acceptable formats: [lighthouse, raw, full]"
+                )
+                return
+        file_handle.write(log_line)
         count += 1
+
     print(message.info(f"instr-trace: Dumped {count} instructions"))
 
 
 @pwndbg.commands.Command(
     parser,
     category=pwndbg.commands.CommandCategory.MISC,
-    examples="instr-trace /tmp/trace.txt 0x555555555158 -v",
+    examples="instr-trace /tmp/trace.txt 0x555555555158 full",
 )
 @pwndbg.commands.OnlyWhenRunning
-def instr_trace(trace_file: str, end_address: int, verbose: bool) -> None:
+def instr_trace(trace_file: str, end_address: int, format: str) -> None:
     vmmap_object = pwndbg.aglib.vmmap.get_memory_map()
 
     async def dump_trace_log(ec: pwndbg.dbg_mod.ExecutionController) -> None:
         with open(trace_file, "w+") as f:
-            if not verbose:
-                await _step_and_log(ec, file_handle=f, vmmap_object=None, end_address=end_address)
-            else:
-                await _step_and_log(
-                    ec, file_handle=f, vmmap_object=vmmap_object, end_address=end_address
-                )
+            await _step_and_log(
+                ec, file_handle=f, vmmap_object=vmmap_object, end_address=end_address, format=format
+            )
 
     try:
         print(
